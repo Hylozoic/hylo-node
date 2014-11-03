874b77ed3ec405837c523aabb077e3132ef26ce4
Lawrence Wang <l@lw.io>
2014-11-03 14:14:19 -0800

    allow logged-in user to view their communities

--- config/policies.js
+++ config/policies.js
@@ -29,7 +29,8 @@ module.exports.policies = {
   '*': false,
 
   UserController: {
-    findOne: ['playSessionCheck', 'sessionAuth', 'isSelf']
+    findOne: ['playSessionCheck', 'sessionAuth', 'isSelf'],
+    populate: ['playSessionCheck', 'sessionAuth', 'isParent']
   }
 
   /***************************************************************************
