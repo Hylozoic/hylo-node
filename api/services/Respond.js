a2fc3e2a0639b6dacabf29b67965802f075719d4
Lawrence Wang <l@lw.io>
2014-11-11 16:35:25 -0800

    generalize Respond.with

--- api/services/Respond.js
+++ api/services/Respond.js
@@ -1,5 +1,6 @@
 module.exports = {
   with: function(obj, res) {
+    if (!obj) obj = {};
     var json = JSON.stringify(obj, null, (process.env.PRETTY_JSON ? 2 : null));
     res.setHeader('Content-Type', 'application/json');
     res.send(json);
