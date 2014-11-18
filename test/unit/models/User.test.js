bcfb8cbc47dddd182d52d504893199ba889a8032
Lawrence Wang <l@lw.io>
2014-11-18 13:14:32 -0800

    make names consistent

--- test/unit/models/User.test.js
+++ test/unit/models/User.test.js
@@ -9,24 +9,24 @@ describe('User', function() {
   });
 
   it('can be found', function(done) {
-    User.where({name: 'Cat'}).fetch().then(function(user) {
-      expect(user).to.exist;
-      expect(user.get('name')).to.equal('Cat');
+    User.where({name: 'Cat'}).fetch().then(function(cat) {
+      expect(cat).to.exist;
+      expect(cat.get('name')).to.equal('Cat');
     }).done(done);
   })
 
   it('can join communities', function(done) {
     var community1 = new Community({name: 'House'}),
       community2 = new Community({name: 'Yard'}),
-      user;
+      cat;
 
     User.named('Cat')
-    .then(function(u) { user = u; })
+    .then(function(u) { cat = u; })
     .then(function() { return community1.save(); })
     .then(function() { return community2.save(); })
-    .then(function() { return user.joinCommunity(community1); })
-    .then(function() { return user.joinCommunity(community2); })
-    .then(function() { return user.communities().fetch(); })
+    .then(function() { return cat.joinCommunity(community1); })
+    .then(function() { return cat.joinCommunity(community2); })
+    .then(function() { return cat.communities().fetch(); })
     .then(function(communities) {
       expect(communities).to.exist;
       expect(communities.models).to.exist;
