ede4ed5c4dc9ccde168377b14e2f450c0f82d5d9
Ray Marceau <razor2n4@gmail.com>
2014-11-21 18:17:34 -0800

    migrate postController.find to node and pass followers back with seed
    https://app.asana.com/0/5451362965522/19407564783104

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -5,26 +5,85 @@ module.exports = {
 
     Community.withId(req.param('id')).then(function(community) {
 
-      // weird:
-      //
-      // if i just do community.posts().fetch(), it runs:
-      //
-      //   select "post".*, "post_community"."community_id" as "_pivot_community_id", "post_community"."post_id" as "_pivot_post_id"
-      //   from "post" inner join "post_community" on "post_community"."post_id" = "post"."id"
-      //   where "post_community"."community_id" = $1
-      //
-      // but if i add other clauses as below, it drops the join and runs:
-      //
-      //   select * from "post" where "type" = $1 order by "last_updated" desc limit $2 offset $3
+      community.posts().query(function(qb) {
+        if (params.postType && params.postType != 'all') {
+          qb.where({type: params.postType});
+        }
+        qb.where({active: true});
+        qb.orderBy(sortCol, 'desc');
+        qb.limit(params.limit);
+        qb.offset(params.start);
+      }).fetch({
+        withRelated: [{"creator": function(qb) {
+          qb.column("id", "name", "avatar_url")
+        }}, {"community": function(qb) {
+          qb.column("id", 'name', "slug")
+        }}, "followers", {"followers.user": function(qb) {
+          qb.column("id", "name", "avatar_url")
+        }}, "contributors", {"contributors.user": function(qb){
+          qb.column("id", "name", "avatar_url")
+        }}]
+      }).then(function(posts) {
+        var postIds = posts.pluck("id");
 
-      var query = community.posts().query();
-      if (params.postType && params.postType != 'all') {
-        query = query.where({type: params.postType});
-      }
-      query.orderBy(sortCol, 'desc')
-      .limit(params.limit)
-      .offset(params.start).then(function(posts) {
-        res.ok(posts);
+        // Determine which posts this user voted on already
+        Vote.userVotesWithin(req.session.user.id, postIds).pluck("post_id").then(function(myVotesWithin) {
+
+          var postsDto = [];
+
+          posts.each(function(post, index) {
+
+            var followers = post.related("followers").map(function(follower) {
+              var user = follower.related("user");
+              return {
+                "value": Number(user.get("id")),
+                "name": user.get("name"),
+                "avatar": user.get("avatar_url")
+              }
+            })
+
+            var creator = post.related("creator");
+            var creatorDto = {
+              "id": Number(creator.get("id")),
+              "name": creator.get("name"),
+              "avatar": creator.get("avatar_url")
+            };
+
+            var contributors = post.related("contributors").map(function(contributor) {
+              var user = contributor.related("user");
+              return {
+                "id": Number(user.get("id")),
+                "name": user.get("name"),
+                "avatar": user.get("avatar_url")
+              };
+            });
+
+            var dto = {
+              "id": Number(post.get("id")),
+              "name": post.get("name"),
+              "description": post.get("description"),
+              "postType": post.get("type"),
+              "imageUrl": post.get("image_url"),
+              "user": creatorDto,
+              "creationDate": post.get("creation_date"),
+              "votes": post.get("num_votes"),
+              "numComments": post.get("num_comments"),
+              "fulfilled": post.get("fulfilled"),
+              "contributors": contributors,
+              "communitySlug": post.related("community").first().get("slug"),
+              "cName": post.related("community").first().get("name"),
+              "myVote": _.contains(myVotesWithin, post.get("id")),
+              "comments": [], // TODO Load Comments?
+              "commentsLoaded": false,
+              "followers": followers,
+              "followersLoaded": true,
+              "numFollowers": followers.length
+            }
+            postsDto.push(dto)
+          });
+
+          res.ok(postsDto);
+        })
       })
     })
   }
