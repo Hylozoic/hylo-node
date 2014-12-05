17e5ab5aee23b38bacc3dc3a1b908411b0ccb73f
Lawrence Wang <l@lw.io>
2014-12-05 14:47:31 -0800

    Membership#withIds -> Membership#find
    rename to reflect that it supports slugs as well as ids

--- api/policies/isMember.js
+++ api/policies/isMember.js
@@ -1,5 +1,5 @@
 module.exports = function isMember(req, res, next) {
-  Membership.withIds(req.session.user.id, req.param('id'))
+  Membership.find(req.session.user.id, req.param('id'))
   .then(function(membership) {
     membership ? next() : res.forbidden();
   });
