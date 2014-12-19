4a1fc45933adf9873e4286376bca6d3181860ae2
Lawrence Wang <l@lw.io>
2014-12-18 18:25:10 -0800

    add Comment model

---
+++ api/models/Comment.js
@@ -0,0 +1,17 @@
+module.exports = bookshelf.Model.extend({
+
+  tableName: 'comment',
+
+  user: function() {
+    return this.belongsTo(User);
+  },
+
+  post: function() {
+    return this.belongsTo(Post);
+  },
+
+  text: function() {
+    return this.get('comment_text');
+  }
+
+})
