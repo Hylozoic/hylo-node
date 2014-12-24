6560cce72e40eb7667843f6bba1e99ecc41f6964
Ray Marceau <razor2n4@gmail.com>
2014-12-24 02:17:05 -0500

    backend for thanks

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -67,7 +67,57 @@ module.exports = {
   },
 
   thanks: function(req, res) {
+    var params = _.pick(req.allParams(), ['id', 'limit', 'start']),
+      limit = params.limit ? params.limit : 15,
+      start = params.start ? params.start : 0;
 
+    var userId = params.id;
+
+    var isSelf = req.session.user.id === userId;
+
+    User.find(userId).then(function(user) {
+      user.thanks().query(function(qb) {
+        qb.orderBy("date_thanked");
+        qb.limit(limit);
+        qb.offset(start);
+
+        if (!isSelf) {
+          qb.join("comment", "comment.id", "=", "thank_you.comment_id");
+          qb.join("post", "post.id", "=", "comment.post_id");
+          qb.join("post_community", "post_community.post_id", "=", "post.id");
+          qb.join("community", "community.id", "=", "post_community.community_id");
+
+          var curUserActiveMembershipsSubQuery = bookshelf.knex.select("community_id")
+            .from("users_community")
+            .where("users_id", "=", req.session.user.id)
+            .where("active", "=", true);
+
+          qb.whereIn("community.id", curUserActiveMembershipsSubQuery);
+        }
+      }).fetch({
+        withRelated: [
+          {
+            "thankedBy": function(qb) {
+              qb.column("id", "name", "avatar_url");
+            },
+            "comment": function(qb) {
+              qb.column("id", 'comment_text', 'post_id')
+            },
+            "comment.post.creator": function (qb) {
+              qb.column("id", "name", "avatar_url");
+            },
+            "comment.post": function (qb) {
+              qb.column("id", "name", "creator_id", "type");
+            },
+            "comment.post.communities": function(qb) {
+              qb.column("id", "name");
+            }
+          }
+        ]
+      }).then(function(thanks) {
+        res.ok(thanks);
+      });
+    });
   },
 
   update: function(req, res) {
