a7808b4bac3cd347d344c7ce0febfba10fb78085
Lawrence Wang <l@lw.io>
2015-01-08 19:19:01 -0800

    implement skill updating
    look at that sexy test. writing the test first really helped me
    implement this faster and ensure correctness.

--- api/models/Skill.js
+++ api/models/Skill.js
@@ -1,3 +1,5 @@
+var Promise = require('bluebird');
+
 module.exports = bookshelf.Model.extend({
   tableName: 'users_skill',
 
@@ -12,11 +14,28 @@ module.exports = bookshelf.Model.extend({
     });
   },
 
-  batchCreate: function(skills, userId) {
-    // for each skill
-    // create if it doesn't already exist
-    // link to user
+  update: function(skills, userId) {
+    return Skill.where({users_id: userId}).fetchAll().then(function(collection) {
+      var existing = Skill.simpleList(collection),
+        toAdd = _.difference(skills, existing),
+        toRemove = _.difference(existing, skills),
+        queries = [], q;
 
+      if (toRemove.length > 0) {
+        q = bookshelf.knex('users_skill').where('users_id', userId).whereIn('skill_name', toRemove).del();
+        queries.push(q);
+      }
+
+      if (toAdd.length > 0) {
+        var values = _.map(toAdd, function(name) {
+          return {skill_name: name, users_id: userId};
+        })
+        q = bookshelf.knex('users_skill').insert(values);
+        queries.push(q);
+      }
+
+      return Promise.all(queries);
+    });
   }
 
 });
