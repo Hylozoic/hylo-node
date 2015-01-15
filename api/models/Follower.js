cac889f4f2abb816d1c3a397220641030b4b73de
Ray Marceau <razor2n4@gmail.com>
2015-01-15 11:39:33 -0800

    add user who created seed as a follower to the seed on create

--- api/models/Follower.js
+++ api/models/Follower.js
@@ -20,7 +20,7 @@ module.exports = bookshelf.Model.extend({
 
   addFollower: function(postId, followerId, addedById, options) {
     // TODO add validation to make sure follower is a member of the community that the post belongs to.
-    return Follower.where({post_id: postId, user_id: followerId}).fetch().then(function(existingFollower) {
+    return Follower.where({post_id: postId, user_id: followerId}).fetch(_.pick(options, "transacting")).then(function(existingFollower) {
       if (existingFollower) {
         sails.log.debug("user already following post... returning follower");
         return Promise.resolve(existingFollower);
