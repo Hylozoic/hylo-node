299315521f44a0eb6c6f39290ed654d3442e8ba0
Ray Marceau <razor2n4@gmail.com>
2015-01-15 13:19:43 -0800

    formatting for addFollower method

--- api/models/Follower.js
+++ api/models/Follower.js
@@ -20,18 +20,20 @@ module.exports = bookshelf.Model.extend({
 
   addFollower: function(postId, followerId, addedById, options) {
     // TODO add validation to make sure follower is a member of the community that the post belongs to.
-    return Follower.where({post_id: postId, user_id: followerId}).fetch(_.pick(options, "transacting")).then(function(existingFollower) {
-      if (existingFollower) {
-        sails.log.debug("user already following post... returning follower");
-        return Promise.resolve(existingFollower);
-      } else {
-        return new Follower({
-          post_id: postId,
-          date_added: new Date(),
-          user_id: followerId,
-          added_by_id: addedById
-        }).save(null, options);
-      }
-    });
+    return Follower.where({post_id: postId, user_id: followerId})
+      .fetch(_.pick(options, "transacting"))
+      .then(function(existingFollower) {
+        if (existingFollower) {
+          sails.log.debug("user already following post... returning follower");
+          return Promise.resolve(existingFollower);
+        } else {
+          return new Follower({
+            post_id: postId,
+            date_added: new Date(),
+            user_id: followerId,
+            added_by_id: addedById
+          }).save(null, options);
+        }
+      });
   }
 });
