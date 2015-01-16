cf3483ec6974613668b32ec519acc4b5e913e9c6
Lawrence Wang <l@lw.io>
2015-01-16 12:52:25 -0800

    make transition to new play session handling seamless

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -11,7 +11,7 @@ module.exports = function(req, res, next) {
 
   // User is allowed, proceed to the next policy,
   // or if this is the last policy, the controller
-  if (req.session.authenticated) {
+  if (req.session.authenticated && req.session.userId) {
     next();
   } else {
     var playSession = new PlaySession(req);
