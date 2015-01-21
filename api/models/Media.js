2bc078149797e56d3a94e5101f9ba2639857bf92
Lawrence Wang <l@lw.io>
2015-01-20 21:15:15 -0800

    save media with new posts

--- api/models/Media.js
+++ api/models/Media.js
@@ -1,7 +1,17 @@
 module.exports = bookshelf.Model.extend({
+  tableName: 'media',
 
   post: function() {
     return this.belongsTo(Post);
+  }
+
+}, {
+
+  create: function(opts) {
+    return new Media({
+      post_id: opts.postId,
+      url: opts.url
+    }).save(null, _.pick(opts, 'transacting'));
   }
 
 });
