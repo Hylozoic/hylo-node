d16a43962680e6ba30eb2f375d20b6f6fa0c3703
Lawrence Wang <l@lw.io>
2015-01-26 15:27:08 -0800

    better sails console
    stop trying to start a server

--- console.js
+++ console.js
@@ -1,17 +1,48 @@
 #!/usr/bin/env node
-global.sails = require('./app');
 
-sails.on('lifted', function() {
-  var repl = require('repl').start({
-    prompt: '> ',
-    input: process.stdin,
-    output: process.stdout
+var _ = require('lodash'),
+  colors = require('colors'),
+  sails = require('sails');
+
+console.log("Lifting Sails in interactive mode...".blue);
+
+(function() {
+  // Try to get `rc` dependency
+  var rc;
+  try {
+    rc = require('rc');
+  } catch (e0) {
+    try {
+      rc = require('sails/node_modules/rc');
+    } catch (e1) {
+      console.error('Could not find dependency: `rc`.');
+      console.error('Your `.sailsrc` file(s) will be ignored.');
+      console.error('To resolve this, run:');
+      console.error('npm install rc --save');
+      rc = function () { return {}; };
+    }
+  }
+
+  // Start server
+  sails.lift(_.merge(rc('sails'), {
+    log: {
+      noShip: true
+    },
+    hooks: {
+      http: false,
+      sockets: false,
+      views: false
+    }
+  }), function(err) {
+
+    var repl = require('repl').start('sails> ');
+
+    repl.on('exit', function() {
+      process.exit();
+    });
+
+    require('promirepl').promirepl(repl);
+
   });
 
-  repl.on('exit', function() {
-    process.exit();
-  });
-
-  require('promirepl').promirepl(repl);
-})
-
+})();
