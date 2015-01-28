c2e147312b3b4360e2ca2cf62a3eb86b780fde20
Lawrence Wang <l@lw.io>
2015-01-28 11:56:20 -0800

    add history to console

--- console.js
+++ console.js
@@ -1,7 +1,12 @@
 #!/usr/bin/env node
 
+/**
+ * n.b.: hella copy-pasted from sails/bin/sails-console.js
+ */
+
 var _ = require('lodash'),
   colors = require('colors'),
+  fs = require('fs'),
   sails = require('sails');
 
 console.log("Lifting Sails in interactive mode...".blue);
@@ -36,7 +41,11 @@ console.log("Lifting Sails in interactive mode...".blue);
   }), function(err) {
 
     var repl = require('repl').start('sails> ');
-
+    try {
+      history(repl, require('path').join(sails.config.paths.tmp, '.node_history'));
+    } catch (e) {
+      console.log('Error finding console history:', e);
+    }
     repl.on('exit', function() {
       process.exit();
     });
@@ -46,3 +55,50 @@ console.log("Lifting Sails in interactive mode...".blue);
   });
 
 })();
+
+
+/**
+* REPL History
+* Pulled directly from https://github.com/tmpvar/repl.history
+* with the slight tweak of setting historyIndex to -1 so that
+* it works as expected.
+*/
+
+function history(repl, file) {
+
+try {
+  var stat = fs.statSync(file);
+  repl.rli.history = fs.readFileSync(file, 'utf-8').split('\n').reverse();
+  repl.rli.history.shift();
+  repl.rli.historyIndex = -1;
+} catch (e) {}
+
+var fd = fs.openSync(file, 'a'),
+  reval = repl.eval;
+
+repl.rli.addListener('line', function(code) {
+  if (code && code !== '.history') {
+    fs.write(fd, code + '\n');
+  } else {
+    repl.rli.historyIndex++;
+    repl.rli.history.pop();
+  }
+});
+
+process.on('exit', function() {
+  fs.closeSync(fd);
+});
+
+repl.commands['.history'] = {
+  help: 'Show the history',
+  action: function() {
+    var out = [];
+    repl.rli.history.forEach(function(v, k) {
+      out.push(v);
+    });
+    repl.outputStream.write(out.reverse().join('\n') + '\n');
+    repl.displayPrompt();
+  }
+};
+}
+
