365b76734a3bd7b6bb709742679a1e25aeeaa337
Lawrence Wang <l@lw.io>
2015-02-04 14:43:01 -0800

    expand commenting test to multiple followers

--- test/unit/controllers/CommentController.test.js
+++ test/unit/controllers/CommentController.test.js
@@ -1,4 +1,5 @@
 var setup = require(require('root-path')('test/setup')),
+  format = require('util').format,
   Promise = require("bluebird"),
   CommentController = requireFromRoot('api/controllers/CommentController');
 
@@ -10,6 +11,7 @@ describe('CommentController', function() {
       return Promise.props({
         u1: new User({name: 'U1'}).save(),
         u2: new User({name: 'U2'}).save(),
+        u3: new User({name: 'U3'}).save(),
         p1: new Post({name: 'P1'}).save(),
         c1: new Community({name: "C1"}).save()
       }).then(function(props) {
@@ -33,25 +35,36 @@ describe('CommentController', function() {
   describe('#create', function() {
 
     it('creates a comment', function(done) {
+      var commentText = format("<p>Hey <a data-user-id=\"%s\">U2</a> and <a data-user-id=\"%s\">U3</a>, you're mentioned ;)</p>",
+        fixtures.u2.id, fixtures.u3.id);
+
       req.param = function(name) {
-        if (name == 'text')
-          return "<p>Hey <a data-user-id='" + fixtures.u2.id + "'>U2</a>, you're mentioned ;)</p>";
+        if (name == 'text') return commentText;
       };
 
       res.locals = {post: fixtures.p1};
 
+      // FIXME find a better solution to this nested try/catch
       res.ok = function(data) {
-        expect(data).to.exist;
-        expect(data.user).to.exist;
-        expect(data.text).to.equal("<p>Hey <a data-user-id=\"" + fixtures.u2.id + "\">U2</a>, you're mentioned ;)</p>");
+        try {
+          expect(data).to.exist;
+          expect(data.user).to.exist;
+          expect(data.text).to.equal(commentText);
 
-        // mentioning should cause an email notification
-        expect(require('kue').jobCount()).to.equal(1);
+          // mentioning should cause email notifications
+          expect(require('kue').jobCount()).to.equal(2);
 
-        fixtures.p1.load('followers').then(function(post) {
-          expect(post.relations.followers.length).to.equal(1);
-          done();
-        })
+          fixtures.p1.load('followers').then(function(post) {
+            try {
+              expect(post.relations.followers.length).to.equal(2);
+              done();
+            } catch(err) {
+              done(err);
+            }
+          })
+        } catch(err) {
+          done(err);
+        }
       };
 
       CommentController.create(req, res);
