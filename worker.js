103731c9e8c5692595793a3494a5e997e397d390
Lawrence Wang <l@lw.io>
2015-02-11 16:44:08 -0800

    stub out cron script
    factor out Sails startup into lib/skiff

--- worker.js
+++ worker.js
@@ -1,15 +1,11 @@
-if (process.env.NEW_RELIC_LICENSE_KEY) {
-  require('newrelic');
-}
-
-require('./config/kue');
+var skiff = require('./lib/skiff'); // this must be first
+require('./config/kue'); // this must be second
 
 var _ = require('lodash'),
   colors = require('colors'),
   Promise = require('bluebird'),
-  rc = require('rc'),
-  rollbar = require('rollbar'),
-  sails = require('sails'),
+  queue = require('kue').createQueue(),
+  sails = skiff.sails,
   util = require('util');
 
 // define new jobs here.
@@ -35,12 +31,7 @@ var jobDefinitions = {
 
 };
 
-var Worker = function() {
-  this.queue = require('kue').createQueue();
-};
-
-Worker.prototype.start = function() {
-  var queue = this.queue;
+var processJobs = function() {
 
   // load jobs
   _.forIn(jobDefinitions, function(promise, name) {
@@ -67,38 +58,12 @@ Worker.prototype.start = function() {
   // check for delayed jobs to enqueue.
   // this must be run in only one process to avoid a race condition:
   // https://github.com/learnboost/kue#delayed-jobs
-  worker.queue.promote(2000);
+  queue.promote(2000);
 };
 
-var worker = new Worker();
-
-// set up graceful shutdown.
-// these have to be defined outside the "sails lift" callback,
-// otherwise they are overridden by Sails.
-process.on('SIGINT', function() {
-  console.log();
-  process.emit('SIGTERM');
-  return false;
-})
-.on('SIGTERM', function() {
-  sails.log.info("Landing...".yellow);
-  worker.queue.shutdown(function(err) {
-    sails.lower();
-    sails.log.info("Done".green);
-  }, 5000);
-});
-
-// go!
-sails.log.info("Lifting...".yellow);
-sails.lift(_.merge(rc('sails'), {
-  log: {noShip: true},
-  hooks: {http: false, sockets: false, views: false}
-}), function(err) {
-  if (err) {
-    console.error("Couldn't lift Sails: " + err);
-    process.exit(1);
+skiff.lift({
+  start: processJobs,
+  stop: function(done) {
+    queue.shutdown(done, 5000);
   }
-
-  worker.start();
-  sails.log.info('Aloft.'.blue);
 });
