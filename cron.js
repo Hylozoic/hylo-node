d337a1c92bfeded0908c9531f28ca1c250b297cc
Lawrence Wang <l@lw.io>
2015-02-12 19:19:52 -0800

    add "npm run cron"

--- cron.js
+++ cron.js
@@ -1,9 +1,26 @@
 var skiff = require('./lib/skiff'),
   sails = skiff.sails;
 
+var intervals = {
+
+  daily: function() {
+
+  },
+
+  hourly: function() {
+
+  },
+
+  every10minutes: function() {
+
+  }
+
+}
+
 skiff.lift({
   start: function(argv) {
-    sails.log.info('interval: ' + argv.interval);
+    sails.log.info('running ' + argv.interval + ' job');
+    intervals[argv.interval]();
     skiff.lower();
   }
 });
