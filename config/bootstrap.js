65023e38a9e6d286c2e1e4b8c43feb0d0716ec84
Lawrence Wang <l@lw.io>
2015-02-18 14:44:31 -0800

    set up migrations

--- config/bootstrap.js
+++ config/bootstrap.js
@@ -16,27 +16,9 @@ var fs = require('fs'),
   root = require('root-path'),
   util = require('util');
 
-if (!process.env.DATABASE_URL) throw 'DATABASE_URL is not set';
-
-var url = require('url').parse(process.env.DATABASE_URL), user, password;
-if (url.auth) {
-  var i = url.auth.indexOf(':');
-  user = url.auth.slice(0, i);
-  password = url.auth.slice(i + 1);
-}
-
 module.exports.bootstrap = function(done) {
 
-  var knex = require('knex')({
-    client: 'pg',
-    connection: {
-      host: url.hostname,
-      port: url.port,
-      user: user,
-      password: password,
-      database: url.path.substring(1)
-    }
-  });
+  var knex = require('knex')(require('../knexfile')[process.env.NODE_ENV]);
 
   // log SQL queries
   if (sails.config.environment == 'development') {
---
+++ knexfile.js
@@ -0,0 +1,36 @@
+// Update with your config settings.
+
+require('dotenv').load();
+
+if (!process.env.DATABASE_URL) throw 'DATABASE_URL is not set';
+
+var url = require('url').parse(process.env.DATABASE_URL), user, password;
+if (url.auth) {
+  var i = url.auth.indexOf(':');
+  user = url.auth.slice(0, i);
+  password = url.auth.slice(i + 1);
+}
+
+var defaults = {
+  client: 'pg',
+  connection: {
+    host: url.hostname,
+    port: url.port,
+    user: user,
+    password: password,
+    database: url.path.substring(1)
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+};
+
+module.exports = {
+
+  development: defaults,
+
+  staging: defaults,
+
+  production: defaults
+
+};
