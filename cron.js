ba9d6d395293f4031718183aa0c1711a1c15d8e9
Lawrence Wang <l@lw.io>
2015-02-19 16:12:01 -0800

    fix cron
    make it wait for jobs to end before lowering sails

--- cron.js
+++ cron.js
@@ -1,16 +1,19 @@
 
 var skiff = require('./lib/skiff'), // this must be required first
+  format = require('util').format,
   moment = require('moment-timezone'),
   rollbar = skiff.rollbar,
   sails = skiff.sails,
-  Digest = require('./lib/community/digest');
+  Digest = require('./lib/community/digest'),
+  Promise = require('bluebird');
 
 require('colors');
 
-var intervals = {
+var jobs = {
 
   daily: function() {
     sails.log.debug('noop!');
+    return Promise.resolve(null);
   },
 
   hourly: function() {
@@ -19,27 +22,37 @@ var intervals = {
     switch (now.hour()) {
       case 12:
         sails.log.debug('Sending daily digests');
-        Digest.sendDaily();
-        break;
+        return Digest.sendDaily();
+      default:
+        return Promise.resolve(null);
     }
   },
 
   every10minutes: function() {
     sails.log.debug('noop!');
+    return Promise.resolve(null);
   }
 
 };
 
+var runJob = Promise.method(function(name) {
+  var job = jobs[name];
+  if (typeof(job) !== 'function') {
+    throw new Error(format('Unknown job name: "%s"', name));
+  }
+  sails.log.debug(format('Running %s job', name));
+  return job();
+});
+
 skiff.lift({
   start: function(argv) {
-    try {
-      sails.log.debug('running ' + argv.interval + ' job');
-      intervals[argv.interval]();
-    } catch(err) {
+    runJob(argv.interval).then(function() {
+      skiff.lower();
+    })
+    .catch(function(err) {
       sails.log.error(err.message.red);
       rollbar.handleError(err);
-    }
-
-    skiff.lower();
+      skiff.lower();
+    })
   }
 });
