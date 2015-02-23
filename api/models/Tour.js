e987deae3294d0c62a2fc22b0f7ec76febd91d62
Lawrence Wang <l@lw.io>
2015-02-23 14:53:23 -0800

    add Onboarding service
    with .resetForUser: sets can_skip_seed_form and new_user in tour.status

--- api/models/Tour.js
+++ api/models/Tour.js
@@ -8,11 +8,16 @@ module.exports = bookshelf.Model.extend({
 
 }, {
 
-  resetOnboarding: function(userId) {
+  // set opts.new_user to true or false depending on whether onboarding is
+  // being activated for a newly-created user or an existing one
+  resetOnboarding: function(userId, opts) {
     var tour = new Tour({
       user_id: userId,
       type: 'onboarding',
-      status: {step: 'start'},
+      status: _.merge(
+        _.pick(opts, 'new_user', 'can_skip_seed_form'),
+        {step: 'start'}
+      ),
       created_at: new Date()
     });
     return tour.save();
