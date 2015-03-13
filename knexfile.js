3c4095c8f4ba8f0fae0be6d67b53faa1f93cc64c
Lawrence Wang <l@lw.io>
2015-03-12 17:21:02 -0700

    skip DATABASE_URL processing in test env

--- knexfile.js
+++ knexfile.js
@@ -2,28 +2,32 @@
 
 require('dotenv').load();
 
-if (!process.env.DATABASE_URL) throw 'DATABASE_URL is not set';
+if (process.env.NODE_ENV != 'test') {
+  if (!process.env.DATABASE_URL) throw 'DATABASE_URL is not set';
 
-var url = require('url').parse(process.env.DATABASE_URL), user, password;
-if (url.auth) {
-  var i = url.auth.indexOf(':');
-  user = url.auth.slice(0, i);
-  password = url.auth.slice(i + 1);
-}
-
-var defaults = {
-  client: 'pg',
-  connection: {
-    host: url.hostname,
-    port: url.port,
-    user: user,
-    password: password,
-    database: url.path.substring(1)
-  },
-  migrations: {
-    tableName: 'knex_migrations'
+  var url = require('url').parse(process.env.DATABASE_URL), user, password;
+  if (url.auth) {
+    var i = url.auth.indexOf(':');
+    user = url.auth.slice(0, i);
+    password = url.auth.slice(i + 1);
   }
-};
+
+  var defaults = {
+    client: 'pg',
+    connection: {
+      host: url.hostname,
+      port: url.port,
+      user: user,
+      password: password,
+      database: url.path.substring(1)
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  };
+} else {
+  var defaults = {};
+}
 
 module.exports = {
 
