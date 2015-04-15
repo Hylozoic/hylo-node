57d5186853d45991ef8894841e462dcf28c1d4c1
Lawrence Wang <l@lw.io>
2015-04-14 17:35:54 -0700

    add segment tracking to emails

--- api/services/Analytics.js
+++ api/services/Analytics.js
@@ -1,4 +1,5 @@
 var sails = require('sails'),
+  uuid = require('node-uuid'),
   instance;
 
 if (process.env.NODE_ENV == 'test') {
@@ -10,5 +11,22 @@ if (process.env.NODE_ENV == 'test') {
 } else {
   instance = require('analytics-node')(process.env.SEGMENT_KEY);
 }
+
+instance.pixelUrl = function(emailName, props) {
+  var prefix = 'https://api.segment.io/v1/pixel/track?data=';
+
+  var data = {
+    writeKey: process.env.SEGMENT_KEY,
+    event: 'Viewed Email: ' + emailName,
+    properties: props
+  };
+
+  if (!data.userId) {
+    data.anonymousId = uuid.v4();
+  }
+
+  var encodedData = new Buffer(JSON.stringify(data), 'utf8').toString('base64');
+  return prefix + encodedData;
+};
 
 module.exports = instance;
