68cf63e1f4ff2141552f38962565a3a6ea2a15e0
Lawrence Wang <l@lw.io>
2015-04-16 20:12:18 -0700

    allow moderators to remove posts
    this was supposed to work before but wasn’t due to the assumption that
    communityId is set (which it is for the other use of
    checkAndSetWritablePost, but not deletion)

--- api/policies/checkAndSetWritablePost.js
+++ api/policies/checkAndSetWritablePost.js
@@ -2,15 +2,16 @@ var format = require('util').format;
 
 module.exports = function checkAndSetWritablePost(req, res, next) {
 
-  Post.find(req.param('postId'))
+  Post.find(req.param('postId'), {withRelated: [{communities: function(qb) { qb.column('id'); }}]})
   .tap(function(post) {
-    if (!post) throw new Error(format('Seed %s not found', req.param('postId')));
+    if (!post) throw new Error(format('post %s not found', req.param('postId')));
 
     if (Admin.isSignedIn(req) || post.get('creator_id') == req.session.userId)
-      return Promise.resolve(true);
+      return true;
 
-    return Membership.hasModeratorRole(req.session.userId, req.param('communityId')).then(function(isModerator) {
-      return (isModerator ? Promise.resolve() : Promise.reject());
+    var communityId = post.relations.communities.first().id;
+    return Membership.hasModeratorRole(req.session.userId, communityId).then(function(isModerator) {
+      if (!isModerator) throw 'not a moderator';
     });
   })
   .then(function(post) {
@@ -19,7 +20,7 @@ module.exports = function checkAndSetWritablePost(req, res, next) {
   })
   .catch(function(err) {
     sails.log.debug(format("Fail checkAndSetWritablePost policy %s %s: %s",
-      req.session.userId, req.param('postId'), err.message));
+      req.session.userId, req.param('postId'), err));
     res.forbidden();
   });
 };
