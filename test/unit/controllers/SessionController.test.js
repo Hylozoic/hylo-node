fbd763d642014e4a0da93317856b139997cc8043
Lawrence Wang <l@lw.io>
2015-04-20 16:58:31 -0700

    add a test for SessionController

---
+++ test/unit/controllers/SessionController.test.js
@@ -0,0 +1,52 @@
+var setup = require(require('root-path')('test/setup'));
+
+describe('SessionController', function() {
+
+  var req = {
+    session: {},
+    param: function(name) {
+      return this.params[name];
+    }
+  },
+    noop = function() { return function() {} },
+    res, cat;
+
+  describe('.create', function() {
+
+    before(function(done) {
+      _.extend(req, {
+        params: {
+          email: 'iam@cat.org',
+          password: 'password'
+        }
+      });
+
+      res = {
+        ok: spy(function() {}),
+        status: spy(function() { return this }),
+        send: noop()
+      };
+
+      cat = new User({name: 'Cat', email: 'iam@cat.org'});
+      cat.save().then(function() {
+        return new LinkedAccount({
+          provider_user_id: '$2a$10$UPh85nJvMSrm6gMPqYIS.OPhLjAMbZiFnlpjq1xrtoSBTyV6fMdJS',
+          provider_key: 'password',
+          user_id: cat.id
+        }).save();
+      }).exec(done);
+    });
+
+    it('works with a valid username and password', function(done) {
+      SessionController.create(req, res).then(function() {
+        expect(res.status).not.to.have.been.called();
+        expect(res.ok).to.have.been.called();
+        expect(req.session.userId).to.equal(cat.id);
+        done();
+      })
+      .catch(done);
+    })
+
+  })
+
+})
