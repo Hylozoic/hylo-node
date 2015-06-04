6f4b04de10cda257381be7ea2eb0662ac6a188f2
Lawrence Wang <l@lw.io>
2015-06-03 19:24:25 -0700

    promisify PushNotification.send
    and remove a few stray console.logs

--- api/models/PushNotification.js
+++ api/models/PushNotification.js
@@ -1,32 +1,25 @@
 var format = require('util').format,
-    ZeroPush = require("nzero-push");
+  Promise = require('bluebird'),
+  ZeroPush = require("nzero-push");
 
 module.exports = bookshelf.Model.extend({
   tableName: 'push_notifications',
 
   send: function() {
 
-    console.log("pushNotification.send()");
-    
-    var zeroPush = new ZeroPush(process.env.ZEROPUSH_SERVER_TOKEN);
-    var platform = "ios_macos";
-    var deviceTokens = [ this.get("device_token")];
-    var notification = {
-      alert: this.get("alert"),
-      info: JSON.parse(this.get("payload")),
-      badge: "+1"
-    };
+    var zeroPush = new ZeroPush(process.env.ZEROPUSH_SERVER_TOKEN),
+      notify = Promise.promisify(zeroPush.notify, zeroPush),
+      platform = "ios_macos",
+      deviceTokens = [this.get("device_token")],
+      notification = {
+        alert: this.get("alert"),
+        info: JSON.parse(this.get("payload")),
+        badge: "+1"
+      };
 
     this.set("time_sent", (new Date()).toISOString());
-    this.save()
-      .then(pn => {
-        zeroPush.notify(platform, deviceTokens, notification, function (err, response) {
-          if (err)
-            return console.error(err);
-
-          return console.log(response);
-        });
-      });        
+    return this.save()
+    .then(pn => notify(platform, deviceTokens, notification));
   }
-    
+
 });
