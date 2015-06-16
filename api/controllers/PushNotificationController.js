ef12da7ab8d4bfa2a9deec3db5feb20fb3b89f12
Robbie Carlton <robbie.carlton@gmail.com>
2015-06-16 13:45:46 -0700

    catch errors in PushNotificationController.js

--- api/controllers/PushNotificationController.js
+++ api/controllers/PushNotificationController.js
@@ -11,47 +11,61 @@ module.exports = {
    * `PushNotificationController.addDevice()`
    */
   addDevice: function (req, res) {
-    if (req.session.userId && req.param("deviceToken")) {
-      Device.forge({
-        token: req.param("deviceToken"),
-        user_id: req.session.userId
-      })
-      .fetch()
-      .then(function (device) {
-        if(!device) {
-          Device.forge({
-            token: req.param("deviceToken"),
-            user_id: req.session.userId
-          })
-          .save()
-          .then(function(device) {
-            res.ok({result: "Added"});
-          })
-          .catch(function (e) {
-            res.serverError(e);
-	  })
-        };          
-      });
-    };
+    if (req.session.userId) {
+      if (req.param("deviceToken")) {
+        Device.forge({
+          token: req.param("deviceToken"),
+          user_id: req.session.userId
+        })
+        .fetch()
+        .then(function (device) {
+          if(!device) {
+            Device.forge({
+              token: req.param("deviceToken"),
+              user_id: req.session.userId
+            })
+            .save()
+            .then(function(device) {
+              res.ok({result: "Added"});
+            })
+            .catch(function (e) {
+              res.serverError(e);
+	    });
+          } else {
+            res.ok({result: "Known"});          
+          };          
+        });
+      } else {
+        res.serverError(new Error('Missing param: deviceToken'))
+      };
+    } else {
+      res.forbidden()
+    }
   },
   
   updateBadgeNo: function (req, res) {
-    if (req.session.userId && req.param("deviceToken")) {
-      Device.forge({
-        token: req.param("deviceToken"),
-        user_id: req.session.userId
-      })
-      .fetch()
-      .then(function (device) {
-        device.save({
-          badge_no: req.param("badgeNo") || 0
+    if (req.session.userId) {
+      if (req.param("deviceToken")) {
+        Device.forge({
+          token: req.param("deviceToken"),
+          user_id: req.session.userId
+        })
+        .fetch()
+        .then(function (device) {
+          device.save({
+            badge_no: req.param("badgeNo") || 0
+          });
+        })
+        .then(() => res.ok({result: "Updated"}))
+        .catch(function (e) {
+          res.serverError(e);          
         });
-      })
-      .then(() => res.ok({result: "Updated"}))
-      .catch(function (e) {
-        res.serverError(e);          
-      });
-    };
+      } else {
+        res.serverError(new Error('Missing param: deviceToken'))
+      }              
+    } else {
+      res.forbidden()
+    }
   }     
   
 };
