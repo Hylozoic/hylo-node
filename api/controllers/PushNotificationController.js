bf1b9a702d126847c5c114fbae30a72591eb4bc4
Robbie Carlton <robbie.carlton@gmail.com>
2015-06-16 15:37:47 -0700

    move authorization into policies.js

--- api/controllers/PushNotificationController.js
+++ api/controllers/PushNotificationController.js
@@ -11,61 +11,55 @@ module.exports = {
    * `PushNotificationController.addDevice()`
    */
   addDevice: function (req, res) {
-    if (req.session.userId) {
-      if (req.param("deviceToken")) {
+    var token = req.param('deviceToken');
+    if (!token) {
+      return res.serverError('no device token');
+    }
+
+    Device.forge({
+      token: req.param("deviceToken"),
+      user_id: req.session.userId
+    })
+    .fetch()
+    .then(function (device) {
+      if(!device) {
         Device.forge({
           token: req.param("deviceToken"),
           user_id: req.session.userId
         })
-        .fetch()
-        .then(function (device) {
-          if(!device) {
-            Device.forge({
-              token: req.param("deviceToken"),
-              user_id: req.session.userId
-            })
-            .save()
-            .then(function(device) {
-              res.ok({result: "Added"});
-            })
-            .catch(function (e) {
-              res.serverError(e);
-	    });
-          } else {
-            res.ok({result: "Known"});          
-          };          
-        });
+        .save()
+        .then(function(device) {
+          res.ok({result: "Added"});
+        })
+        .catch(function (e) {
+          res.serverError(e);
+	});
       } else {
-        res.serverError(new Error('Missing param: deviceToken'))
-      };
-    } else {
-      res.forbidden()
-    }
+        res.ok({result: "Known"});          
+      };          
+    });
   },
   
   updateBadgeNo: function (req, res) {
-    if (req.session.userId) {
-      if (req.param("deviceToken")) {
-        Device.forge({
-          token: req.param("deviceToken"),
-          user_id: req.session.userId
-        })
-        .fetch()
-        .then(function (device) {
-          device.save({
-            badge_no: req.param("badgeNo") || 0
-          });
-        })
-        .then(() => res.ok({result: "Updated"}))
-        .catch(function (e) {
-          res.serverError(e);          
-        });
-      } else {
-        res.serverError(new Error('Missing param: deviceToken'))
-      }              
-    } else {
-      res.forbidden()
+    var token = req.param('deviceToken');
+    if (!token) {
+      return res.serverError('no device token');
     }
+    
+    Device.forge({
+      token: token,
+      user_id: req.session.userId
+    })
+    .fetch()
+    .then(function (device) {
+      device.save({
+        badge_no: req.param("badgeNo") || 0
+      });
+    })
+    .then(() => res.ok({result: "Updated"}))
+    .catch(function (e) {
+      res.serverError(e);          
+    });
   }     
   
 };
