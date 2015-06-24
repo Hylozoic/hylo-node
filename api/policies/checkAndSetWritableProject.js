b4856415aaa9f99eed8521bf83a91e37898b2495
Lawrence Wang <l@lw.io>
2015-06-23 18:28:13 -0700

    allow admins to edit projects

--- api/policies/checkAndSetWritableProject.js
+++ api/policies/checkAndSetWritableProject.js
@@ -1,7 +1,7 @@
 module.exports = function(req, res, next) {
 
   Project.find(req.param('projectId')).then(function(project) {
-    if (req.session.userId === project.get('user_id')) {
+    if (req.session.userId === project.get('user_id') || Admin.isSignedIn(req)) {
       res.locals.project = project;
       next();
     } else {
