928b8fac7013704ff95b8bb64aa91f6f32bdb4a2
Robbie Carlton <robbie.carlton@gmail.com>
2015-07-06 13:54:19 -0700

    cleanup

--- api/controllers/PushNotificationController.js
+++ api/controllers/PushNotificationController.js
@@ -39,14 +39,11 @@ module.exports = {
   },
 
   disableDevice: function (req, res) {
-    sails.log("disableDevice");
     var token = req.param('deviceToken');
     if (!token) {
       return res.serverError('no device token');
     }
 
-    sails.log("token" + token);
-    
     return Device.forge({
       token: token,
       user_id: req.session.userId
@@ -68,7 +65,7 @@ module.exports = {
       user_id: req.session.userId
     })
     .fetch()
-      .then(device => device.save({badge_no: req.param("badgeNo") || 0}))
+    .then(device => device.save({badge_no: req.param("badgeNo") || 0}))
     .then(() => res.ok({result: "Updated"}))
     .catch(res.serverError);
   }
