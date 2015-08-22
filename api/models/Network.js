01b52c06d5e0ebd2b502e234e8a164ed2a535b30
Lawrence Wang <l@lw.io>
2015-08-21 23:47:21 -0700

    standardize more

--- api/models/Network.js
+++ api/models/Network.js
@@ -1,11 +1,11 @@
-var knex = bookshelf.knex;
+var knex = bookshelf.knex
 
-var networkIdsQuery = function(userId) {
+var networkIdsQuery = function (userId) {
   var communityIdsQuery = knex.select('community_id').from('users_community')
-  .where({user_id: userId, active: true});
+    .where({user_id: userId, active: true})
 
   return knex.select().distinct('network_id').from('community')
-  .whereIn('id', communityIdsQuery).whereRaw('network_id is not null');
+    .whereIn('id', communityIdsQuery).whereRaw('network_id is not null')
 }
 
 module.exports = bookshelf.Model.extend({
@@ -14,30 +14,29 @@ module.exports = bookshelf.Model.extend({
   communities: () => this.hasMany(Community)
 
 }, {
-
-  find: function(idOrSlug, options) {
+  find: function (idOrSlug, options) {
     if (isNaN(Number(idOrSlug))) {
-      return this.where({slug: idOrSlug}).fetch(options);
+      return this.where({slug: idOrSlug}).fetch(options)
     }
-    return this.where({id: idOrSlug}).fetch(options);
+    return this.where({id: idOrSlug}).fetch(options)
   },
 
-  containsUser: function(networkId, userId) {
+  containsUser: function (networkId, userId) {
     return this.idsForUser(userId)
-    .then(ids => _.contains(ids, networkId.toString()));
+      .then(ids => _.contains(ids, networkId.toString()))
   },
 
-  activeCommunityIds: function(userId, rawQuery) {
+  activeCommunityIds: function (userId, rawQuery) {
     var query = knex.select('id').from('community')
-    .whereIn('network_id', networkIdsQuery(userId));
+      .whereIn('network_id', networkIdsQuery(userId))
 
-    if (rawQuery) return query;
-    return query.then(rows => _.pluck(rows, 'id'));
+    if (rawQuery) return query
+    return query.then(rows => _.pluck(rows, 'id'))
   },
 
-  idsForUser: function(userId) {
+  idsForUser: function (userId) {
     return networkIdsQuery(userId)
-    .then(rows => _.pluck(rows, 'network_id'));
+      .then(rows => _.pluck(rows, 'network_id'))
   }
 
-});
+})
