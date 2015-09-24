12f0762c7c9994f4e34821f9afe70fa819cdeae7
Lawrence Wang <l@lw.io>
2015-09-23 18:15:33 -0700

    standardize formatting

--- api/models/Media.js
+++ api/models/Media.js
@@ -1,18 +1,18 @@
 module.exports = bookshelf.Model.extend({
   tableName: 'media',
 
-  post: function() {
-    return this.belongsTo(Post);
+  post: function () {
+    return this.belongsTo(Post)
   }
 
 }, {
 
-  create: function(opts) {
+  create: function (opts) {
     return new Media({
       post_id: opts.postId,
       url: opts.url,
       type: opts.type
-    }).save(null, _.pick(opts, 'transacting'));
+    }).save(null, _.pick(opts, 'transacting'))
   }
 
-});
+})
