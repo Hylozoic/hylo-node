96d86826a23c76bf2f8c516a0422eaffbf106016
Robbie Carlton <robbie.carlton@gmail.com>
2015-10-06 11:25:48 -0700

    fix accidental regression

--- api/controllers/PostController.js
+++ api/controllers/PostController.js
@@ -58,7 +58,7 @@ var afterSavingPost = function (post, opts) {
     // Add image, if any
     (opts.imageUrl && Media.createImage(post.id, opts.imageUrl, opts.transacting)),
 
-    (opts.docs && Promise.map(opts.docs, doc => createDoc(post.id, doc, opts.transacting))),
+    (opts.docs && Promise.map(opts.docs, doc => Media.createDoc(post.id, doc, opts.transacting))),
 
     Queue.classMethod('Post', 'sendPushNotifications', {postId: post.id})
 
