da935ace21637b126c0040df9c70dbd96caa1b7f
Lawrence Wang <l@lw.io>
2015-10-06 17:14:24 -0700

    stop expecting daily_digest in UserController

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -161,7 +161,7 @@ module.exports = {
   update: function (req, res) {
     var attrs = _.pick(req.allParams(), [
       'name', 'bio', 'avatar_url', 'banner_url', 'twitter_name', 'linkedin_url', 'facebook_url',
-      'email', 'send_email_preference', 'daily_digest', 'work', 'intention', 'extra_info',
+      'email', 'send_email_preference', 'work', 'intention', 'extra_info',
       'new_notification_count', 'settings'
     ])
 
