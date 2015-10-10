3453db1200150c30cff67f75c98bd98d63349759
Lawrence Wang <l@lw.io>
2015-10-09 17:02:25 -0700

    cleanup

--- api/models/Follow.js
+++ api/models/Follow.js
@@ -1,20 +1,20 @@
 module.exports = bookshelf.Model.extend({
   tableName: 'follower',
 
-  post: function() {
+  post: function () {
     return this.belongsTo(Post)
   },
 
-  user: function() {
+  user: function () {
     return this.belongsTo(User).query({where: {active: true}})
   }
 }, {
-  create: function(postId, options) {
+  create: function (postId, options) {
     return new Follow({
       post_id: postId,
       date_added: new Date(),
       user_id: options.followerId,
       added_by_id: options.addedById
-    }).save(null, _.pick(options, "transacting"))
+    }).save(null, _.pick(options, 'transacting'))
   }
 })
