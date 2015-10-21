943a7e930450c94e42ef121823e80350ec5f6a8d
Robbie Carlton <robbie.carlton@gmail.com>
2015-10-21 13:57:14 -0700

    wip updating badge no correctly

--- api/models/Device.js
+++ api/models/Device.js
@@ -1,32 +1,30 @@
-var format = require('util').format;
-var apn = require('apn');
-
 module.exports = bookshelf.Model.extend({
   tableName: 'devices',
 
   user: function () {
-    return this.belongsTo(User, "user_id");
+    return this.belongsTo(User, 'user_id')
   },
 
-  sendPushNotification: function(alert, path, options) {
-
-    if (!this.get("enabled"))
-      return;
-
-    var badge_no = this.get('badge_no') + 1;
-    this.set("badge_no", badge_no);
-    return this.save({}, options)
-      .then(function (device) {
-        return PushNotification.forge({
-          device_token: device.get('token'),
-          alert: alert,
-          path: path,
-          badge_no: badge_no,
-          platform: this.get("platform"),
-          time_queued: (new Date()).toISOString()
-        })
-        .save({}, options)
-        .then(pushNotification => pushNotification.send(options));
-      });
+  sendPushNotification: function (alert, path, options) {
+    var device = this
+    if (!this.get('enabled')) {
+      return
+    }
+    sails.log.debug("about to go into the promise")
+    User.find(this.get('user_id'), options)
+    .then(function (user) {
+      sails.log.debug("made it into the promise,", user)
+      var badge_no = user.get('new_notification_count')
+      return PushNotification.forge({
+        device_token: device.get('token'),
+        alert: alert,
+        path: path,
+        badge_no: badge_no,
+        platform: device.get('platform'),
+        time_queued: (new Date()).toISOString()
+      })
+      .save({}, options)
+      .then(pushNotification => pushNotification.send(options))
+    })
   }
-});
+})
