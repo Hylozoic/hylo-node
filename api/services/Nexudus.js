6b11d3d6961741b062795d681afd91271eecdf9d
Lawrence Wang <l@lw.io>
2015-10-23 14:00:48 -0700

    revise Nexudus code
    fetch only members, which are users that are active and have an active
    contract

--- api/services/Nexudus.js
+++ api/services/Nexudus.js
@@ -5,30 +5,62 @@ var get = Promise.promisify(request.get, request)
 const apiHost = 'https://spaces.nexudus.com/api'
 const pageSize = 500
 
-var consumePage = function (num, username, password) {
-  return get({
-    url: format('%s/sys/users?size=%s&page=%s', apiHost, pageSize, num),
-    json: true,
-    auth: {user: username, pass: password}
-  })
-  .spread((res2, body) => {
-    if (res2.statusCode !== 200) {
-      throw new Error(res2.statusCode)
-    }
+var API = function (username, password) {
+  this.username = username
+  this.password = password
+}
 
-    if (body.CurrentPage < body.TotalPages) {
-      return body.Records.concat(consumePage(num + 1, username, password))
-    } else {
-      return body.Records
-    }
-  })
+API.prototype.getRecords = function (path, query) {
+  var self = this
+  var getPage = function (pageNum) {
+    var url = format('%s/%s', apiHost, path)
+    console.log(url)
+    return get({
+      url: url,
+      json: true,
+      auth: {user: self.username, pass: self.password},
+      qs: _.merge({
+        size: pageSize,
+        page: pageNum
+      }, query)
+    })
+    .spread((res2, body) => {
+      if (res2.statusCode !== 200) {
+        throw new Error(res2.statusCode)
+      }
+
+      if (body.CurrentPage < body.TotalPages) {
+        return body.Records.concat(getPage(pageNum + 1))
+      } else {
+        return body.Records
+      }
+    })
+  }
+
+  return getPage(1)
 }
 
 module.exports = {
-  fetchUsers: function (username, password) {
+  // members are users who are active and have an active contract
+  fetchMembers: function (username, password) {
     var self = this
-    return consumePage(1, username, password)
-    .then(records => records.map(self.formatRecord))
+    var api = new API(username, password)
+    var r1, r2
+    return api.getRecords('billing/coworkercontracts', {CoworkerContract_Active: true})
+    .then(records => {
+      console.log(format('got %s coworker contracts', records.length))
+      r1 = records
+      var coworkerIds = records.map(r => r.CoworkerId)
+      return api.getRecords('spaces/coworkers', {Coworker_Active: true})
+      .tap(records => {
+        console.log(format('got %s coworkers', records.length))
+        r2 = records
+      })
+      .then(records => records
+        .filter(r => _.contains(coworkerIds, r.Id))
+        .map(self.formatRecord))
+    })
+    .then(formattedRecords => [r1, r2, formattedRecords])
   },
 
   formatRecord: record => ({
