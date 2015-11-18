0503ecff3d85f5bd29409f2f873ae40b7c2ad1e5
Robbie Carlton <robbie.carlton@gmail.com>
2015-11-18 15:44:55 -0800

    handle https in getIMageSize

--- api/services/GetImageSize.js
+++ api/services/GetImageSize.js
@@ -2,11 +2,13 @@ var imageSize = require('image-size')
 var Promise = require('bluebird')
 var url = require('url')
 var http = require('http')
+var https = require('https')
 
 module.exports = function (image_url) {
   return new Promise(function (resolve, reject) {
     var options = url.parse(image_url)
-    http.get(options, function (response) {
+    var protocol = (options.protocol === 'https:' ? https : http)
+    protocol.get(options, function (response) {
       var chunks = []
       response.on('data', function (chunk) {
         chunks.push(chunk)
