339484ee986a37e215d99bb15c9eecde2caf78f5
Robbie Carlton <robbie.carlton@gmail.com>
2015-11-20 15:44:03 -0800

    remove test

--- test/unit/services/GetImageSize.test.js
+++ test/unit/services/GetImageSize.test.js
@@ -3,15 +3,7 @@ require(root('test/setup'))
 var GetImageSize = require(root('api/services/GetImageSize'))
 
 describe('GetImageSize', () => {
-  it('gets the size when http', () => {
-    return GetImageSize('http://i.huffpost.com/gen/672963/images/r-610894252-large570.jpg')
-    .then(dimensions => {
-      expect(dimensions.width).to.equal(570)
-      expect(dimensions.height).to.equal(238)
-    })
-  })
-
-  it('gets the size when https', () => {
+  it('gets the size', () => {
     return GetImageSize('https://www.hylo.com/img/smallh.png')
     .then(dimensions => {
       expect(dimensions.width).to.equal(144)
