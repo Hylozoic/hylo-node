e1a2bc49810aba22aa7ef4d1f3cffa9ebf7a8f0a
Lawrence Wang <l@lw.io>
2015-11-20 13:04:16 -0800

    standardize

--- app.js
+++ app.js
@@ -18,58 +18,57 @@
  * `node app.js --silent --port=80 --prod`
  */
 
-require('dotenv').load();
+require('dotenv').load()
 
 if (process.env.NEW_RELIC_LICENSE_KEY) {
-  require('newrelic');
+  require('newrelic')
 }
 
-var _ = require('lodash');
-require('colors');
+var _ = require('lodash')
+require('colors')
 
 // Ensure we're in the project directory, so relative paths work as expected
 // no matter where we actually lift from.
-process.chdir(__dirname);
+process.chdir(__dirname)
 
 // Ensure a "sails" can be located:
-(function() {
-  var sails;
+;(function () {
+  var sails
   try {
-    sails = require('sails');
+    sails = require('sails')
   } catch (e) {
-    console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.');
-    console.error('To do that, run `npm install sails`');
-    console.error('');
-    console.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.');
-    console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,');
-    console.error('but if it doesn\'t, the app will run with the global sails instead!');
-    return;
+    console.error('To run an app using `node app.js`, you usually need to have a version of `sails` installed in the same directory as your app.')
+    console.error('To do that, run `npm install sails`')
+    console.error('')
+    console.error('Alternatively, if you have sails installed globally (i.e. you did `npm install -g sails`), you can use `sails lift`.')
+    console.error('When you run `sails lift`, your app will still use a local `./node_modules/sails` dependency if it exists,')
+    console.error("but if it doesn't, the app will run with the global sails instead!")
+    return
   }
 
   // Try to get `rc` dependency
-  var rc;
+  var rc
   try {
-    rc = require('rc');
+    rc = require('rc')
   } catch (e0) {
     try {
-      rc = require('sails/node_modules/rc');
+      rc = require('sails/node_modules/rc')
     } catch (e1) {
-      console.error('Could not find dependency: `rc`.');
-      console.error('Your `.sailsrc` file(s) will be ignored.');
-      console.error('To resolve this, run:');
-      console.error('npm install rc --save');
-      rc = function () { return {}; };
+      console.error('Could not find dependency: `rc`.')
+      console.error('Your `.sailsrc` file(s) will be ignored.')
+      console.error('To resolve this, run:')
+      console.error('npm install rc --save')
+      rc = () => {}
     }
   }
 
-
   // Start server
-  sails.log.info('Lifting...'.yellow);
+  sails.log.info('Lifting...'.yellow)
   sails.lift(_.merge(rc('sails'), {
     log: {noShip: true}
-  }), function(err) {
-    sails.log.info('Aloft.'.blue);
-  });
+  }), function (err) {
+    sails.log.info('Aloft.'.blue)
+  })
 
-  module.exports = sails;
-})();
+  module.exports = sails
+})()
--- config/bootstrap.js
+++ config/bootstrap.js
@@ -9,73 +9,72 @@
  * http://sailsjs.org/#/documentation/reference/sails.config/sails.config.bootstrap.html
  */
 
-require('dotenv').load();
+require('dotenv').load()
 
-var fs = require('fs'),
-  path = require('path'),
-  root = require('root-path'),
-  util = require('util');
+var fs = require('fs')
+var path = require('path')
+var root = require('root-path')
+var util = require('util')
 
 // very handy, these
-global.format = util.format;
-global.Promise = require('bluebird');
-global._ = require('lodash'); // override Sails' old version of lodash
+global.format = util.format
+global.Promise = require('bluebird')
+global._ = require('lodash') // override Sails' old version of lodash
 
-module.exports.bootstrap = function(done) {
-
-  var knex = require('knex')(require('../knexfile')[process.env.NODE_ENV]);
+module.exports.bootstrap = function (done) {
+  var knex = require('knex')(require('../knexfile')[process.env.NODE_ENV])
 
   // log SQL queries
   if (process.env.DEBUG_SQL) {
-    require('colors');
-    knex.on('query', function(data) {
-      var args = (_.clone(data.bindings) || []).map(function(s) {
-        if (s === null) return 'null'.blue;
-        if (s === undefined) return 'undefined'.red;
-        if (typeof(s) === 'object') return JSON.stringify(s).blue;
-        return s.toString().blue;
-      });
-      args.unshift(data.sql.replace(/\?/g, '%s'));
+    require('colors')
+    knex.on('query', function (data) {
+      var args = (_.clone(data.bindings) || []).map(function (s) {
+        if (s === null) return 'null'.blue
+        if (s === undefined) return 'undefined'.red
+        if (typeof (s) === 'object') return JSON.stringify(s).blue
+        return s.toString().blue
+      })
+      args.unshift(data.sql.replace(/\?/g, '%s'))
 
       // TODO fix missing limit and boolean values
       var query = util.format.apply(util, args)
-        .replace(/^(select|insert|update)/, '$1'.yellow);
+        .replace(/^(select|insert|update)/, '$1'.yellow)
 
-      sails.log.info(query);
-    });
+      sails.log.info(query)
+    })
   }
 
-  if (sails.config.environment == 'production') {
-    var rollbar = require('rollbar');
+  if (sails.config.environment === 'production') {
+    var rollbar = require('rollbar')
 
-    knex.on('query', function(data) {
+    knex.on('query', function (data) {
       if (_.contains(data.bindings, 'undefined')) {
         rollbar.handleError('undefined value in SQL query', {
           sql: data.sql,
           bindings: data.bindings
-        });
+        })
       }
-    });
+    })
   }
 
   // add bookshelf and each model to global namespace
-  global.bookshelf = require('bookshelf')(knex);
-  _.each(fs.readdirSync(root('api/models')), function(filename) {
-    if (path.extname(filename) == '.js') {
-      var modelName = path.basename(filename, '.js');
-      global[modelName] = require(root('api/models/' + modelName));
+  global.bookshelf = require('bookshelf')(knex)
+  _.each(fs.readdirSync(root('api/models')), function (filename) {
+    if (path.extname(filename) === '.js') {
+      var modelName = path.basename(filename, '.js')
+      global[modelName] = require(root('api/models/' + modelName))
     }
-  });
+  })
 
   // add presenters to global namespace
-  _.each(fs.readdirSync(root('api/presenters')), function(filename) {
-    if (path.extname(filename) == '.js') {
-      var modelName = path.basename(filename, '.js');
-      global[modelName] = require(root('api/presenters/' + modelName));
+  _.each(fs.readdirSync(root('api/presenters')), function (filename) {
+    if (path.extname(filename) === '.js') {
+      var modelName = path.basename(filename, '.js')
+      global[modelName] = require(root('api/presenters/' + modelName))
     }
-  });
+  })
 
   // It's very important to trigger this callback method when you are finished
   // with the bootstrap!  (otherwise your server will never lift, since it's waiting on the bootstrap)
-  done();
-};
+  done()
+}
