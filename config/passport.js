2895acec3704e4cd391c201d1db2b247c8d25ba5
Lawrence Wang <l@lw.io>
2015-11-30 17:29:48 -0800

    standardize formatting

--- config/passport.js
+++ config/passport.js
@@ -1,41 +1,39 @@
+var passport = require('passport')
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy
+var GoogleTokenStrategy = require('passport-google-token').Strategy
+var FacebookStrategy = require('passport-facebook').Strategy
+var FacebookTokenStrategy = require('passport-facebook-token').Strategy
+var LinkedinStrategy = require('passport-linkedin-oauth2').Strategy
+var LinkedInTokenStrategy = require('passport-linkedin-token-oauth2').Strategy
 
-var passport = require('passport'),
-  GoogleStrategy = require('passport-google-oauth').OAuth2Strategy,
-  GoogleTokenStrategy = require('passport-google-token').Strategy,
-  FacebookStrategy = require('passport-facebook').Strategy,
-  FacebookTokenStrategy = require('passport-facebook-token').Strategy,
-  LinkedinStrategy = require('passport-linkedin-oauth2').Strategy;
-  LinkedInTokenStrategy = require('passport-linkedin-token-oauth2').Strategy;
-
-/////////////////////////////
+// -----------
 // admin login
 
 var adminStrategy = new GoogleStrategy({
   clientID: process.env.ADMIN_GOOGLE_CLIENT_ID,
   clientSecret: process.env.ADMIN_GOOGLE_CLIENT_SECRET,
   callbackURL: format('%s://%s%s', process.env.PROTOCOL, process.env.DOMAIN, '/noo/admin/login/oauth')
-}, function(accessToken, refreshToken, profile, done) {
-  var email = profile.emails[0].value;
+}, function (accessToken, refreshToken, profile, done) {
+  var email = profile.emails[0].value
 
   if (email.match(/hylo\.com$/)) {
-    done(null, {email: email});
+    done(null, {email: email})
   } else {
-    done(null, false, {message: "Not a hylo.com address."});
+    done(null, false, {message: 'Not a hylo.com address.'})
   }
-});
-adminStrategy.name = 'admin';
-passport.use(adminStrategy);
+})
+adminStrategy.name = 'admin'
+passport.use(adminStrategy)
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
-});
+passport.serializeUser(function (user, done) {
+  done(null, user)
+})
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
-});
+passport.deserializeUser(function (user, done) {
+  done(null, user)
+})
 
-
-/////////////////////////////
+// -----------
 // user login
 //
 // doesn't use the serialize and deserialize handlers above
@@ -47,11 +45,11 @@ passport.deserializeUser(function(user, done) {
 // use req.login to set req.user, and only the admin login is unconventional
 //
 
-var url = function(path) {
-  return format('%s://%s%s', process.env.PROTOCOL, process.env.DOMAIN, path);
-};
+var url = function (path) {
+  return format('%s://%s%s', process.env.PROTOCOL, process.env.DOMAIN, path)
+}
 
-var formatProfile = function(profile, accessToken, refreshToken) {
+var formatProfile = function (profile, accessToken, refreshToken) {
   return _.merge(profile, {
     name: profile.displayName,
     email: profile.emails[0].value,
@@ -59,43 +57,43 @@ var formatProfile = function(profile, accessToken, refreshToken) {
       access_token: accessToken,
       refresh_token: refreshToken
     }
-  });
-};
+  })
+}
 
 var googleStrategy = new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
   callbackURL: url('/noo/login/google/oauth')
-}, function(accessToken, refreshToken, profile, done) {
-  done(null, formatProfile(profile));
-});
-passport.use(googleStrategy);
+}, function (accessToken, refreshToken, profile, done) {
+  done(null, formatProfile(profile))
+})
+passport.use(googleStrategy)
 
 var facebookStrategy = new FacebookStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET,
   callbackURL: url('/noo/login/facebook/oauth'),
   scope: ['public_profile', 'email', 'user_friends', 'user_about_me', 'user_likes', 'user_location']
-}, function(accessToken, refreshToken, profile, done) {
-  done(null, formatProfile(profile, accessToken, refreshToken));
-});
-passport.use(facebookStrategy);
+}, function (accessToken, refreshToken, profile, done) {
+  done(null, formatProfile(profile, accessToken, refreshToken))
+})
+passport.use(facebookStrategy)
 
 var facebookTokenStrategy = new FacebookTokenStrategy({
   clientID: process.env.FACEBOOK_APP_ID,
   clientSecret: process.env.FACEBOOK_APP_SECRET
-}, function(accessToken, refreshToken, profile, done) {
-  done(null, formatProfile(profile, accessToken, refreshToken));
-});
-passport.use(facebookTokenStrategy);
+}, function (accessToken, refreshToken, profile, done) {
+  done(null, formatProfile(profile, accessToken, refreshToken))
+})
+passport.use(facebookTokenStrategy)
 
 var googleTokenStrategy = new GoogleTokenStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET
-}, function(accessToken, refreshToken, profile, done) {
-  done(null, formatProfile(profile));
-});
-passport.use(googleTokenStrategy);
+}, function (accessToken, refreshToken, profile, done) {
+  done(null, formatProfile(profile))
+})
+passport.use(googleTokenStrategy)
 
 var linkedinStrategy = new LinkedinStrategy({
   clientID: process.env.LINKEDIN_API_KEY,
@@ -103,15 +101,15 @@ var linkedinStrategy = new LinkedinStrategy({
   callbackURL: url('/noo/login/linkedin/oauth'),
   scope: ['r_emailaddress', 'r_basicprofile'],
   state: true
-}, function(accessToken, refreshToken, profile, done) {
-  done(null, formatProfile(profile));
-});
-passport.use(linkedinStrategy);
+}, function (accessToken, refreshToken, profile, done) {
+  done(null, formatProfile(profile))
+})
+passport.use(linkedinStrategy)
 
 var linkedinTokenStrategy = new LinkedInTokenStrategy({
   clientID: process.env.LINKEDIN_API_KEY,
-  clientSecret: process.env.LINKEDIN_API_SECRET,
-}, function(accessToken, refreshToken, profile, done) {
-  done(null, formatProfile(profile));
-});
-passport.use(linkedinTokenStrategy);
+  clientSecret: process.env.LINKEDIN_API_SECRET
+}, function (accessToken, refreshToken, profile, done) {
+  done(null, formatProfile(profile))
+})
+passport.use(linkedinTokenStrategy)
