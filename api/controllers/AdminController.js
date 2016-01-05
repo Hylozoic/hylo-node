43282f3a13ef38ca414be159c4e3e435604fdafe
Lawrence Wang <l@lw.io>
2016-01-05 13:04:34 -0800

    standardize

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -1,19 +1,19 @@
-var heredoc = require('heredoc'),
-  moment = require('moment');
+var heredoc = require('heredoc')
+var moment = require('moment')
 
-var sanitizeForJSON = function(str){
-  return str.replace(/\\/g, "\\\\")
-  .replace(/\n/g, "\\n")
-  .replace(/\r/g, "\\r")
-  .replace(/\t/g, "\\t")
-  .replace(/\f/g, "\\f")
-  .replace(/"/g,"\\\"")
-  .replace(/'/g,"\\\'")
-  .replace(/\&/g, "\\&");
-};
+var sanitizeForJSON = function (str) {
+  return str.replace(/\\/g, '\\\\')
+  .replace(/\n/g, '\\n')
+  .replace(/\r/g, '\\r')
+  .replace(/\t/g, '\\t')
+  .replace(/\f/g, '\\f')
+  .replace(/"/g, '\\"')
+  .replace(/'/g, "\\'")
+  .replace(/\&/g, '\\&')
+}
 
-var nvd3Format = function(data, times) {
-  var maxSeries;
+var nvd3Format = function (data, times) {
+  var maxSeries
   return _(data).keys()
   .tap(keys => maxSeries = Math.min(keys.length, 19))
   .sortByAll([key => -1 * _.sum(_.values(data[key])), _.identity])
@@ -22,82 +22,84 @@ var nvd3Format = function(data, times) {
       result.push({
         key: sanitizeForJSON(name),
         values: _.map(times, t => [Number(t), data[name][t] || 0])
-      });
+      })
     } else if (index === maxSeries) {
-      var otherNames = keys.slice(index, keys.length);
+      var otherNames = keys.slice(index, keys.length)
       result.push({
         key: format('Other (%s)', otherNames.length),
         values: _.map(times, t => [Number(t), _.sum(otherNames, name => data[name][t] || 0)])
-      });
+      })
     }
-    return result;
-  }, []);
-};
+    return result
+  }, [])
+}
 
-var countNew = function(model, interval, unit) {
-  var now = moment(),
-    then = now.clone().subtract(interval, unit),
-    data = {},
-    times = {},
-    withRelated = (model === Comment ? ['post.communities'] : ['communities']);
+var countNew = function (model, interval, unit) {
+  var now = moment()
+  var then = now.clone().subtract(interval, unit)
+  var data = {}
+  var times = {}
+  var withRelated = (model === Comment ? ['post.communities'] : ['communities'])
 
   return model.query(q => {
-    q.whereRaw('created_at between ? and ?', [then, now]);
+    q.whereRaw('created_at between ? and ?', [then, now])
 
     if (model === Post) {
-      q.where('type', '!=', 'welcome');
+      q.where('type', '!=', 'welcome')
     }
   }).fetchAll({withRelated: withRelated})
   .then(results => {
     results.models.map(x => {
-      var community = (model === Comment ? x.relations.post : x).relations.communities.first(),
-        series = (community ? community.get('name').substring(0, 15) : 'none'),
-        time = Number(moment(x.get('created_at')).startOf('day'));
+      var community = (model === Comment ? x.relations.post : x).relations.communities.first()
+      var series = (community ? community.get('name').substring(0, 15) : 'none')
+      var time = Number(moment(x.get('created_at')).startOf('day'))
 
       // create a nested hash for communities & times
-      if (!data[series]) data[series] = {};
-      if (!data[series][time]) data[series][time] = 0;
-      data[series][time] += 1;
+      if (!data[series]) data[series] = {}
+      if (!data[series][time]) data[series][time] = 0
+      data[series][time] += 1
 
-      // keep a list of all times seen;
+      // keep a list of all times seen
       // the stacked bar chart in nvd3 requires all series
       // to be the same length, so we have to zero-fill
-      if (!times[time]) times[time] = true;
-    });
+      if (!times[time]) times[time] = true
+    })
 
-    return nvd3Format(data, _.keys(times).sort());
-  });
-};
+    return nvd3Format(data, _.keys(times).sort())
+  })
+}
 
-var newUserActionRate = function(table, startTime, pgInterval) {
-  return bookshelf.knex.raw(format(heredoc.strip(function() {/*
+var newUserActionRate = function (table, startTime, pgInterval) {
+  var query = heredoc.strip(function () { /*
+  select
+    cohort,
+    count(*) as user_count,
+    round(sum(sub.acted)/count(*)::float::numeric, 2) as post_rate
+  from (
     select
-      cohort,
-      count(*) as user_count,
-      round(sum(sub.acted)/count(*)::float::numeric, 2) as post_rate
-    from (
-      select
-        u.id,
-        u.email,
-        date_trunc('day', u.created_at)::date as cohort,
-        case when count(x.id) > 0 then 1 else 0 end as acted
-      from
-        users u
-        left join %s x on (
-          u.id = x.user_id
-          and x.created_at - u.created_at < interval '%s'
-        )
-      where u.created_at >= ?
-      group by u.id
-    ) sub
-    group by cohort
-    order by cohort desc
-  */}), table, pgInterval), startTime)
-  .then(data => data.rows.map(r => [Date.parse(r.cohort), Number(r.post_rate)]));
-};
+      u.id,
+      u.email,
+      date_trunc('day', u.created_at)::date as cohort,
+      case when count(x.id) > 0 then 1 else 0 end as acted
+    from
+      users u
+      left join %s x on (
+        u.id = x.user_id
+        and x.created_at - u.created_at < interval '%s'
+      )
+    where u.created_at >= ?
+    group by u.id
+  ) sub
+  group by cohort
+  order by cohort desc
+  */})
 
-var newUserActivity = function(interval, unit) {
-  var startTime = moment().subtract(interval, unit);
+  return bookshelf.knex.raw(format(query, table, pgInterval), startTime)
+  .then(data => data.rows.map(r => [Date.parse(r.cohort), Number(r.post_rate)]))
+}
+
+var newUserActivity = function (interval, unit) {
+  var startTime = moment().subtract(interval, unit)
   return Promise.all([
     Promise.props({
       key: 'post in 7 days',
@@ -109,26 +111,25 @@ var newUserActivity = function(interval, unit) {
       values: newUserActionRate('comment', startTime, '7 days'),
       color: '#ff9f4a'
     })
-  ]);
-};
+  ])
+}
 
 module.exports = {
-
   countNew: countNew,
   newUserActivity: newUserActivity,
 
-  index: function(req, res) {
-    res.ok(req.user);
+  index: function (req, res) {
+    res.ok(req.user)
   },
 
-  metrics: function(req, res) {
+  metrics: function (req, res) {
     Promise.props({
       newUsers: countNew(User, 1, 'month'),
       newPosts: countNew(Post, 1, 'month'),
       newComments: countNew(Comment, 1, 'month'),
       newUserActivity: newUserActivity(1, 'month')
     })
-    .then(res.ok, res.serverError);
+    .then(res.ok, res.serverError)
   },
 
   loginAsUser: function (req, res) {
@@ -136,4 +137,4 @@ module.exports = {
     .then(user => UserSession.login(req, user, 'admin'))
     .then(() => res.redirect('/'))
   }
-};
+}
