e88cad95dfeec7c6ed4436f8f0138f65c755caa3
Robbie Carlton <robbie.carlton@gmail.com>
2016-01-13 17:27:46 -0800

    standardize

--- api/controllers/ActivityController.js
+++ api/controllers/ActivityController.js
@@ -1,46 +1,45 @@
 module.exports = {
-
-  find: function(req, res) {
-    Activity.query(function(qb) {
-      qb.where('reader_id', req.session.userId);
-      qb.limit(req.param('limit') || 10);
-      qb.orderBy('created_at', 'desc');
+  find: function (req, res) {
+    Activity.query(function (qb) {
+      qb.where('reader_id', req.session.userId)
+      qb.limit(req.param('limit') || 10)
+      qb.orderBy('created_at', 'desc')
 
       qb.whereRaw('(comment.active = true or comment.id is null)')
-      .leftJoin('comment', () => this.on('comment.id', '=', 'activity.comment_id'));
+        .leftJoin('comment', () => this.on('comment.id', '=', 'activity.comment_id'))
 
       qb.whereRaw('(post.active = true or post.id is null)')
-      .leftJoin('post', () => this.on('post.id', '=', 'activity.post_id'));
-
-    }).fetchAll({withRelated: [
-      {actor: qb => qb.column('id', 'name', 'avatar_url')},
-      {comment: qb => qb.column('id', 'comment_text', 'created_at')},
-      {'comment.thanks': qb => qb.where('thanked_by_id', req.session.userId)},
-      {post: qb => qb.column('id', 'name', 'user_id', 'type', 'description')},
-      {'post.communities': qb => qb.column('community.id', 'slug')},
-      {'post.relatedUsers': qb => qb.column('users.id', 'name', 'avatar_url')}
+        .leftJoin('post', () => this.on('post.id', '=', 'activity.post_id'))
+    })
+    .fetchAll({withRelated: [
+        {actor: qb => qb.column('id', 'name', 'avatar_url')},
+        {comment: qb => qb.column('id', 'comment_text', 'created_at')},
+        {'comment.thanks': qb => qb.where('thanked_by_id', req.session.userId)},
+        {post: qb => qb.column('id', 'name', 'user_id', 'type', 'description')},
+        {'post.communities': qb => qb.column('community.id', 'slug')},
+        {'post.relatedUsers': qb => qb.column('users.id', 'name', 'avatar_url')}
     ]})
     .then(res.ok)
-    .catch(res.serverError);
+    .catch(res.serverError)
   },
 
-  markAllRead: function(req, res) {
+  markAllRead: function (req, res) {
     Activity.query().where({reader_id: req.session.userId}).update({unread: false})
-      .then(function() {
-        res.ok({});
+      .then(function () {
+        res.ok({})
       })
-      .catch(res.serverError.bind(res));
+      .catch(res.serverError.bind(res))
   },
 
-  update: function(req, res) {
-    Activity.find(req.param('activityId')).then(function(activity) {
-      activity.attributes = _.pick(req.allParams(), 'unread');
-      return activity.save();
+  update: function (req, res) {
+    Activity.find(req.param('activityId')).then(function (activity) {
+      activity.attributes = _.pick(req.allParams(), 'unread')
+      return activity.save()
     })
-    .then(function() {
-      res.ok({});
-    })
-    .catch(res.serverError.bind(res));
+      .then(function () {
+        res.ok({})
+      })
+      .catch(res.serverError.bind(res))
   }
 
-};
+}
