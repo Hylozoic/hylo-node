61b28dbac9ced8cf50b62abc362d85389406ab15
Lawrence Wang <l@lw.io>
2016-01-20 14:48:52 -0800

    use PST for event times in digests

--- test/unit/services/Digest.test.js
+++ test/unit/services/Digest.test.js
@@ -25,4 +25,30 @@ describe('Digest', function () {
       return digest.fetchData().then(() => digest.sendTestEmail(user))
     })
   })
+
+  describe('.formatTime', function () {
+    it('handles empty inputs', () => {
+      expect(Digest.formatTime(null)).to.equal('')
+    })
+
+    it('handles a start time without an end time', () => {
+      var start = new Date('2015-01-23 16:00:00Z')
+      var expected = 'Friday, Jan 23, 2015 at 11:00 AM'
+      expect(Digest.formatTime(start, null, 'America/New_York')).to.equal(expected)
+    })
+
+    it('handles a start and end time on the same day', () => {
+      var start = new Date('2015-01-23 16:00:00Z')
+      var end = new Date('2015-01-24 02:00:00Z')
+      var expected = 'Friday, Jan 23, 2015 from 11:00 AM to 9:00 PM'
+      expect(Digest.formatTime(start, end, 'America/New_York')).to.equal(expected)
+    })
+
+    it('handles a start and end time on different days', () => {
+      var start = new Date('2015-01-23 16:00:00Z')
+      var end = new Date('2015-01-25 01:00:00Z')
+      var expected = 'Friday, Jan 23, 2015 at 11:00 AM to Saturday, Jan 24, 2015 at 8:00 PM'
+      expect(Digest.formatTime(start, end, 'America/New_York')).to.equal(expected)
+    })
+  })
 })
