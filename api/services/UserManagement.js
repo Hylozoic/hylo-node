e25b23f37720000b0fbc293b4adf85b6ec94e58c
Lawrence Wang <l@lw.io>
2016-02-03 19:38:56 -0800

    convert websites to extra info

--- api/services/UserManagement.js
+++ api/services/UserManagement.js
@@ -1,3 +1,5 @@
+var url = require('url')
+
 var userFieldsToCopy = [
   'avatar_url',
   'banner_url',
@@ -144,6 +146,21 @@ module.exports = {
     .then(function() {
       return queries;
     });
-  }
+  },
 
+  convertWebsitesToExtraInfo: function (userId) {
+    return UserWebsite.where('user_id', userId).fetchAll()
+    .then(sites => {
+      if (sites.length === 0) return
+
+      var sitesMarkdown = sites.map(site => {
+        var text = site.get('value')
+        if (!text.startsWith('http')) text = 'http://' + text
+        var u = url.parse(text)
+        return `[${u.hostname}](${u.href})`
+      }).join('\n')
+
+      return bookshelf.knex.raw(`update users set extra_info = E'${sitesMarkdown}\n\n' || extra_info where id = ${userId}`)
+    })
+  }
 };
