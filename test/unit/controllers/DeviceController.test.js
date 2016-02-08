cf0cc83503e21dbedd8448cde4bc7d8988b9829d
Robbie Carlton <robbie.carlton@gmail.com>
2016-02-08 15:32:49 -0800

    add test

--- test/unit/controllers/DeviceController.test.js
+++ test/unit/controllers/DeviceController.test.js
@@ -14,7 +14,8 @@ describe('DeviceController', () => {
   before(() =>
     setup.clearDb()
     .then(() => Promise.props({
-      u1: new User({name: 'U1'}).save()
+      u1: new User({name: 'U1'}).save(),
+      u2: new User({name: 'U2'}).save()
     }))
     .then(props => fixtures = props))
 
@@ -46,6 +47,34 @@ describe('DeviceController', () => {
           expect(device.get('user_id')).to.equal(fixtures.u1.id)
         })
       })
+    })
+
+    it('switches current device to new user', () => {
+      _.extend(req.params, {
+        token: deviceToken,
+        platform: platform
+      })
+
+      req.headers = {'ios-version': version}
+
+      return Device.forge({
+        token: req.param('token'),
+        platform: platform,
+        version: version,
+        user_id: fixtures.u2.id
+      })
+      .save()
+      .then(() => DeviceController.create(req, res)
+        .then(() => {
+          return Device.where('token', '=', deviceToken)
+          .fetch()
+          .then(device => {
+            expect(device).to.exist
+            expect(device.get('token')).to.equal(deviceToken)
+            expect(device.get('user_id')).to.equal(fixtures.u1.id)
+          })
+        })
+      )
     })
   })
 })
