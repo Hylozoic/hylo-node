6d2b90af63cfa94aa597f7912633bee0283b86be
Connor Turland <connorturland@gmail.com>
2016-02-10 11:59:52 +1300

    changes based on code review

--- api/models/Post.js
+++ api/models/Post.js
@@ -241,22 +241,22 @@ module.exports = bookshelf.Model.extend({
 
         _.remove(users, user => user.get('id') === creator.get('id'))
         return Promise.join(
-          Promise.map(communities.models, (community) => {
+          Promise.map(communities.models, community => {
             if (!community.get('slack_hook')) return
-            return Community.sendSlackNotification(community.id, post); 
+            return Community.sendSlackNotification(community.id, post);
           }),
           Promise.map(users, (user) => {
-	    if (!user.get('push_new_post_preference')) return
-	    if (post.isWelcome()) return
-	    var userCommunities = user.relations.communities.models
-	    var postCommunitiesIds = communities.models.map(community => community.get('id'))
-	    var community, path, alertText
-	    community = _.find(userCommunities, community => _.contains(postCommunitiesIds, community.get('id')))
-	    if (!community) return
-	    path = url.parse(Frontend.Route.post(post, community)).path
-	    alertText = PushNotification.textForNewPost(post, community, user.get('id'))
-	    return user.sendPushNotification(alertText, path)
-	  })
+            if (!user.get('push_new_post_preference')) return
+            if (post.isWelcome()) return
+            var userCommunities = user.relations.communities.models
+            var postCommunitiesIds = communities.models.map(community => community.get('id'))
+            var community, path, alertText
+            community = _.find(userCommunities, community => _.contains(postCommunitiesIds, community.get('id')))
+            if (!community) return
+            path = url.parse(Frontend.Route.post(post, community)).path
+            alertText = PushNotification.textForNewPost(post, community, user.get('id'))
+            return user.sendPushNotification(alertText, path)
+          })
         )
       })
   },
--- api/services/Slack.js
+++ api/services/Slack.js
@@ -1,5 +1,7 @@
-var requestPromise = require('request-promise')
 var format = require('util').format
+var Promise = require('bluebird')
+var request = require('request')
+var post = Promise.promisify(request.post)
 
 module.exports = {
   textForNewPost: function (post, community) {
@@ -7,11 +9,11 @@ module.exports = {
 
     if (post.isWelcome()) {
       relatedUser = post.relations.relatedUsers.first()
-      return format('<%s|%s> joined <%s|%s>', 
+      return format('<%s|%s> joined <%s|%s>',
                Frontend.Route.profile(relatedUser), relatedUser.get('name'),
                Frontend.Route.community(community), community.get('name'))
     } else {
-      return format('<%s|%s> posted <%s|"%s"> in <%s|%s>', 
+      return format('<%s|%s> posted <%s|"%s"> in <%s|%s>',
                Frontend.Route.profile(creator), creator.get('name'),
                Frontend.Route.post(post, community), post.get('name'),
                Frontend.Route.community(community), community.get('name'))
@@ -19,15 +21,14 @@ module.exports = {
   },
   send: function (message, url) {
     var options = {
-      method: 'POST',
       uri: url,
       body: {
         text: message,
         username: 'Hylobot',
-        icon_url: 'https://d1qb2nb5cznatu.cloudfront.net/startups/i/46146-d8eff20488206f7c96e2c4ef2b0fb1f4-medium_jpg.jpg?buster=1410306509'
+        icon_url: 'https://cdn.rawgit.com/Connoropolous/hylo-image/master/hylo48.png'
       },
-      json: true // Automatically stringifies the body to JSON 
+      json: true // Automatically stringifies the body to JSON
     };
-    return requestPromise(options)
+    return post(options)
   }
 }
