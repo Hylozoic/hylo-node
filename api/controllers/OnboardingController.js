df40d0a0737878b9939cf230cb08596d9609fe56
Lawrence Wang <l@lw.io>
2016-02-09 17:56:59 -0800

    allow specifying community for onboarding data

--- api/controllers/OnboardingController.js
+++ api/controllers/OnboardingController.js
@@ -1,26 +1,30 @@
 module.exports = {
-
   // find data related to onboarding, not an onboarding object
-  find: function(req, res) {
-    Membership.lastViewed(req.session.userId).fetch({withRelated: [
+  find: function (req, res) {
+    var fetchOptions = {withRelated: [
       {community: qb => qb.column('id', 'leader_id', 'name', 'welcome_message', 'avatar_url', 'slug')},
       {'community.leader': qb => qb.column('id', 'name', 'avatar_url')}
-    ]})
+    ]}
+
+    ;(req.param('communityId')
+      ? Community.find(req.param('communityId'))
+        .then(community => Membership.find(req.session.userId, community.id, fetchOptions))
+      : Membership.lastViewed(req.session.userId).fetch(fetchOptions))
     .then(membership => res.ok(membership.relations.community))
-    .catch(res.serverError);
+    .catch(res.serverError)
   },
 
-  update: function(req, res) {
+  update: function (req, res) {
     Tour.where({user_id: req.session.userId, type: 'onboarding'}).fetch()
-    .then(function(tour) {
+    .then(function (tour) {
       if (req.param('step')) {
-        var status = tour.get('status');
-        status.step = req.param('step');
-        return tour.save({status: status}, {patch: true});
+        var status = tour.get('status')
+        status.step = req.param('step')
+        return tour.save({status: status}, {patch: true})
       }
     })
     .then(res.ok)
-    .catch(res.serverError);
+    .catch(res.serverError)
   }
 
-};
+}
