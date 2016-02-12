b41f707bc33bbd761bd38891ddf13af15c21942c
Lawrence Wang <l@lw.io>
2016-02-11 21:15:22 -0800

    add a more robust digest test

--- test/unit/services/Digest.test.js
+++ test/unit/services/Digest.test.js
@@ -4,25 +4,70 @@ var Digest = require(root('lib/community/digest'))
 var moment = require('moment')
 
 describe('Digest', function () {
-  var community, user
+  var community, user, p1, p2
 
   before(() => {
+    var now = new Date()
+
     community = new Community({name: 'foo', slug: 'foo'})
-    user = new User({name: 'Cat', email: 'cat@cat.org'})
+    user = new User({
+      name: 'Cat',
+      email: 'cat@cat.org',
+      active: true,
+      created_at: now
+    })
+
+    var postAttrs = {
+      user_id: user.id,
+      name: 'Hi!',
+      active: true,
+      created_at: now,
+      type: 'chat'
+    }
+
+    p1 = Post.forge(postAttrs)
+    p2 = Post.forge(_.merge(postAttrs, {created_at: moment().subtract(1, 'month')}))
 
     return setup.clearDb()
-    .then(() => community.save())
-    .then(() => user.save())
-    .then(() => user.joinCommunity(community))
-    .then(() => new Post({user_id: user.id, name: 'Hi!'}).save())
-    .then(post => community.posts().attach(post.id))
+    .then(() => Promise.join(
+      community.save(),
+      user.save()
+    ))
+    .then(() => Promise.join(
+      user.joinCommunity(community),
+      p1.save(),
+      p2.save()
+    ))
+    .spread((x, p1, p2) => Promise.join(
+      community.posts().attach(p1.id),
+      community.posts().attach(p2.id),
+      Comment.forge({
+        comment_text: 'meow',
+        user_id: user.id,
+        post_id: p2.id,
+        active: true,
+        created_at: now
+      }).save()
+    ))
   })
 
   describe('.sendTestEmail', function () {
-    it("doesn't throw errors", function () {
+    it("collects correct data and doesn't throw errors", function () {
       this.timeout(5000)
-      var digest = new Digest(community, moment(), moment().subtract(1, 'week'))
-      return digest.fetchData().then(() => digest.sendTestEmail(user))
+      var startTime = moment().subtract(1, 'week')
+      var endTime = moment().add(1, 'minute')
+      var digest = new Digest(community, startTime, endTime)
+
+      return digest.fetchData()
+      .then(() => {
+        expect(digest.users.length).to.equal(1)
+        expect(digest.users[0].id).to.equal(user.id)
+        expect(digest.posts.length).to.equal(1)
+        expect(digest.posts[0].id).to.equal(p1.id)
+        expect(digest.commentedPosts.length).to.equal(1)
+        expect(digest.commentedPosts[0].id).to.equal(p2.id)
+        digest.sendTestEmail(user)
+      })
     })
   })
 
