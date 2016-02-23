b86ed5da978cd8375020c2646e4b0f98ec2322a7
Lawrence Wang <l@lw.io>
2016-02-22 17:36:44 -0800

    return 404 in checkAndSetPost as appropriate

--- api/policies/checkAndSetPost.js
+++ api/policies/checkAndSetPost.js
@@ -1,25 +1,23 @@
-module.exports = function checkAndSetPost(req, res, next) {
+module.exports = function checkAndSetPost (req, res, next) {
+  var postId = req.param('postId')
 
-  var fail = function(log) {
-    sails.log.debug(format('policy: checkAndSetPost: %s', log));
-    res.forbidden();
-  };
+  var fail = function (log, responseType) {
+    sails.log.debug(`policy: checkAndSetPost: ${log}`)
+    res[responseType || 'forbidden']()
+  }
 
   return Post.find(req.param('postId'), {withRelated: 'communities'})
-  .tap(post => {
-    if (!post) throw new Error(format('post %s not found', req.param('postId')));
+  .then(post => {
+    if (!post) return fail(`post ${postId} not found`, 'notFound')
+
+    res.locals.post = post
+
+    var ok = Admin.isSignedIn(req) ||
+      (res.locals.publicAccessAllowed && post.isPublic())
+
+    return (ok ? Promise.resolve(true)
+      : Post.isVisibleToUser(post.id, req.session.userId))
+    .then(allowed => allowed ? next() : fail('not allowed'))
   })
-  .then(function(post) {
-    res.locals.post = post;
-
-    if (Admin.isSignedIn(req))
-      return Promise.resolve(true);
-
-    if (res.locals.publicAccessAllowed && post.isPublic())
-      return Promise.resolve(true);
-
-    return Post.isVisibleToUser(post.id, req.session.userId);
-  })
-  .then(allowed => allowed ? next() : fail('not allowed'))
-  .catch(err => fail(err.message));
-};
+  .catch(err => fail(err.message, 'serverError'))
+}
