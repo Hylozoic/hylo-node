0fc852579ccb6e86de0a06091959d6e8e0761428
Robbie Carlton <robbie.carlton@gmail.com>
2016-02-23 15:25:50 -0800

    add example in comments

--- api/controllers/MobileAppController.js
+++ api/controllers/MobileAppController.js
@@ -4,6 +4,15 @@ module.exports = {
     var result = {}
 
     switch (req.headers['ios-version']) {
+      /*
+      Format for result:
+      result = {
+        type: 'force',  // can be 'force' or 'suggest'
+        title: 'A new version of the app is available',
+        message: 'You can go to the App Store now to update',
+        iTunesItemIdentifier: '1002185140'
+      }
+      */
       case undefined:
         break
     }
