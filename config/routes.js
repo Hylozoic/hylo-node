36de38f4420c6590d12606bb866ed5a4a171357a
Robbie Carlton <robbie.carlton@gmail.com>
2016-02-23 12:30:58 -0800

    prefix "mobile"

--- config/routes.js
+++ config/routes.js
@@ -165,7 +165,7 @@ module.exports.routes = {
 
   'POST    /noo/subscription':                            'SubscriptionController.create',
 
-  'GET     /noo/auto-update-info':                        'MobileAppController.updateInfo',
+  'GET     /noo/mobile/auto-update-info':                        'MobileAppController.updateInfo',
 
   '/*':                                                   'StaticPageController.proxy'
 }
