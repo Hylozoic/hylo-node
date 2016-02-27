6060dc5fad947d5d465688650dd627fe5941cdd0
Lawrence Wang <l@lw.io>
2016-02-26 16:40:47 -0800

    link directly to comment in comment notification email
    (won’t actually work until we switch over to the new frontend)

--- api/models/Comment.js
+++ api/models/Comment.js
@@ -163,8 +163,8 @@ module.exports = bookshelf.Model.extend({
           comment_text: text,
           post_label: postLabel,
           post_title: post.get('name'),
-          post_url: Frontend.Route.tokenLogin(recipient, token,
-            Frontend.Route.post(post, community) + '?ctt=comment_email'),
+          comment_url: Frontend.Route.tokenLogin(recipient, token,
+            Frontend.Route.post(post) + '?ctt=comment_email' + `#comment-${comment.id}`),
           unfollow_url: Frontend.Route.tokenLogin(recipient, token,
             Frontend.Route.unfollow(post, community)),
           tracking_pixel_url: Analytics.pixelUrl('Comment', {userId: recipient.id})
