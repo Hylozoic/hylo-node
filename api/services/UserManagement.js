b41ca0b86df0249c5da6744241154f0ac2a760d9
Lawrence Wang <l@lw.io>
2016-02-29 15:05:06 -0800

    cleanup and update UserManagement

--- api/services/UserManagement.js
+++ api/services/UserManagement.js
@@ -1,5 +1,3 @@
-var url = require('url')
-
 var userFieldsToCopy = [
   'avatar_url',
   'banner_url',
@@ -16,135 +14,140 @@ var userFieldsToCopy = [
   'linkedin_url',
   'twitter_name',
   'work'
-];
+]
 
 // knex is passed as an argument here because it can be a transaction object
 // see http://knexjs.org/#Transactions
-var generateMergeQueries = function(primaryUserId, secondaryUserId, knex) {
-
-  var ps = [primaryUserId, secondaryUserId],
-      psp = [primaryUserId, secondaryUserId, primaryUserId],
-      updates = [];
+var generateMergeQueries = function (userId, duplicateUserId, knex) {
+  var ps = [userId, duplicateUserId]
+  var psp = [userId, duplicateUserId, userId]
+  var updates = []
+  var push = (q, values) => updates.push(knex.raw(q, values))
 
   // simple updates
-  [
+  ;[
     // table name, user id column
-    ['post',             'user_id'],
-    ['post',             'deactivated_by_id'],
-    ['activity',         'actor_id'],
-    ['comment',          'user_id'],
-    ['comment',          'deactivated_by_id'],
-    ['follower',         'added_by_id'],
-    ['thank_you',        'user_id'],
-    ['thank_you',        'thanked_by_id'],
+    ['post', 'user_id'],
+    ['posts_about_users', 'user_id'],
+    ['post', 'deactivated_by_id'],
+    ['projects', 'user_id'],
+    ['activity', 'actor_id'],
+    ['comment', 'user_id'],
+    ['comment', 'deactivated_by_id'],
+    ['follower', 'added_by_id'],
+    ['thank_you', 'user_id'],
+    ['thank_you', 'thanked_by_id'],
     ['community_invite', 'invited_by_id'],
-    ['community_invite', 'used_by_id']
-  ].forEach(function(args) {
-    updates.push(knex.raw(format('update %s set %s = ? where %s = ?', args[0], args[1], args[1]), ps));
-  });
+    ['community_invite', 'used_by_id'],
+    ['user_external_data', 'user_id'],
+    ['community', 'leader_id']
+  ].forEach(args => {
+    var table = args[0]
+    var userCol = args[1]
+    push(`update ${table} set ${userCol} = ? where ${userCol} = ?`, ps)
+  })
 
   // updates where we have to avoid duplicate records
-  [
+  ;[
     // table name, user id column, column with unique value
-    ['users_community', 'user_id',     'community_id'],
-    ['contributor',     'user_id',     'post_id'],
-    ['follower',        'user_id',     'post_id'],
-    ['linked_account',  'user_id',     'provider_user_id'],
-    ['users_skill',     'user_id',     'skill_name'],
-    ['users_org',       'user_id',     'org_name'],
-    ['phones',          'user_id',     'value'],
-    ['emails',          'user_id',     'value'],
-    ['websites',        'user_id',     'value'],
-    ['tours',           'user_id',     'type'],
-    ['vote',            'user_id',     'post_id']
-  ].forEach(function(args) {
-    var table = args[0], userCol = args[1], uniqueCol = args[2];
-    updates.push(knex.raw(format('update %s set %s = ? where %s = ? and %s not in (select %s from %s where %s = ?)',
-      table, userCol, userCol, uniqueCol, uniqueCol, table, userCol), psp));
-  });
+    ['users_community', 'user_id', 'community_id'],
+    ['contributor', 'user_id', 'post_id'],
+    ['follower', 'user_id', 'post_id'],
+    ['linked_account', 'user_id', 'provider_user_id'],
+    ['users_skill', 'user_id', 'skill_name'],
+    ['users_org', 'user_id', 'org_name'],
+    ['phones', 'user_id', 'value'],
+    ['emails', 'user_id', 'value'],
+    ['websites', 'user_id', 'value'],
+    ['tours', 'user_id', 'type'],
+    ['vote', 'user_id', 'post_id']
+  ].forEach(args => {
+    var table = args[0]
+    var userCol = args[1]
+    var uniqueCol = args[2]
+    push(`update ${table} set ${userCol} = ? ` +
+      `where ${userCol} = ? and ${uniqueCol} not in ` +
+      `(select ${uniqueCol} from ${table} where ${userCol} = ?)`, psp)
+  })
 
-  return {updates: updates, deletes: generateRemoveQueries(secondaryUserId, knex)};
-};
+  return {updates, deletes: generateRemoveQueries(duplicateUserId, knex)}
+}
 
-var generateRemoveQueries = function(userId, knex) {
-
-  var removals = [];
+var generateRemoveQueries = function (userId, knex) {
+  var removals = []
+  var push = (q, values) => removals.push(knex.raw(q, values))
 
   // clear columns without deleting rows
-  [
-    ['comment',   'deactivated_by_id'],
+  ;[
+    ['comment', 'deactivated_by_id'],
     ['community', 'created_by_id'],
-    ['follower',  'added_by_id']
-  ].forEach(function(args) {
-    removals.push(knex.raw(format('update %s set %s = null where %s = %s', args[0], args[1], args[1], userId)));
-  });
+    ['follower', 'added_by_id']
+  ].forEach(args => {
+    var table = args[0]
+    var userCol = args[1]
+    push(`update ${table} set ${userCol} = null where ${userCol} = ?`, userId)
+  })
 
   // cascading deletes
-  removals.push(knex.raw('delete from thank_you where comment_id in ' +
-    '(select id from comment where user_id = ?)', userId));
+  push('delete from thank_you where comment_id in ' +
+    '(select id from comment where user_id = ?)', userId)
 
   // deletes
-  [
+  ;[
     // table, user id column
-    ['users_community',     'user_id'],
-    ['community_invite',    'invited_by_id'],
-    ['community_invite',    'used_by_id'],
-    ['contributor',         'user_id'],
-    ['follower',            'user_id'],
-    ['linked_account',      'user_id'],
-    ['users_skill',         'user_id'],
-    ['users_org',           'user_id'],
-    ['phones',              'user_id'],
-    ['emails',              'user_id'],
-    ['websites',            'user_id'],
+    ['users_community', 'user_id'],
+    ['community_invite', 'invited_by_id'],
+    ['community_invite', 'used_by_id'],
+    ['contributor', 'user_id'],
+    ['follower', 'user_id'],
+    ['linked_account', 'user_id'],
+    ['users_skill', 'user_id'],
+    ['users_org', 'user_id'],
+    ['phones', 'user_id'],
+    ['emails', 'user_id'],
+    ['websites', 'user_id'],
     ['user_post_relevance', 'user_id'],
-    ['activity',            'reader_id'],
-    ['activity',            'actor_id'],
-    ['tours',               'user_id'],
-    ['vote',                'user_id'],
-    ['comment',             'user_id'],
-    ['users',               'id']
-  ].forEach(function(args) {
-    removals.push(knex.raw(format('delete from %s where %s = ?', args[0], args[1]), [userId]));
-  });
+    ['activity', 'reader_id'],
+    ['activity', 'actor_id'],
+    ['tours', 'user_id'],
+    ['vote', 'user_id'],
+    ['comment', 'user_id'],
+    ['user_external_data', 'user_id'],
+    ['users', 'id']
+  ].forEach(args => {
+    var table = args[0]
+    var userCol = args[1]
+    push(`delete from ${table} where ${userCol} = ?`, userId)
+  })
 
-  return removals;
+  return removals
 }
 
 module.exports = {
+  // this does not delete posts!
+  removeUser: userId =>
+    bookshelf.knex.transaction(trx =>
+      Promise.all(generateRemoveQueries(userId, trx))),
 
-  removeUser: function(userId) {
-    return bookshelf.knex.transaction(function(trx) {
-      return Promise.all(generateRemoveQueries(userId, trx));
-    });
-  },
+  mergeUsers: (userId, duplicateUserId) => {
+    var queries
 
-  mergeUsers: function(primaryUserId, secondaryUserId) {
-    var queries;
-
-    return bookshelf.knex.transaction(function(trx) {
-      queries = generateMergeQueries(primaryUserId, secondaryUserId, trx);
+    return bookshelf.knex.transaction(trx => {
+      queries = generateMergeQueries(userId, duplicateUserId, trx)
 
       return Promise.join(
-        User.find(primaryUserId, {transacting: trx}),
-        User.find(secondaryUserId, {transacting: trx})
+        User.find(userId, {transacting: trx}),
+        User.find(duplicateUserId, {transacting: trx})
       )
-      .spread(function(primaryUser, secondaryUser) {
-        userFieldsToCopy.forEach(function(attr) {
-          if (!primaryUser.get(attr)) {
-            primaryUser.set(attr, secondaryUser.get(attr));
-          }
-        });
+      .spread((user, dupe) => {
+        userFieldsToCopy.forEach(f => user.get(f) || user.set(f, dupe.get(f)))
 
-        if (!_.isEmpty(primaryUser.changed)) {
-          return primaryUser.save(primaryUser.changed, {patch: true, transacting: trx});
-        }
+        return _.isEmpty(user.changed) ||
+          user.save(user.changed, {patch: true, transacting: trx})
       })
-      .then(Promise.all.bind(Promise, queries.updates))
-      .then(Promise.all.bind(Promise, queries.deletes));
+      .then(() => Promise.all(queries.updates))
+      .then(() => Promise.all(queries.deletes))
     })
-    .then(function() {
-      return queries;
-    });
+    .then(() => queries.updates.concat(queries.deletes).map(q => q.toSQL()))
   }
-};
+}
