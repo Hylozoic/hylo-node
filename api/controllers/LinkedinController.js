a115c0221e9a15f6f87d48719917fff01d983357
Lawrence Wang <l@lw.io>
2016-04-05 13:02:12 -0700

    fix old bug with LinkedInController
    also standardize and clean up a bit

--- api/controllers/LinkedinController.js
+++ api/controllers/LinkedinController.js
@@ -1,63 +1,52 @@
-var crypto = require('crypto'),
-  Promise = require('bluebird'),
-  request = require('request'),
+const crypto = require('crypto')
+const Promise = require('bluebird')
+const request = require('request')
+const get = Promise.promisify(request.get)
+const post = Promise.promisify(request.post)
+const parseXml = Promise.promisify(require('xml2js').parseString)
+const authUrlPrefix = 'https://www.linkedin.com/uas/oauth2/authorization'
+const tokenUrlPrefix = 'https://www.linkedin.com/uas/oauth2/accessToken'
+const peopleApiPrefix = 'https://api.linkedin.com/v1/people'
 
-  get = Promise.promisify(request.get),
-  post = Promise.promisify(request.post),
-  parseXml = Promise.promisify(require('xml2js').parseString),
+const generateCSRFToken = () =>
+  crypto.randomBytes(10).toString('hex')
 
-  authUrlPrefix = 'https://www.linkedin.com/uas/oauth2/authorization',
-  tokenUrlPrefix = 'https://www.linkedin.com/uas/oauth2/accessToken',
-  peopleApiPrefix = 'https://api.linkedin.com/v1/people';
-
-var generateCSRFToken = function() {
-  return crypto.randomBytes(10).toString('hex');
-};
-
-var redirectUri = function() {
-  return format('%s://%s%s', process.env.PROTOCOL, process.env.DOMAIN, '/noo/linkedin/provide');
-};
+const redirectUri = () =>
+  `${process.env.PROTOCOL}://${process.env.DOMAIN}/noo/linkedin/provide`
 
 module.exports = {
+  authorize: function (req, res) {
+    const token = generateCSRFToken()
+    const url = format('%s?response_type=code&scope=%s&client_id=%s&state=%s&redirect_uri=%s',
+      authUrlPrefix, 'r_basicprofile', process.env.LINKEDIN_API_KEY, token, redirectUri())
 
-  authorize: function(req, res) {
-    var token = generateCSRFToken(),
-      url = format('%s?response_type=code&scope=%s&client_id=%s&state=%s&redirect_uri=%s',
-        authUrlPrefix, 'r_basicprofile', process.env.LINKEDIN_API_KEY, token, redirectUri());
-
-    req.session.linkedinAuthCSRFToken = token;
-    res.writeHead(302, {Location: url});
-    res.end();
+    req.session.linkedinAuthCSRFToken = token
+    res.writeHead(302, {Location: url})
+    res.end()
   },
 
-  provideData: function(req, res) {
-    if (req.session.linkedinAuthCSRFToken != req.param('state')) {
-      return res.badRequest();
+  provideData: function (req, res) {
+    if (req.session.linkedinAuthCSRFToken !== req.param('state')) {
+      return res.badRequest()
     }
 
-    var authCode = req.param('code'),
-      requestTokenUrl = format(
-        '%s?grant_type=authorization_code&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s',
-        tokenUrlPrefix, authCode, redirectUri(), process.env.LINKEDIN_API_KEY, process.env.LINKEDIN_API_SECRET);
+    const authCode = req.param('code')
+    const requestTokenUrl = format(
+      '%s?grant_type=authorization_code&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s',
+      tokenUrlPrefix, authCode, redirectUri(), process.env.LINKEDIN_API_KEY, process.env.LINKEDIN_API_SECRET)
 
-    post(requestTokenUrl).spread(function(response, body) {
-      return JSON.parse(body).access_token;
-    })
-    .then(function(token) {
-      return get(format('%s/~:(public-profile-url)?oauth2_access_token=%s', peopleApiPrefix, token));
-    })
-    .spread(function(response, body) {
-      return parseXml(body);
-    })
-    .then(function(doc) {
-      return doc.person['public-profile-url'][0];
-    })
-    .then(function(url) {
-      return res.view('popupDone', {context: 'linkedin-profile', url: url, layout: null, returnDomain: null});
-    })
-    .catch(function(err) {
-      res.serverError(err);
-    });
+    post(requestTokenUrl)
+    .spread((response, body) => JSON.parse(body).access_token)
+    .then(token => get(`${peopleApiPrefix}/~:(public-profile-url)?oauth2_access_token=${token}`))
+    .spread((response, body) => parseXml(body))
+    .then(doc => _.get(doc, 'person.public-profile-url.0'))
+    .then(url => res.view('popupDone', {
+      context: 'linkedin-profile',
+      url: url,
+      layout: null,
+      returnDomain: null
+    }))
+    .catch(res.serverError)
   }
 
-};
+}
