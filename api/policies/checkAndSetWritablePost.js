2b86fb31f68f469676c4e789ce1e3d19fe37cdf2
Lawrence Wang <l@lw.io>
2016-04-13 18:08:54 -0700

    standardize formatting

--- api/policies/checkAndSetWritablePost.js
+++ api/policies/checkAndSetWritablePost.js
@@ -1,24 +1,27 @@
-module.exports = function checkAndSetWritablePost(req, res, next) {
+module.exports = function checkAndSetWritablePost (req, res, next) {
+  Post.find(req.param('postId'), {withRelated: [
+    {communities: q => q.column('community.id')}
+  ]})
+  .tap(post => {
+    if (!post) throw new Error(format('post %s not found', req.param('postId')))
 
-  Post.find(req.param('postId'), {withRelated: [{communities: q => q.column('community.id')}]})
-  .tap(function(post) {
-    if (!post) throw new Error(format('post %s not found', req.param('postId')));
+    if (Admin.isSignedIn(req) || post.get('user_id') === req.session.userId) {
+      return true
+    }
 
-    if (Admin.isSignedIn(req) || post.get('user_id') == req.session.userId)
-      return true;
-
-    var communityId = post.relations.communities.first().id;
-    return Membership.hasModeratorRole(req.session.userId, communityId).then(function(isModerator) {
-      if (!isModerator) throw 'not a moderator';
-    });
+    const communityId = post.relations.communities.first().id
+    return Membership.hasModeratorRole(req.session.userId, communityId)
+    .then(isModerator => {
+      if (!isModerator) throw new Error('not a moderator')
+    })
   })
-  .then(function(post) {
-    res.locals.post = post;
-    next();
+  .then(function (post) {
+    res.locals.post = post
+    next()
   })
-  .catch(function(err) {
-    sails.log.debug(format("Fail checkAndSetWritablePost policy %s %s: %s",
-      req.session.userId, req.param('postId'), err));
-    res.forbidden();
-  });
-};
+  .catch(function (err) {
+    sails.log.debug(format('Fail checkAndSetWritablePost policy %s %s: %s',
+      req.session.userId, req.param('postId'), err.message))
+    res.forbidden()
+  })
+}
