43d3102f92ffac964fe1b9d52a5ae5a55a260cbe
Robbie Carlton <robbie.carlton@gmail.com>
2016-04-20 13:43:50 -0700

    standardize

--- test/unit/models/Membership.test.js
+++ test/unit/models/Membership.test.js
@@ -1,53 +1,49 @@
-var setup = require(require('root-path')('test/setup'));
+var setup = require(require('root-path')('test/setup'))
 
-describe('Membership', function() {
+describe('Membership', function () {
+  var user, community
 
-  var user, community;
-
-  describe('.find', function() {
-
-    before(function() {
-      community = new Community({slug: 'foo', name: 'Foo'});
-      user = new User({name: 'Cat', email: 'a@b.c'});
-      return setup.clearDb().then(function() {
+  describe('.find', function () {
+    before(function () {
+      community = new Community({slug: 'foo', name: 'Foo'})
+      user = new User({name: 'Cat', email: 'a@b.c'})
+      return setup.clearDb().then(function () {
         return Promise.join(
           community.save(),
           user.save()
-        );
-      }).then(function() {
-        return user.joinCommunity(community);
-      });
-    });
+        )
+      }).then(function () {
+        return user.joinCommunity(community)
+      })
+    })
 
-    it('works with a community id', function() {
-      return Membership.find(user.id, community.id).then(function(membership) {
-        expect(membership).to.exist;
-      });
-    });
+    it('works with a community id', function () {
+      return Membership.find(user.id, community.id).then(function (membership) {
+        expect(membership).to.exist
+      })
+    })
 
-    it('works with a community slug', function() {
-      return Membership.find(user.id, community.get('slug')).then(function(membership) {
-        expect(membership).to.exist;
-      });
-    });
+    it('works with a community slug', function () {
+      return Membership.find(user.id, community.get('slug')).then(function (membership) {
+        expect(membership).to.exist
+      })
+    })
 
-    it('returns nothing for a blank user id', function() {
-      return Membership.find(null, community.id).then(function(membership) {
-        expect(membership).not.to.exist;
-      });
-    });
+    it('returns nothing for a blank user id', function () {
+      return Membership.find(null, community.id).then(function (membership) {
+        expect(membership).not.to.exist
+      })
+    })
 
-    it('does not return an inactive membership', function() {
+    it('does not return an inactive membership', function () {
       return Membership.query().where({
         user_id: user.id,
         community_id: community.id
       }).update({active: false}).then(() => {
-        return Membership.find(user.id, community.id);
+        return Membership.find(user.id, community.id)
       }).then(membership => {
-        expect(membership).not.to.exist;
-      });
-    });
-
-  });
-
-});
+        expect(membership).not.to.exist
+      })
+    })
+  })
+})
