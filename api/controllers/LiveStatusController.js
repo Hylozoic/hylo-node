163b3dd336468593857c428ad49c70fbf2eefa8e
Lawrence Wang <l@lw.io>
2016-04-22 16:10:48 -0700

    add community new notification counts to live status

--- api/controllers/LiveStatusController.js
+++ api/controllers/LiveStatusController.js
@@ -1,10 +1,14 @@
 module.exports = {
-
   show: function (req, res) {
-    if (!req.session.userId) {
-      return res.ok({})
-    }
-    User.find(req.session.userId)
-    .then(user => res.ok({new_notification_count: user.get('new_notification_count')}))
+    if (!req.session.userId) return res.ok({})
+
+    User.find(req.session.userId, {withRelated: ['memberships']})
+    .then(user => res.ok({
+      new_notification_count: user.get('new_notification_count'),
+      memberships: user.relations.memberships.map(m => ({
+        id: m.id,
+        new_notification_count: m.get('new_notification_count')
+      }))
+    }))
   }
 }
