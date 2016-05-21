4a13d9e175c04f2414f13a9665f87afb8f02f658
Lawrence Wang <l@lw.io>
2016-05-20 20:39:05 -0700

    handle bad user urls
    e.g. /u/undefined

--- api/policies/inSameCommunityOrNetwork.js
+++ api/policies/inSameCommunityOrNetwork.js
@@ -1,14 +1,15 @@
 module.exports = function (req, res, next) {
-  if (Admin.isSignedIn(req)) return next();
+  if (Admin.isSignedIn(req)) return next()
 
-  if (req.session.userId === req.param('userId')) return next();
+  if (req.session.userId === req.param('userId')) return next()
+  if (isNaN(Number(req.param('userId')))) return res.notFound()
 
   Membership.inSameCommunity([req.session.userId, req.param('userId')])
-    .then(same => {
-      if (same) return next();
+  .then(same => {
+    if (same) return next()
 
-      Membership.inSameNetwork(req.session.userId, req.param('userId'))
-      .then(same => (same ? next() : res.forbidden()));
-    })
-    .catch(res.serverError);
-};
+    Membership.inSameNetwork(req.session.userId, req.param('userId'))
+    .then(same => (same ? next() : res.forbidden()))
+  })
+  .catch(res.serverError)
+}
