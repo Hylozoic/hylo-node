2d1459ba015cfde7fe893aee37f9e4052e108101
Robbie Carlton <robbie.carlton@gmail.com>
2016-05-31 14:47:55 -0700

    Revert "Merge pull request #128 from Hylozoic/force-ios-update"
    This reverts commit 64fb19f1cce89c7d7f51a310977bae0af7b4829d, reversing
    changes made to a4681ac95798586dacd3616544317be3aeee734c.

--- api/controllers/MobileAppController.js
+++ api/controllers/MobileAppController.js
@@ -3,26 +3,16 @@ module.exports = {
   updateInfo: function (req, res) {
     var result = {}
 
-    /*
-    Format for result:
-    result = {
-      type: 'force',  // can be 'force' or 'suggest'
-      title: 'A new version of the app is available',
-      message: 'You can go to the App Store now to update',
-      iTunesItemIdentifier: '1002185140'
-    }
-    */
-
     switch (req.headers['ios-version']) {
-      case '1.6':
-      case '1.65':
-        result = {
-          type: 'force',  // can be 'force' or 'suggest'
-          title: 'A new version of the app is available',
-          message: 'The version you are using is no longer compatible with the site. Please go to the App Store now to update',
-          iTunesItemIdentifier: '1002185140'
-        }
-        break
+      /*
+      Format for result:
+      result = {
+        type: 'force',  // can be 'force' or 'suggest'
+        title: 'A new version of the app is available',
+        message: 'You can go to the App Store now to update',
+        iTunesItemIdentifier: '1002185140'
+      }
+      */
       case undefined:
         break
     }
