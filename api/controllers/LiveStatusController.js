0239c881368b16da21453037c79e15b1d51a826b
Robbie Carlton <robbie.carlton@gmail.com>
2016-06-06 13:41:33 -0700

    LiveStatus controller returns slug

--- api/controllers/LiveStatusController.js
+++ api/controllers/LiveStatusController.js
@@ -11,6 +11,7 @@ module.exports = {
         .then(leftNavTags => res.ok({
           new_notification_count: user.get('new_notification_count'),
           left_nav_tags: leftNavTags,
+          slug: community && community.get('slug'),
           memberships: user.relations.memberships.map(m => ({
             id: m.id,
             new_notification_count: m.get('new_notification_count')
