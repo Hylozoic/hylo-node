073afe31fd2378561eb438408046176159ed6e2b
Lawrence Wang <l@lw.io>
2016-06-06 12:05:52 -0700

    cleanup

--- test/unit/services/Email.test.js
+++ test/unit/services/Email.test.js
@@ -1,44 +1,33 @@
-require(require('root-path')('test/setup'));
+require(require('root-path')('test/setup'))
 
-describe('Email', function() {
+describe('Email', function () {
+  describe('.sendInvitation', function () {
+    it('makes a successful request to SendWithUs', function () {
+      this.timeout(5000)
+      return Email.sendInvitation('foo@bar.com', {})
+      .then(result => expect(result.success).to.be.true)
+    })
+  })
 
-  describe('.sendInvitation', function() {
-
-    it('makes a successful request to SendWithUs', function(done) {
-      this.timeout(5000);
-
-      Email.sendInvitation('foo@bar.com', {})
-      .then(function(result) {
-        expect(result.success).to.be.true;
-        done();
-      })
-      .catch(done);
-
-    });
-
-  });
-
-  describe('.postReplyAddress', function() {
-
+  describe('.postReplyAddress', () => {
     // this expects dev environment variables:
     // MAILGUN_EMAIL_SALT=FX988194AD22EE636
     // MAILGUN_DOMAIN=mg.hylo.com
     // PLAY_APP_SECRET=5qX69G/e3ZJ29qIeaEpJKQuJYr3MHOe52EFmRNGVOqfW8VAxUwSKUg
-    it('encrypts the post and user ids', function() {
-      var postId = 7823, userId = 5942,
-        expected = 'reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com';
+    it('encrypts the post and user ids', () => {
+      const postId = 7823
+      const userId = 5942
+      const expected = 'reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com'
 
-      expect(Email.postReplyAddress(postId, userId)).to.equal(expected);
-    })
-
-  });
-
-  describe('.decodePostReplyAddress', function() {
-    it('works with human-readable formats', function() {
-      var address = '"reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com" <reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com>';
-
-      expect(Email.decodePostReplyAddress(address)).to.deep.equal({postId: '7823', userId: '5942'});
+      expect(Email.postReplyAddress(postId, userId)).to.equal(expected)
     })
   })
 
-});
+  describe('.decodePostReplyAddress', () => {
+    it('works with human-readable formats', () => {
+      var address = '"reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com" <reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com>'
+
+      expect(Email.decodePostReplyAddress(address)).to.deep.equal({postId: '7823', userId: '5942'})
+    })
+  })
+})
