92d39f940e824564a7880aee0299de5f3835fa6b
Lawrence Wang <l@lw.io>
2016-06-27 14:27:14 -0700

    add user id to raw events
    so we can count uniques

--- api/controllers/AdminController.js
+++ api/controllers/AdminController.js
@@ -121,6 +121,31 @@ var newUserActivity = function (interval, unit) {
   ])
 }
 
+var rawMetricsQuery = startTime => Promise.props({
+  community: Community.query(q => {
+    q.select(['id', 'name', 'created_at', 'avatar_url'])
+  }).query(),
+
+  user: User.query(q => {
+    q.where('users.created_at', '>', startTime)
+    q.leftJoin('users_community', 'users.id', 'users_community.user_id')
+    q.select(['users.id', 'users.created_at', 'users_community.community_id'])
+  }).query(),
+
+  post: Post.query(q => {
+    q.where('post.created_at', '>', startTime)
+    q.where('post.type', '!=', 'welcome')
+    q.join('post_community', 'post.id', 'post_community.post_id')
+    q.select(['post.id', 'post.created_at', 'post_community.community_id', 'post.user_id'])
+  }).query(),
+
+  comment: Comment.query(q => {
+    q.where('comment.created_at', '>', startTime)
+    q.join('post_community', 'comment.post_id', 'post_community.post_id')
+    q.select(['comment.id', 'comment.created_at', 'post_community.community_id', 'comment.user_id'])
+  }).query()
+})
+
 module.exports = {
   countNew: countNew,
   newUserActivity: newUserActivity,
@@ -147,42 +172,23 @@ module.exports = {
 
   rawMetrics: function (req, res) {
     const startTime = moment().subtract(3, 'months').toDate()
-    return Promise.props({
-      community: Community.query(q => {
-        q.select(['id', 'name', 'created_at', 'avatar_url'])
-      }).query(),
-
-      user: User.query(q => {
-        q.where('users.created_at', '>', startTime)
-        q.leftJoin('users_community', 'users.id', 'users_community.user_id')
-        q.select(['users.id', 'users.created_at', 'users_community.community_id'])
-      }).query(),
-
-      post: Post.query(q => {
-        q.where('post.created_at', '>', startTime)
-        q.where('post.type', '!=', 'welcome')
-        q.join('post_community', 'post.id', 'post_community.post_id')
-        q.select(['post.id', 'post.created_at', 'post_community.community_id'])
-      }).query(),
-
-      comment: Comment.query(q => {
-        q.where('comment.created_at', '>', startTime)
-        q.join('post_community', 'comment.post_id', 'post_community.post_id')
-        q.select(['comment.id', 'comment.created_at', 'post_community.community_id'])
-      }).query()
-    })
+    return rawMetricsQuery(startTime)
     .then(props => {
       let result = props.community.reduce((acc, c) => {
         acc[c.id] = merge(c, {events: []})
         return acc
       }, {})
 
-      result.none = {name: 'No community', events: []}
+      result.none = {id: 'none', name: 'No community', events: []}
 
       ;['user', 'post', 'comment'].forEach(name => {
         props[name].forEach(item => {
           const key = item.community_id || 'none'
-          result[key].events.push({time: Date.parse(item.created_at), name})
+          result[key].events.push({
+            time: Date.parse(item.created_at),
+            user_id: item.user_id || item.id,
+            name
+          })
         })
       })
 
