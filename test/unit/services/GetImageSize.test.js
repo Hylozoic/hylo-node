b55a487c4929474e776c9138a69b580954699483
Lawrence Wang <l@lw.io>
2016-06-29 15:26:41 -0700

    make a flaky test less flaky

--- test/unit/services/GetImageSize.test.js
+++ test/unit/services/GetImageSize.test.js
@@ -3,7 +3,8 @@ require(root('test/setup'))
 var GetImageSize = require(root('api/services/GetImageSize'))
 
 describe('GetImageSize', () => {
-  it('gets the size', () => {
+  it('gets the size', function () {
+    this.timeout(5000)
     return GetImageSize('https://www.hylo.com/favicon.png')
     .then(dimensions => {
       expect(dimensions.width).to.equal(32)
