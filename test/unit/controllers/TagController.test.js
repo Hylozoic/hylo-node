39c3dc942b483efab9a8d4cd0bab400f2add8269
Lawrence Wang <l@lw.io>
2016-06-30 19:10:23 -0700

    remove old test

--- test/unit/controllers/TagController.test.js
+++ test/unit/controllers/TagController.test.js
@@ -94,42 +94,6 @@ describe('TagController', () => {
     })
   })
 
-  describe('.findForLeftNav', () => {
-    it('returns followed and created tags ', () => {
-      req.session.userId = fixtures.u1.id
-      _.extend(req.params, {
-        communityId: fixtures.c1.get('slug')
-      })
-
-      return new TagFollow({
-        community_id: fixtures.c1.id,
-        tag_id: fixtures.t1.id,
-        user_id: fixtures.u1.id,
-        new_post_count: 4
-      }).save()
-      .then(() => new TagFollow({
-        community_id: fixtures.c1.id,
-        tag_id: fixtures.t2.id,
-        user_id: fixtures.u1.id,
-        new_post_count: 5
-      }).save())
-      .then(() => new CommunityTag({
-        community_id: fixtures.c1.id,
-        tag_id: fixtures.t2.id,
-        user_id: fixtures.u1.id
-      }).save())
-      .then(() => TagController.findForLeftNav(req, res))
-      .then(() => {
-        expect(res.body.followed.length).to.equal(1)
-        expect(res.body.followed[0].name).to.equal('tagone')
-        expect(res.body.followed[0].new_post_count).to.equal(4)
-        expect(res.body.created.length).to.equal(1)
-        expect(res.body.created[0].name).to.equal('tagtwo')
-        expect(res.body.created[0].new_post_count).to.equal(5)
-      })
-    })
-  })
-
   describe('.resetNewPostCount', () => {
     it('resets new_post_count to 0', () => {
       req.session.userId = fixtures.u1.id
