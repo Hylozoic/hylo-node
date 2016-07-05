0addcbcb720f00196e2193cab789d950cca5be7e
Lawrence Wang <l@lw.io>
2016-07-05 15:36:24 -0700

    cleanup

--- api/services/Analytics.js
+++ api/services/Analytics.js
@@ -1,34 +1,34 @@
-var sails = require('sails'),
-  uuid = require('node-uuid'),
-  instance;
+const sails = require('sails')
+const uuid = require('node-uuid')
+var instance
 
-if (process.env.NODE_ENV == 'test') {
+if (process.env.NODE_ENV === 'test') {
   instance = {
-    track: function(opts) {
-      sails.log.verbose('Analytics.track: ' + JSON.stringify(opts));
+    track: function (opts) {
+      sails.log.verbose('Analytics.track: ' + JSON.stringify(opts))
     }
-  };
+  }
 } else {
-  instance = require('analytics-node')(process.env.SEGMENT_KEY);
+  instance = require('analytics-node')(process.env.SEGMENT_KEY)
 }
 
-instance.pixelUrl = function(emailName, props) {
-  var prefix = 'https://api.segment.io/v1/pixel/track?data=';
+instance.pixelUrl = function (emailName, props) {
+  var prefix = 'https://api.segment.io/v1/pixel/track?data='
 
   var data = {
     writeKey: process.env.SEGMENT_KEY,
     event: 'Viewed Email: ' + emailName,
     properties: props
-  };
-
-  if (props.userId) {
-    data.userId = props.userId;
-  } else {
-    data.anonymousId = uuid.v4();
   }
 
-  var encodedData = new Buffer(JSON.stringify(data), 'utf8').toString('base64');
-  return prefix + encodedData;
-};
+  if (props.userId) {
+    data.userId = props.userId
+  } else {
+    data.anonymousId = uuid.v4()
+  }
 
-module.exports = instance;
+  var encodedData = new Buffer(JSON.stringify(data), 'utf8').toString('base64')
+  return prefix + encodedData
+}
+
+module.exports = instance
