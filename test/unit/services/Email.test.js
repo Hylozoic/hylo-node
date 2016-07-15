b83c678328bd8fbf03e5ef0ff8ab3b6e3818e63c
David Reed <dreed@thoughtworks.com>
2016-07-15 17:45:41 +1000

    Changing PLAY_APP_SECRET and MAILGUN_EMAIL_SALT in README to fake values

--- test/unit/services/Email.test.js
+++ test/unit/services/Email.test.js
@@ -1,25 +1,27 @@
 require(require('root-path')('test/setup'))
 
 describe('Email', function () {
-  describe('.postReplyAddress', () => {
+  describe('reply address', () => {
     // this expects dev environment variables:
-    // MAILGUN_EMAIL_SALT=FX988194AD22EE636
+    // MAILGUN_EMAIL_SALT=FFFFAAAA123456789
     // MAILGUN_DOMAIN=mg.hylo.com
-    // PLAY_APP_SECRET=5qX69G/e3ZJ29qIeaEpJKQuJYr3MHOe52EFmRNGVOqfW8VAxUwSKUg
-    it('encrypts the post and user ids', () => {
-      const postId = 7823
-      const userId = 5942
-      const expected = 'reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com'
+    // PLAY_APP_SECRET=quxgrault12345678
+    const postId = 7823
+    const userId = 5942
+    const email = 'reply-8c26a271fe72895d4e3c20a6893d9c0ee9c9041235c9ce207c0a627196396807@mg.hylo.com'
 
-      expect(Email.postReplyAddress(postId, userId)).to.equal(expected)
+    describe('.postReplyAddress', () => {
+      it('encrypts the post and user ids', () => {
+        expect(Email.postReplyAddress(postId, userId)).to.equal(email)
+      })
     })
-  })
 
-  describe('.decodePostReplyAddress', () => {
-    it('works with human-readable formats', () => {
-      var address = '"reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com" <reply-7152e5d64e5fd9e75e6108c1e9356ef418b81bb1a3f77f32cbf42b11c7d50e0e@mg.hylo.com>'
+    describe('.decodePostReplyAddress', () => {
+      it('works with human-readable formats', () => {
+        var address = `"${email}" <${email}>`
 
-      expect(Email.decodePostReplyAddress(address)).to.deep.equal({postId: '7823', userId: '5942'})
+        expect(Email.decodePostReplyAddress(address)).to.deep.equal({postId: `${postId}`, userId: `${userId}`})
+      })
     })
   })
 })
