02e229fcddcf798431b3d5c7c47217c8ebff6c12
Lawrence Wang <l@lw.io>
2016-07-20 12:56:13 -0700

    don't refer to favicon in tests

--- test/unit/services/GetImageSize.test.js
+++ test/unit/services/GetImageSize.test.js
@@ -5,10 +5,10 @@ var GetImageSize = require(root('api/services/GetImageSize'))
 describe('GetImageSize', () => {
   it('gets the size', function () {
     this.timeout(5000)
-    return GetImageSize('https://www.hylo.com/favicon.png')
+    return GetImageSize('http://cdn.hylo.com/misc/hylo-logo-teal-on-transparent.png')
     .then(dimensions => {
-      expect(dimensions.width).to.equal(32)
-      expect(dimensions.height).to.equal(32)
+      expect(dimensions.width).to.equal(300)
+      expect(dimensions.height).to.equal(300)
     })
   })
 })
