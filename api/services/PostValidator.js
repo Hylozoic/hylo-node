16b688682201a2bb6c162db06bba3be6073d805c
Aaron Edwards <aedwards@thoughtworks.com>
2016-07-26 16:40:25 +1000

    AE/RB #13 created a post params validator

---
+++ api/services/PostValidator.js
@@ -0,0 +1,25 @@
+
+const validations = [
+  {
+    title: "title can't be blank",
+    rule: params => params.name
+  }, {
+    title: "only posts of type 'project' may have a financial request",
+    rule: params => !params.financialRequestAmount || params.type === 'project'
+  }, {
+    title: 'financial requests must be between 0 and 5,000',
+    rule: params => params.financialRequestAmount === undefined || (params.financialRequestAmount > 0 && params.financialRequestAmount <= 5000)
+  }, {
+    title: 'financial request amount must be a numeric value',
+    rule: params => params.financialRequestAmount === undefined || typeof params.financialRequestAmount === 'number'
+  }
+]
+
+module.exports = {
+  validate: function (postParams) {
+    return _.chain(validations)
+    .reject(validation => validation.rule(postParams))
+    .map(validation => validation.title)
+    .value()
+  }
+}
---
+++ test/unit/services/PostValidator.test.js
@@ -0,0 +1,93 @@
+var root = require('root-path')
+require(root('test/setup'))
+var PostValidator = require(root('api/services/PostValidator'))
+
+describe('PostValidator', () => {
+  describe('.validate', () => {
+    const defaultPostParams = {
+      name: 'some post',
+      type: 'project'
+    }
+
+    it('should show no error for a valid post', () => {
+      const errors = PostValidator.validate(defaultPostParams)
+      expect(errors).to.eql([])
+    })
+
+    it('should show no error for a valid project with financialRequestAmount', () => {
+      const postParams = _.merge({}, defaultPostParams, {
+        financialRequestAmount: 100,
+        type: 'project'
+      })
+
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.eql([])
+    })
+
+    it('should return an error if there is no title', () => {
+      const postParams = _.omit(defaultPostParams, ['name'])
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.have.lengthOf(1)
+      expect(errors).to.contain("title can't be blank")
+    })
+
+    it('should return an error if there is a financial request but the type is not a project', () => {
+      const postParams = _.merge({}, defaultPostParams, {
+        type: 'comment',
+        financialRequestAmount: 100
+      })
+
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.have.lengthOf(1)
+      expect(errors).to.contain("only posts of type 'project' may have a financial request")
+    })
+
+    it('should return an error if financialRequestAmount is less than zero', () => {
+      const postParams = _.merge({}, defaultPostParams, { financialRequestAmount: -100 })
+
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.have.lengthOf(1)
+      expect(errors).to.contain('financial requests must be between 0 and 5,000')
+    })
+
+    it('should return an error if financialRequestAmount is equal to zero', () => {
+      const postParams = _.merge({}, defaultPostParams, { financialRequestAmount: 0 })
+
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.have.lengthOf(1)
+      expect(errors).to.contain('financial requests must be between 0 and 5,000')
+    })
+
+    it('should return an error if financialRequestAmount is equal to zero', () => {
+      const postParams = _.merge({}, defaultPostParams, { financialRequestAmount: 0 })
+
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.have.lengthOf(1)
+      expect(errors).to.contain('financial requests must be between 0 and 5,000')
+    })
+
+    it('should return an error if financialRequestAmount is more than 5,000', () => {
+      const postParams = _.merge({}, defaultPostParams, { financialRequestAmount: 5001 })
+
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.have.lengthOf(1)
+      expect(errors).to.contain('financial requests must be between 0 and 5,000')
+    })
+
+    it('should return an error if financialRequestAmount is not numeric', () => {
+      const postParams = _.merge({}, defaultPostParams, { financialRequestAmount: '1000' })
+
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.have.lengthOf(1)
+      expect(errors).to.contain('financial request amount must be a numeric value')
+    })
+
+    it('should return an error if financialRequestAmount is NaN', () => {
+      const postParams = _.merge({}, defaultPostParams, { financialRequestAmount: NaN })
+
+      const errors = PostValidator.validate(postParams)
+      expect(errors).to.have.lengthOf(1)
+      expect(errors).to.contain('financial requests must be between 0 and 5,000')
+    })
+  })
+})
