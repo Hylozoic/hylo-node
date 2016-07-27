b1b4cf8f9751d145b3f5112821ee163b3f2daaa5
Lawrence Wang <l@lw.io>
2016-07-26 18:27:36 -0700

    fix clickthrough tracking in emails

--- test/unit/services/digest2.test.js
+++ test/unit/services/digest2.test.js
@@ -194,9 +194,13 @@ describe('community digest v2', () => {
       const data = {
         community_id: '77',
         community_name: 'foo',
+        community_url: 'https://www.hylo.com/c/foo',
         requests: [],
         offers: [
-          {id: 1, title: 'Hi', user: u4.attributes, comments: []}
+          {
+            id: 1, title: 'Hi', user: u4.attributes, comments: [],
+            url: 'https://www.hylo.com/p/1'
+          }
         ],
         conversations: [
           {
@@ -204,23 +208,27 @@ describe('community digest v2', () => {
             comments: [
               {id: 3, user: user.pick('id', 'avatar_url'), text: 'Na'},
               {id: 4, user: u2.attributes, text: 'Woa'}
-            ]
+            ],
+            url: 'https://www.hylo.com/p/2'
           }
         ]
       }
 
-      return personalizeData(user, data).then(newData =>
+      return personalizeData(user, data).then(newData => {
+        const clickthroughParams = `?ctt=digest_email&cti=${user.id}&ctcn=foo`
         expect(newData).to.deep.equal(merge({}, data, {
           offers: [
             {
               id: 1, title: 'Hi', user: u4.attributes,
-              reply_url: Email.postReplyAddress(1, user.id)
+              reply_url: Email.postReplyAddress(1, user.id),
+              url: 'https://www.hylo.com/p/1' + clickthroughParams
             }
           ],
           conversations: [
             {
               id: 2, title: 'Ya', user: u3.attributes,
               reply_url: Email.postReplyAddress(2, user.id),
+              url: 'https://www.hylo.com/p/2' + clickthroughParams,
               comments: [
                 {id: 3, user: user.pick('id', 'avatar_url'), text: 'Na'},
                 {id: 4, user: u2.attributes, text: 'Woa'}
@@ -231,12 +239,14 @@ describe('community digest v2', () => {
             name: user.get('name'),
             avatar_url: user.get('avatar_url')
           },
-          email_settings_url: Frontend.Route.userSettings() + '?expand=account',
+          email_settings_url: Frontend.Route.userSettings() + clickthroughParams + '&expand=account',
           form_action_url: Frontend.Route.emailPostForm(),
           form_token: Email.postCreationToken(77, user.id),
           tracking_pixel_url: Analytics.pixelUrl('Digest', {userId: user.id, community: 'foo'}),
-          subject: `New activity from ${u4.name}, ${u3.name}, and 2 others`
-        })))
+          subject: `New activity from ${u4.name}, ${u3.name}, and 2 others`,
+          community_url: 'https://www.hylo.com/c/foo' + clickthroughParams
+        }))
+      })
     })
   })
 
@@ -282,6 +292,7 @@ describe('community digest v2', () => {
 
     it('calls SendWithUs with expected data', function () {
       this.timeout(10000)
+      const clickthroughParams = `?ctt=digest_email&cti=${u1.id}&ctcn=${community.get('name')}`
 
       return sendAllDigests('daily').then(result => {
         expect(result).to.deep.equal([[community.id, 1]])
@@ -291,7 +302,7 @@ describe('community digest v2', () => {
           community_id: community.id,
           community_name: community.get('name'),
           community_avatar_url: community.get('avatar_url'),
-          community_url: Frontend.Route.community(community) + '?ctt=digest_email',
+          community_url: Frontend.Route.community(community) + clickthroughParams,
           time_period: 'yesterday',
           subject: `New activity from ${u2.get('name')}`,
           requests: [],
@@ -301,7 +312,7 @@ describe('community digest v2', () => {
               id: post.id,
               title: post.get('name'),
               reply_url: Email.postReplyAddress(post.id, u1.id),
-              url: Frontend.Route.post(post),
+              url: Frontend.Route.post(post) + clickthroughParams,
               user: u2.pick('id', 'avatar_url', 'name'),
               comments: []
             }
@@ -313,7 +324,7 @@ describe('community digest v2', () => {
             userId: u1.id,
             community: community.get('name')
           }),
-          email_settings_url: Frontend.Route.userSettings() + '?expand=account'
+          email_settings_url: Frontend.Route.userSettings() + clickthroughParams + '&expand=account'
         })
       })
     })
