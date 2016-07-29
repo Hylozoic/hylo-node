79da426bb0e37c52b4fdee3a32448506809567b5
Aaron Edwards <aedwards@thoughtworks.com>
2016-07-29 16:09:47 +1000

    Seed scripts are now more robust

--- seeds/communities.js
+++ seeds/communities.js
@@ -1,11 +1,10 @@
-'use strict';
+'use strict'
 
-exports.seed = function(knex, Promise) {
-    return Promise.join(
-        // Deletes ALL existing entries
-        knex('community').del(),
-
-        // Inserts seed entries
-        knex('community').insert({id: 1, name: 'starter-posts', slug: 'starter-posts'})
-    );
-};
+exports.seed = function (knex, Promise) {
+  return knex('post_community').del()
+    .then(() => knex('users_community').del())
+    .then(() => knex('community').del())   // Deletes ALL existing entries
+    .then(() => knex('community')
+                    .insert({id: 1, name: 'starter-posts', slug: 'starter-posts'})
+  )
+}
