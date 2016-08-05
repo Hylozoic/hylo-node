4a02410e7d6448a5b83b600e78a399e5180600b0
Declan Hart <dhart@thoughtworks.com>
2016-08-05 16:11:18 +1000

    EP/DH #6 intergrate with hitfin OAuth

--- config/passport-hitfin-oauth.js
+++ config/passport-hitfin-oauth.js
@@ -3,15 +3,15 @@ var util = require('util')
     , InternalOAuthError = require('passport-oauth').InternalOAuthError;
 
 // TODO: Use HitFin OAuth instead of google (this is pretty much a copy of passport-google-oauth/oauth2.js ATM)
-const AUTH_URL = process.env.HITFIN_API_URL + '/o/oauth2/auth';
-const TOKEN_URL = process.env.HITFIN_API_URL + '/o/oauth2/token';
+const AUTH_URL = process.env.HITFIN_API_URL  + '/oauth/authorize'
+const TOKEN_URL = process.env.HITFIN_API_URL + '/oauth/token'
 const PROVIDER_NAME = 'hit-fin';
 
 function Strategy(options, verify) {
     options = options || {};
     options.authorizationURL = options.authorizationURL || AUTH_URL;
     options.tokenURL = options.tokenURL || TOKEN_URL;
-    options.scope = options.scope || ['email'];
+    options.scope = options.scope || [ 'user', 'wallet' ];
     options.clientSecret = options.clientSecret;
     options.skipUserProfile = true;
     OAuth2Strategy.call(this, options, verify);
