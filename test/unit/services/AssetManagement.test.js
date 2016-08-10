c11acc03bb195ace4ad1ec7d1a443a2ee99a9122
Lawrence Wang <l@lw.io>
2016-08-10 13:31:35 -0700

    fix a bug in AssetManagement
    it wasn’t rejecting the promise if the upload to S3 failed

---
+++ test/unit/services/AssetManagement.test.js
@@ -0,0 +1,32 @@
+require('../../setup')
+const factories = require('../../setup/factories')
+const defaultAvatarUrl = 'http://hsylo-app.s3.amazonaws.com/misc/default_community_avatar.png'
+
+describe('AssetManagement', () => {
+  var community, origBucket
+
+  before(() => {
+    origBucket = process.env.AWS_S3_BUCKET
+    process.env.AWS_S3_BUCKET = ''
+    community = factories.community()
+    return community.save({avatar_url: defaultAvatarUrl})
+  })
+
+  after(() => {
+    process.env.AWS_S3_BUCKET = origBucket
+  })
+
+  describe('copyAsset', () => {
+    it('throws an error if misconfigured', () => {
+      const promise = AssetManagement.copyAsset(community, 'community', 'avatar_url')
+      return expect(promise).to.eventually.be.rejected
+    })
+  })
+
+  describe('resizeAsset', () => {
+    it('throws an error if misconfigured', () => {
+      const promise = AssetManagement.resizeAsset(community, 'avatar_url', {width: 200, height: 200})
+      return expect(promise).to.eventually.be.rejected
+    })
+  })
+})
