159c3b369793cb5cc5ffbc89aafebadbc76a0a95
Lawrence Wang <l@lw.io>
2016-08-15 17:38:07 -0700

    remove memberships from fetchLiveStatus

--- api/controllers/LiveStatusController.js
+++ api/controllers/LiveStatusController.js
@@ -7,17 +7,13 @@ module.exports = {
     const communityId = req.param('communityId')
 
     return Promise.join(
-      User.find(req.session.userId, {withRelated: ['memberships']}),
+      User.find(req.session.userId),
       communityId && Community.find(communityId),
       (user, com) =>
         fetchAndPresentFollowed(get('id', com), user.id)
         .then(left_nav_tags => res.ok({
           new_notification_count: user.get('new_notification_count'),
-          left_nav_tags,
-          memberships: user.relations.memberships.map(m => ({
-            id: m.id,
-            new_notification_count: m.get('new_notification_count')
-          }))
+          left_nav_tags
         })))
   }
 }
