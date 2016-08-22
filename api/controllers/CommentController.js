3e6efbead5f4f06e0ebea59802f34500cf021b4e
Connor Turland <connorturland@gmail.com>
2016-08-22 15:26:34 -0400

    comment lists update when there's new comments

--- api/controllers/CommentController.js
+++ api/controllers/CommentController.js
@@ -33,6 +33,12 @@ const createComment = function (commenterId, text, post, tagDescriptions) {
     .tap(comment => comment.createActivities())
     .tap(comment => post.addFollowers(newFollowers, commenterId))
     .tap(() => updateRecentComments(post.id))
+    .tap(() => {
+      Object.keys(sails.io.sockets.sockets).forEach(function(k, index) {
+        var socket = sails.io.sockets.sockets[k]
+        socket.emit('p/' + post.id + '/comment_added')
+      })
+    })
   })
 }
 
