f4ac355bd01f88d7da85b41cdbaf13e9b5b25665
Jessie Wei <jessieweiyi@gmail.com>
2016-08-26 14:42:45 +1000

    removed the expired date of the redis cached and used the native redis cache expiration

--- api/services/Hitfin.js
+++ api/services/Hitfin.js
@@ -3,15 +3,12 @@ const bluebird = require('bluebird')
 const HitfinAuthenticate = require('../../lib/hitfin/Authenticate')
 const HitfinUser = require('../../lib/hitfin/User')
 const NAME_HITFIN_ACCESS_TOKEN = 'hitfin_syndicate_manager_access_token'
-const EXPIRED_HITFIN_ACCESS_TOKEN = 'hitfin_syndicate_manager_access_token_expired'
 
 const getSyndicateManagerAccessTokenFromHitfin = function(client){
   return HitfinAuthenticate.getAccessToken(process.env.HITFIN_CLIENT_ID, process.env.HITFIN_CLIENT_SECRET).
     then((token) => {
       client.set(NAME_HITFIN_ACCESS_TOKEN, token)
-      var expiredDate = new Date();
-      expiredDate.setDate(expiredDate.getDate() + 1);
-      client.set(EXPIRED_HITFIN_ACCESS_TOKEN, expiredDate.toISOString())
+      client.expire(NAME_HITFIN_ACCESS_TOKEN, 60 * 60 * 23)
       client.quit()
       return token
     })
@@ -36,17 +33,8 @@ module.exports = {
     //get the access token from redis. if not exists then get from hitfin and save it inside redis
     return client.getAsync(NAME_HITFIN_ACCESS_TOKEN).then(function(token) {
       if(token){
-        return client.getAsync(EXPIRED_HITFIN_ACCESS_TOKEN).then(function(expiredDate) {
-          if(expiredDate && new Date(expiredDate) >= new Date() )
-          {
-            client.quit()
-            return token
-          }
-          else
-          {
-            return getSyndicateManagerAccessTokenFromHitfin(client)
-          }
-        })
+        client.quit()
+        return token
       }
       else{
         return getSyndicateManagerAccessTokenFromHitfin(client)
