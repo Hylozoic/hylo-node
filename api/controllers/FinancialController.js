e029e8495403a98adb718a2eece9b10229061aff
Robert Blasetti <rblasett@thoughtworks.com>
2016-08-30 11:54:26 +1000

    JW/RB #26 - Exctracted user balance from financial controller into lib/user

--- api/controllers/FinancialController.js
+++ api/controllers/FinancialController.js
@@ -1,6 +1,7 @@
 const passport = require('passport')
 const rollbar = require('rollbar')
 const request = require('request')
+const User = require('../../lib/Hitfin/User')
 
 const getAccessToken = function (req) {
   return UserExternalData.find(req.session.userId, 'hit-fin').then( user_data => {
@@ -14,37 +15,9 @@ const getAccessToken = function (req) {
 function getUserBalance (req, res, next) {
 
   return getAccessToken(req).then( accessToken => {
-    return new Promise((resolve, reject) => {
-      var respond = (error) => {
-        if (error && error.stack) rollbar.handleError(error, req)
-
-        return resolve(res.view('popupDone', {
-          error,
-          context: req.session.authContext || 'oauth',
-          layout: null,
-          returnDomain: req.session.returnDomain
-        }))
-      }
-      request({
-        url: process.env.HITFIN_API_URL + '/api/stabletoken/balances',
-        headers: {
-          'Authorization': 'Bearer ' + accessToken
-        }
-      }, function(error, response, body){
-        if(error){
-          reject(error);
-        }
-        else if(response.statusCode >=400){
-          reject(JSON.parse(body));
-        }
-        else{
-          console.log(JSON.parse(body))
-          resolve(JSON.parse(body));
-        }
-      })
+      return User.getWalletBalance(accessToken)
     })
-  })
-}
+  }
 
 module.exports = {
     getBalance: function(req, res, next){
