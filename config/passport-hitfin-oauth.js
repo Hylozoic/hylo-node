f4327f4dcb69109d9b2a640e171c65fce7969614
Declan Hart <dhart@thoughtworks.com>
2016-09-01 15:04:19 +1000

    DH/JW get authorize working

--- config/passport-hitfin-oauth.js
+++ config/passport-hitfin-oauth.js
@@ -3,7 +3,7 @@ var util = require('util')
     , InternalOAuthError = require('passport-oauth').InternalOAuthError;
 
 // TODO: Use HitFin OAuth instead of google (this is pretty much a copy of passport-google-oauth/oauth2.js ATM)
-const AUTH_URL = process.env.HITFIN_API_URL  + '/oauth/authorise'
+const AUTH_URL = process.env.HITFIN_API_URL  + '/oauth/authorize'
 const TOKEN_URL = process.env.HITFIN_API_URL + '/oauth/token'
 const PROVIDER_NAME = 'hit-fin';
 
@@ -11,7 +11,7 @@ function Strategy(options, verify) {
     options = options || {};
     options.authorizationURL = options.authorizationURL || AUTH_URL;
     options.tokenURL = options.tokenURL || TOKEN_URL;
-    options.scope = options.scope || [ 'email' ];
+    options.scope = options.scope || [ 'user', 'wallet' ];
     options.clientSecret = options.clientSecret;
     options.skipUserProfile = true;
     OAuth2Strategy.call(this, options, verify);
