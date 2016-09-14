5fe69f937d5559e6a7ae9b534b9735ddde6ea8b9
Lawrence Wang <l@lw.io>
2016-09-13 21:42:36 -0700

    fix inCommunities to work with renamed params

--- api/policies/inCommunities.js
+++ api/policies/inCommunities.js
@@ -1,15 +1,15 @@
 module.exports = function (req, res, next) {
   if (Admin.isSignedIn(req)) return next()
 
-  var communityIds = req.param('communities')
+  var communityIds = req.param('community_ids')
   var userId = req.session.userId
 
   return Membership.query(q => {
     q.whereIn('community_id', communityIds)
     q.where('user_id', userId)
   }).fetchAll()
-    .then(mships => Promise.map(communityIds, id =>
-      mships.find(m =>
-        m.get('community_id') === id || Community.inNetworkWithUser(id, userId))))
-    .then(ok => _.every(ok) ? next() : res.forbidden())
+  .then(mships => Promise.map(communityIds, id =>
+    mships.find(m =>
+      m.get('community_id') === id || Community.inNetworkWithUser(id, userId))))
+  .then(ok => _.every(ok) ? next() : res.forbidden())
 }
