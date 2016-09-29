6d5c1fde9be0b09a2bd74fd528498e042032d12c
Lawrence Wang <l@lw.io>
2016-09-28 17:32:13 -0700

    standardize formatting

--- api/services/PlayCrypto.js
+++ api/services/PlayCrypto.js
@@ -1,23 +1,22 @@
-var crypto = require('crypto');
+import crypto from 'crypto'
 
-var encryptionType = 'aes-128-ecb',
-  key = function() {
-    return new Buffer(process.env.PLAY_APP_SECRET.substring(0, 16), 'utf-8');
-  };
+const encryptionType = 'aes-128-ecb'
+
+function key () {
+  return new Buffer(process.env.PLAY_APP_SECRET.substring(0, 16), 'utf-8')
+}
 
 module.exports = {
+  encrypt: function (text) {
+    var cipher = crypto.createCipheriv(encryptionType, key(), '')
 
-  encrypt: function(text) {
-    var cipher = crypto.createCipheriv(encryptionType, key(), '');
-
-    cipher.end(text);
-    return cipher.read().toString('hex');
+    cipher.end(text)
+    return cipher.read().toString('hex')
   },
 
-  decrypt: function(code) {
-    var decipher = crypto.createDecipheriv(encryptionType, key(), '');
-    decipher.end(new Buffer(code, 'hex'));
-    return decipher.read().toString();
+  decrypt: function (code) {
+    var decipher = crypto.createDecipheriv(encryptionType, key(), '')
+    decipher.end(new Buffer(code, 'hex'))
+    return decipher.read().toString()
   }
-
-};
+}
