76914bc868fee3f84210b49a26d63ae256e13435
Robbie Carlton <robbie.carlton@gmail.com>
2016-10-18 16:19:29 -0700

    make tests shorter

--- test/unit/services/TagPresenter.test.js
+++ test/unit/services/TagPresenter.test.js
@@ -2,68 +2,52 @@ var root = require('root-path')
 var setup = require(root('test/setup'))
 var factories = require(root('test/setup/factories'))
 import moment from 'moment'
-import { find } from 'lodash/fp'
+import { find, times } from 'lodash/fp'
 import { fetchAndPresentForCommunity } from '../../../api/services/TagPresenter.js'
 describe('fetchAndPresentForCommunity', () => {
-  var fixtures
+  var u1, u2, u3, u4, c1, t1, t2, t3, t4, t5
 
   before(() => {
     return setup.clearDb()
-    .then(() => Promise.props({
-      u1: factories.user().save(),
-      u2: factories.user().save(),
-      u3: factories.user().save(),
-      u4: factories.user().save(),
-      c1: factories.community().save(),
-      t1: new Tag({name: 'tagOne'}).save(),
-      t2: new Tag({name: 'tagTwo'}).save(),
-      t3: new Tag({name: 'tagThree'}).save(),
-      t4: new Tag({name: 'tagFourThatisnotinthecommunity'}).save()
+    .then(() => {
+      [ u1, u2, u3, u4 ] = times(() => factories.user(), 4)
+      ;[ t1, t2, t3, t4 ] = ['tagOne', 'tagTwo', 'tagThree', 'tagFourThatisnotinthecommunity']
+        .map(name => new Tag({name}))
+      c1 = factories.community()
+      return Promise.map([u1, u2, u3, u4, t1, t2, t3, t4, c1], x => x.save())
     })
-    .then(props => fixtures = props))
     .then(() => Promise.join(
-      new CommunityTag({
-        user_id: fixtures.u1.id, tag_id: fixtures.t1.id, community_id: fixtures.c1.id
-      }).save(),
-      new CommunityTag({
-        user_id: fixtures.u1.id, tag_id: fixtures.t2.id, community_id: fixtures.c1.id
-      }).save(),
-      new CommunityTag({
-        user_id: fixtures.u1.id, tag_id: fixtures.t3.id, community_id: fixtures.c1.id
-      }).save(),
-      new TagFollow({
-        user_id: fixtures.u1.id, tag_id: fixtures.t2.id, community_id: fixtures.c1.id
-      }).save(),
-      new TagFollow({
-        user_id: fixtures.u2.id, tag_id: fixtures.t2.id, community_id: fixtures.c1.id
-      }).save(),
-      new TagFollow({
-        user_id: fixtures.u3.id, tag_id: fixtures.t2.id, community_id: fixtures.c1.id
-      }).save(),
-      new TagFollow({
-        user_id: fixtures.u4.id, tag_id: fixtures.t2.id, community_id: fixtures.c1.id
-      }).save(),
-      new TagFollow({
-        user_id: fixtures.u1.id, tag_id: fixtures.t1.id, community_id: fixtures.c1.id
-      }).save(),
-      new TagFollow({
-        user_id: fixtures.u2.id, tag_id: fixtures.t1.id, community_id: fixtures.c1.id
-      }).save()
-    ))
+      Promise.map([[u1.id, t1.id], [u1.id, t2.id], [u1.id, t3.id]], uIdtId =>
+        new CommunityTag({
+          user_id: uIdtId[0], tag_id: uIdtId[1], community_id: c1.id
+        }).save()
+      ),
+      Promise.map([
+        [u1.id, t1.id],
+        [u2.id, t1.id],
+        [u1.id, t2.id],
+        [u2.id, t2.id],
+        [u3.id, t2.id],
+        [u4.id, t2.id]
+      ], uIdtId =>
+        new TagFollow({
+          user_id: uIdtId[0], tag_id: uIdtId[1], community_id: c1.id
+        }).save()
+      )))
     .then(() => {
-      fixtures.t5 = new Tag({name: 'tagFive'})
-      return fixtures.t5.save()
+      t5 = new Tag({name: 'tagFive'})
+      return t5.save()
     })
     .then(() => {
       new CommunityTag({
-        user_id: fixtures.u1.id, tag_id: fixtures.t5.id, community_id: fixtures.c1.id,
+        user_id: u1.id, tag_id: t5.id, community_id: c1.id,
         created_at: moment().add(1, 'day')
       }).save()
     })
   })
 
   it('fetches and presents, for Community', () => {
-    return fetchAndPresentForCommunity(fixtures.c1.id)
+    return fetchAndPresentForCommunity(c1.id)
     .then(tags => {
       expect(tags.total).to.equal(4)
       const tagOne = find(t => t.name === 'tagOne', tags.items)
@@ -82,7 +66,7 @@ describe('fetchAndPresentForCommunity', () => {
   })
 
   it('sorts by popularity with popularity param', () => {
-    return fetchAndPresentForCommunity(fixtures.c1.id, {sort: 'popularity'})
+    return fetchAndPresentForCommunity(c1.id, {sort: 'popularity'})
     .then(tags => {
       expect(tags.total).to.equal(4)
       expect(tags.items.map(i => i.name)).to.deep.equal([
