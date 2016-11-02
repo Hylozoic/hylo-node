b58e9f5126f76cbda367038c8c600c5380cafc01
Connor Turland <connorturland@gmail.com>
2016-11-02 15:11:30 -0700

    intention should also be in seeds file

--- seeds/tags.js
+++ seeds/tags.js
@@ -4,6 +4,7 @@ exports.seed = function (knex, Promise) {
   return knex('tags').del()
     .then(() => knex('tags').insert([
       {id: 1, name: 'offer'},
-      {id: 2, name: 'request'}
+      {id: 2, name: 'request'},
+      {id: 3, name: 'intention'}
     ]))
 }
