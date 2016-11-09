8f3f44004aecbb73410d8fb17efd7d8a95636ff1
Connor Turland <connorturland@gmail.com>
2016-11-09 15:50:01 -0800

    socket messages shouldn't send running tests

--- api/services/Websockets.js
+++ api/services/Websockets.js
@@ -1,4 +1,5 @@
 export function pushToSockets (room, messageType, payload, socketToExclude) {
+  if (process.env.NODE_ENV === 'test') return Promise.resolve()
   // for security reasons, only sockets that passed the checkAndSetPost policy
   // get subscribed to the comment stream for that post
   sails.sockets.broadcast(room, messageType, payload, socketToExclude)
