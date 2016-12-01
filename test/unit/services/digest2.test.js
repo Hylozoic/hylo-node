e0057486f70f8956b1c2d8c0a0f6a933d6ea4b19
Robbie Carlton <robbie.carlton@gmail.com>
2016-12-01 15:40:21 -0800

    rename to Email Version in tracking pixel

--- test/unit/services/digest2.test.js
+++ test/unit/services/digest2.test.js
@@ -367,7 +367,7 @@ describe('community digest v2', () => {
           tracking_pixel_url: Analytics.pixelUrl('Digest', {
             userId: u1.id,
             community: community.get('name'),
-            versionName: u1.id % 2 === 0 ? 'multi-reply-form' : 'default'
+            'Email Version': u1.id % 2 === 0 ? 'multi-reply-form' : 'default'
           }),
           email_settings_url: Frontend.Route.userSettings() + clickthroughParams + '&expand=account'
         })
