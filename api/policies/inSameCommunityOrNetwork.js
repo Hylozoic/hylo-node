e66883c71cf6c68abbaf13acf543f26d4565bfbc
Lawrence Wang <l@lw.io>
2016-12-01 13:12:09 -0800

    cleanup
    was trying to get rid of a weird warning about orphaned promises (which
    goes away if I remove the `catch` clause — !??), unsuccessfully.

--- api/policies/inSameCommunityOrNetwork.js
+++ api/policies/inSameCommunityOrNetwork.js
@@ -5,11 +5,8 @@ module.exports = function (req, res, next) {
   if (isNaN(Number(req.param('userId')))) return res.notFound()
 
   Membership.inSameCommunity([req.session.userId, req.param('userId')])
-  .then(same => {
-    if (same) return next()
-
-    return Membership.inSameNetwork(req.session.userId, req.param('userId'))
-    .then(same => (same ? next() : res.forbidden()))
-  })
+  .then(same => same ? next()
+    : Membership.inSameNetwork(req.session.userId, req.param('userId'))
+      .then(same => same ? next() : res.forbidden()))
   .catch(res.serverError)
 }
