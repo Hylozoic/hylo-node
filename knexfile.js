158b0264092a458d26387a3c5b0a92253f7ee49a
Loren Johnson <loren@hellovenado.com>
2016-12-11 14:53:13 -0800

    Make seeds runnable on CLI

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,12 @@
-import { merge } from 'lodash'
 require('dotenv').load()
+
+// LEJ: ES6 import not working for a commandline run of knex, 
+//      replacing with require
+// (e.g. knex seed:run)
+//
+// import { merge } from 'lodash'
+const _ = require('lodash')
+const merge = _.merge
 
 if (!process.env.DATABASE_URL) {
   throw new Error('process.env.DATABASE_URL must be set')
