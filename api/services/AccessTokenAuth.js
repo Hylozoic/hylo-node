142b8e7f9071f5b81916f05f4085e8a62973d0bf
Connor Turland <connorturland@gmail.com>
2016-12-19 16:22:46 +0000

    fix up AccessTokenAuth to use linkedaccount

--- api/services/AccessTokenAuth.js
+++ api/services/AccessTokenAuth.js
@@ -13,8 +13,9 @@ var AccessTokenAuth = module.exports = {
     const token = req.body.access_token || req.query.access_token || req.headers['x-access-token']
     if (!token) return Promise.resolve()
     return User.query(function (qb) {
-       qb.leftJoin('tokens', 'users.id', 'tokens.user_id')
-       qb.where('tokens.token', '=', token)
+       qb.leftJoin('linked_account', 'users.id', 'linked_account.user_id')
+       qb.where('linked_account.provider_user_id', '=', token)
+       qb.andWhere('linked_account.provider_key', '=', 'token')
     })
     .fetch()
     .then(user => {
