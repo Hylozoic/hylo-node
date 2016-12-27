a28c3f348a940f7a1c02cc54301875e5a60469aa
Lawrence Wang <l@lw.io>
2016-12-27 12:52:15 -0800

    remove thanks when deleting comments

--- api/services/PostManagement.js
+++ api/services/PostManagement.js
@@ -1,11 +1,17 @@
-const removeComments = (postId, knex) => {
-  return knex('comments').where('post_id', postId).pluck('id')
+const removeComments = (postId, trx) =>
+  trx('comments').where('post_id', postId).pluck('id')
   .then(ids => {
     if (ids.length === 0) return
-    return knex('comments_tags').where('comment_id', 'in', ids).del()
-    .then(() => knex('comments').where('id', 'in', ids).del())
+
+    const remove = (table, column = 'comment_id') =>
+      trx(table).where(column, 'in', ids).del()
+
+    return Promise.all([
+      remove('thanks'),
+      remove('comments_Tags')
+    ])
+    .then(() => remove('comments', 'id'))
   })
-}
 
 export const removePost = postId => {
   return bookshelf.transaction(trx => {
