ab667306fd0bd0399806071cebcf53a94aa7f4f5
Lawrence Wang <l@lw.io>
2017-02-15 16:45:54 -0800

    add total counts for pagination to our graphql api

--- api/graphql/util/applyPagination.js
+++ api/graphql/util/applyPagination.js
@@ -1,9 +1,16 @@
-export default function applyPagination (query, { first, cursor, order, column = 'id' }) {
+import { countTotal } from '../../../lib/util/knex'
+
+export const PAGINATION_TOTAL_COLUMN_NAME = '__total'
+
+export default function applyPagination (query, tableName, { first, cursor, order, column = 'id' }) {
   query = query.orderBy(column, order)
-  if (first) query = query.limit(first)
+  if (first) {
+    query = query.limit(first)
+    query = countTotal(query, tableName, PAGINATION_TOTAL_COLUMN_NAME)
+  }
   if (cursor) {
     const op = order === 'asc' ? '>' : '<'
-    query = query.where(column, op, cursor)
+    query = query.where(`${tableName}.${column}`, op, cursor)
   }
   return query
 }
