5340f7b15951142327394e63a7e7e37a6a98f43f
Rich Churcher <rich.churcher@gmail.com>
2017-04-08 17:03:26 +1200

    Need communities too

--- api/graphql/makeModels.js
+++ api/graphql/makeModels.js
@@ -54,7 +54,7 @@ export default function makeModels (userId, isAdmin) {
         'url',
         'location'
       ],
-      relations: ['posts'],
+      relations: ['communities', 'posts'],
       filter: nonAdminFilter(q => {
         q.where('users.id', 'in', Membership.query().select('user_id')
           .where('community_id', 'in', myCommunityIds()))
