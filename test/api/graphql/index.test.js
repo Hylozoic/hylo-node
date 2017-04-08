62d7820010e1c895fb0d5f28f32e72e1c44e58bf
Lawrence Wang <l@lw.io>
2017-04-07 17:42:40 -0700

    add more nesting to the graphql test query

--- test/api/graphql/index.test.js
+++ test/api/graphql/index.test.js
@@ -3,7 +3,7 @@ import '../../setup'
 import factories from '../../setup/factories'
 
 describe('graphql request handler', () => {
-  var handler, req, res, user
+  var handler, req, res, user, community, post
 
   before(() => {
     handler = createRequestHandler()
@@ -16,14 +16,34 @@ describe('graphql request handler', () => {
         query: `{
           me {
             name
+            memberships {
+              community {
+                name
+              }
+            }
+            posts {
+              title
+              communities {
+                name
+              }
+            }
           }
         }`
       }
     })
 
     user = factories.user()
-    return user.save()
-    .then(() => { req.session = {userId: user.id} })
+    community = factories.community()
+    post = factories.post()
+    return Promise.all([user.save(), community.save()])
+    .then(() => {
+      req.session = {userId: user.id}
+      return post.save({user_id: user.id})
+    })
+    .then(() => Promise.all([
+      community.posts().attach(post.id),
+      community.users().attach({user_id: user.id, active: true})
+    ]))
   })
 
   it('can handle a simple query', () => {
@@ -32,7 +52,24 @@ describe('graphql request handler', () => {
       expect(JSON.parse(res.body)).to.deep.equal({
         data: {
           me: {
-            name: user.get('name')
+            name: user.get('name'),
+            memberships: [
+              {
+                community: {
+                  name: community.get('name')
+                }
+              }
+            ],
+            posts: [
+              {
+                title: post.get('name'),
+                communities: [
+                  {
+                    name: community.get('name')
+                  }
+                ]
+              }
+            ]
           }
         }
       })
