b70b16cc5da362d8a1de1b8c01f2bb7e9407746f
Rich Churcher <rich.churcher@gmail.com>
2017-05-09 11:37:25 +1200

    Typos

--- seeds/migrations.js
+++ seeds/migrations.js
@@ -4,19 +4,19 @@ const path = require('path')
 exports.seed = function (knex, Promise) {
   return knex('knex_migrations').del()
     .then(() => {
-      const files = readdirSync(path.join(__dirname, 'migrations'))
+      const files = fs.readdirSync(path.join(__dirname, '..', 'migrations'))
       return addMigrations(knex, Promise, files.filter(f => f.slice(-3) === '.js'))
-    }
+    })
 }
 
 // Add all migrations in directory to knex_migrations
 // (any .js file in the directory is assumed to be a migration)
 function addMigrations (knex, Promise, migrations) {
   return Promise.all(
-    migrations.map(name => knex.insert({
+    migrations.map(name => knex('knex_migrations').insert({
       name,
       batch: 1,
-      migration_time: Date.now()
-    })
+      migration_time: knex.fn.now()
+    }))
   )
 }
