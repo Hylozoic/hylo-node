8490175f612dfa47c73e515f3cccdfe4d06d67d5
Rich Churcher <rich.churcher@gmail.com>
2017-05-09 11:00:19 +1200

    Seed everything in the migrations directory

---
+++ seeds/migrations.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+
+exports.seed = function (knex, Promise) {
+  return knex('knex_migrations').del()
+    .then(() => {
+      const files = readdirSync(path.join(__dirname, 'migrations'))
+      return addMigrations(knex, Promise, files.filter(f => f.slice(-3) === '.js'))
+    }
+}
+
+// Add all migrations in directory to knex_migrations
+// (any .js file in the directory is assumed to be a migration)
+function addMigrations (knex, Promise, migrations) {
+  return Promise.all(
+    migrations.map(name => knex.insert({
+      name,
+      batch: 1,
+      migration_time: Date.now()
+    })
+  )
+}
