f9a11ad69ae141f474a3bb296e8a08d2b8928b87
Rich Churcher <rich.churcher@gmail.com>
2017-05-12 11:56:48 +1200

    Test: find

--- test/unit/models/UserConnection.test.js
+++ test/unit/models/UserConnection.test.js
@@ -4,11 +4,13 @@ const factories = require(root('test/setup/factories'))
 
 describe.only('UserConnection', () => {
   let u
+  let u2
 
   beforeEach(() => {
     u = factories.user()
+    u2 = factories.user()
     return setup.clearDb()
-      .then(() => u.save())
+      .then(() => Promise.all([u.save(), u2.save()]))
   })
 
   describe('create', () => {
@@ -37,28 +39,49 @@ describe.only('UserConnection', () => {
   })
 
   describe('createOrUpdate', () => {
-    let u2
-
-    beforeEach(() => {
-      u2 = factories.user()
-      return u2.save()
-    })
-
     it('creates a connection if one does not already exist', () => {
       return UserConnection.createOrUpdate(u.get('id'), u2.get('id'), 'message')
-        .then(({ changed }) => expect(changed).to.deep.equal({}))
+        .then(() => UserConnection.fetchAll())
+        .then(connections => expect(connections.length).to.equal(1))
     })
 
     it('updates a connection if one already exists', () => {
-      const c = factories.userConnection({ user_id: u.get('id'), other_user_id: u2.get('id') })
+      const user_id = u.get('id')
+      const other_user_id = u2.get('id')
+      const c = factories.userConnection({ user_id, other_user_id })
       return c.save()
-        .then(() => UserConnection.createOrUpdate(u.get('id'), u2.get('id'), 'message'))
+        .then(() => UserConnection.createOrUpdate(user_id, other_user_id, 'message'))
         .then(() => UserConnection.fetchAll())
         .then(connections => {
           expect(connections.length).to.equal(1)
           const connection = connections.first()
           expect(connection.get('updated_at')).not.to.equal(c.get('updated_at'))
         })
+    })
+  })
+
+  describe('find', () => {
+    it('throws if user_id is missing', () => {
+      expect(() => UserConnection.find())
+        .to.throw(/Parameter user_id must be supplied/)
+    })
+
+    it('resolves with null if no matching connection exists', () => {
+      const user_id = u.get('id')
+      const other_user_id = u2.get('id')
+      const c = factories.userConnection({ user_id: other_user_id, other_user_id: user_id })
+      return c.save()
+        .then(() => UserConnection.find(user_id, other_user_id, 'message'))
+        .then(connection => expect(connection).to.equal(null))
+    })
+
+    it('finds a connection if a match exists', () => {
+      const user_id = u.get('id')
+      const other_user_id = u2.get('id')
+      const c = factories.userConnection({ user_id, other_user_id })
+      return c.save()
+        .then(() => UserConnection.find(user_id, other_user_id, 'message'))
+        .then(connection => expect(connection.get('id')).to.equal(c.get('id')))
     })
   })
 })
