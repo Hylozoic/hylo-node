782de839e891630b07876797bd6f49298ecdca45
Lawrence Wang <l@lw.io>
2017-05-16 22:55:35 -0700

    make grunt autotest stop on one failure

--- tasks/config/autotest.js
+++ tasks/config/autotest.js
@@ -1,6 +1,6 @@
 const gaze = require('gaze')
 const minimist = require('minimist')
-const child_process = require('child_process')
+const child = require('child_process')
 const debounce = require('lodash/debounce')
 
 module.exports = function (grunt) {
@@ -9,11 +9,11 @@ module.exports = function (grunt) {
 
     const argv = minimist(process.argv)
     const file = argv.file || argv.f || ''
-    const cmd = `npm test -s -- -R min ${file}`
+    const cmd = `npm test -s -- -b -R min ${file}`
 
     gaze(['api/**/*', 'lib/**/*', 'test/**/*'], function (_, watcher) {
       this.on('all', debounce(() => {
-        child_process.spawn('bash', ['-c', cmd], {stdio: 'inherit'})
+        child.spawn('bash', ['-c', cmd], {stdio: 'inherit'})
       }, 500, true))
     })
   })
