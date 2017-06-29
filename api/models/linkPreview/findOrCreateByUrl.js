bcd472acbff0a6abd2f804e733df1d5fea0d7599
Loren Johnson <loren@hellovenado.com>
2017-06-29 14:56:01 -0700

    Fixes

--- api/models/linkPreview/findOrCreateByUrl.js
+++ api/models/linkPreview/findOrCreateByUrl.js
@@ -1,27 +1,15 @@
 export default function findOrCreateByUrl (url) {
   return LinkPreview.find(url).then(preview => {
-    if (!preview) {
-      return LinkPreview.queue(url)
-      .then(() => {
-        return {status: 'queued'}
-      })
-    }
-
-    if (!preview.get('done')) {
-      return {status: 'loading'}
-    }
-
-    return Object.assign(
-      preview.pick(
-        'id',
-        'url',
-        'image_url',
-        'title',
-        'description',
-        'image_width',
-        'image_height'
-      ),
-      {status: 'loaded'}
+    if (!preview) return LinkPreview.queue(url)
+    if (!preview.get('done')) return
+    return preview.pick(
+      'id',
+      'url',
+      'image_url',
+      'title',
+      'description',
+      'image_width',
+      'image_height'
     )
   })
 }
