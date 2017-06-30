2eeadcb422e5ac9738a91a0fdd47f4516b2ca580
Loren Johnson <loren@hellovenado.com>
2017-06-30 10:31:40 -0700

    graphql config will handle this

--- api/models/linkPreview/findOrCreateByUrl.js
+++ api/models/linkPreview/findOrCreateByUrl.js
@@ -2,14 +2,6 @@ export default function findOrCreateByUrl (url) {
   return LinkPreview.find(url).then(preview => {
     if (!preview) return LinkPreview.queue(url)
     if (!preview.get('done')) return
-    return preview.pick(
-      'id',
-      'url',
-      'image_url',
-      'title',
-      'description',
-      'image_width',
-      'image_height'
-    )
+    return preview
   })
 }
