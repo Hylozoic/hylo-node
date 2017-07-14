59e64d48a8fc29d454af75c263b4d214a5f1449c
Loren Johnson <loren@hellovenado.com>
2017-07-14 14:20:33 -0400

    Fix updateNetwork

--- api/models/network/validateNetworkData.js
+++ api/models/network/validateNetworkData.js
@@ -1,7 +1,6 @@
-import { isEmpty } from 'lodash'
-
 export default function validateNetworkData (userId, data) {
   if (!data.name) {
     throw new Error('title can\'t be blank')
   }
+  return Promise.resolve(data)
 }
