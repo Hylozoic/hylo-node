580ac125d2d8687a006a7d7be5e8afa60119e493
Robbie Carlton <robbie.carlton@gmail.com>
2017-09-05 16:29:28 -0600

    canDeleteComment function

--- api/graphql/mutations.js
+++ api/graphql/mutations.js
@@ -295,11 +295,21 @@ export function createCommunity (userId, data) {
   })
 }
 
+export function canDeleteComment (userId, comment) {
+  if (comment.get('user_id') === userId) return Promise.resolve(true)
+  return comment.load('post.communities')
+  .then(comment => Promise.any(
+    comment.relations.post.relations.communities.map(c =>
+      Membership.hasModeratorRole(userId, c.id))
+  ))
+}
+
 export function deleteComment (userId, commentId) {
   return Comment.find(commentId)
-  .then(comment => {
-    if (comment.get('user_id') !== userId) throw new Error("You don't have permission to delete this comment")
-    return underlyingDeleteComment(comment, userId)
-  })
+  .then(comment => canDeleteComment(userId, comment)
+    .then(canDelete => {
+      if (!canDelete) throw new Error("You don't have permission to delete this comment")
+      return underlyingDeleteComment(comment, userId)
+    }))
   .then(() => ({success: true}))
 }
