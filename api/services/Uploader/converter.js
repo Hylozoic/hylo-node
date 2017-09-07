cbe624f9fd2e9943804cc18d24fd9d3c60fbd5d5
Lawrence Wang <l@lw.io>
2017-09-06 18:02:04 -0700

    add more crop/resize settings

--- api/services/Uploader/converter.js
+++ api/services/Uploader/converter.js
@@ -1,15 +1,12 @@
 import sharp from 'sharp'
 
-export const userAvatarUploadSettings = person => ({
-  id: person.id,
-  subject: 'user-avatar',
-  path: `user/${person.id}/avatar`,
-  convert: {width: 200, height: 200, fit: 'crop', rotate: 'exif'}
-})
-
 const sizes = {
   userAvatar: {width: 200, height: 200},
-  userBanner: {width: 1600, height: 600}
+  userBanner: {width: 1600, height: 600},
+  communityAvatar: {width: 160, height: 160},
+  communityBanner: {width: 1600, height: 600},
+  networkAvatar: {width: 160, height: 160},
+  networkBanner: {width: 1600, height: 600}
 }
 
 export function createConverterStream (type, id, strategy = 'center') {
