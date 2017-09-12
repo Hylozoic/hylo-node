4925337389d3369e767f0d023f3ccbf7fab5a703
Lawrence Wang <l@lw.io>
2017-09-11 18:51:52 -0700

    allow only network moderators to edit networks

--- api/graphql/mutations/network.js
+++ api/graphql/mutations/network.js
@@ -3,7 +3,11 @@ import underlyingUpdateNetwork from '../../models/network/updateNetwork'
 import convertGraphqlData from './convertGraphqlData'
 
 export function updateNetwork (userId, { id, data }) {
-  const convertedData = convertGraphqlData(data)
-  return validateNetworkData(userId, convertedData)
-  .then(() => underlyingUpdateNetwork(userId, id, convertedData))
+  return NetworkMembership.hasModeratorRole(userId, id)
+  .then(ok => {
+    if (!ok) throw new Error("You don't have permission to modify this network")
+    const convertedData = convertGraphqlData(data)
+    return validateNetworkData(userId, convertedData)
+    .then(() => underlyingUpdateNetwork(userId, id, convertedData))
+  })
 }
