24474c3ddba6641a16c4416694f59e3cd3ec88f3
Robbie Carlton <robbie.carlton@gmail.com>
2017-09-21 16:58:55 -0600

    remove console

--- api/policies/sessionAuth.js
+++ api/policies/sessionAuth.js
@@ -9,7 +9,6 @@ var fail = function (res) {
 
 module.exports = function (req, res, next) {
   if (UserSession.isLoggedIn(req)) {
-    console.log('userId', req.session.userId)
     return next()
   }
 
