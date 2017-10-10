7013cadcef2af362199db2558750a73df056b63d
Ray Marceau <razor2n4@gmail.com>
2017-10-10 12:31:35 -0700

    fixes bug where updateMigrationsTable was deleting already applied migrations after switching to an older branch.

--- migrations/scripts/updateMigrationsTable.js
+++ migrations/scripts/updateMigrationsTable.js
@@ -6,10 +6,28 @@ const path = require('path')
 
 console.info('Attempting to remove outdated migration filenames from database...')
 const migrations = fs.readdirSync(path.join(__dirname, '..'))
-  .filter(f => f.slice(-3) === '.js')
+.filter(f => f.slice(-3) === '.js')
+
+var newestMigrationTime
+
+if (migrations.length > 0) {
+  const timestamp = migrations[migrations.length - 1].split('_')[0]
+  newestMigrationTime = new Date(
+    timestamp.substring(0, 4),
+    Number(timestamp.substring(4, 6)) - 1,
+    timestamp.substring(6, 8),
+    timestamp.substring(8, 10),
+    timestamp.substring(10, 12),
+    timestamp.substring(12, 14)
+  )
+} else {
+  newestMigrationTime = new Date()
+}
+
 knex('knex_migrations')
-  .whereNotIn('name', migrations)
-  .del()
-  .then(n => console.info(`...${n} rows affected.`))
-  .then(() => knex.destroy())
-  .catch(console.error)
+.whereNotIn('name', migrations)
+.where('migration_time', '<', newestMigrationTime)
+.del()
+.then(n => console.info(`...${n} rows affected.`))
+.then(() => knex.destroy())
+.catch(console.error)
