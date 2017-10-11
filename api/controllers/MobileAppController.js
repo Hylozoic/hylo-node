2abf6e70b34e5faa8059aae8bc3b375e1aba0443
Andy Gimma <andy@hylo.com>
2017-10-11 12:23:59 -0500

    Return object based on mobile version

--- api/controllers/MobileAppController.js
+++ api/controllers/MobileAppController.js
@@ -2,6 +2,17 @@ module.exports = {
 
   updateInfo: function (req, res) {
     var result = {}
+    var iTunesItemIdentifier = '1002185140'
+    var forceUpdate = {
+      type: 'force',
+      title: 'A new version of the app is available',
+      iTunesItemIdentifier
+    }
+    var suggestUpdate = {
+      type: 'suggest',
+      title: 'An update is available',
+      iTunesItemIdentifier
+    }
 
     /*
     Format for result:
@@ -12,26 +23,29 @@ module.exports = {
       iTunesItemIdentifier: '1002185140'
     }
     */
-
-    switch (req.headers['ios-version']) {
+    // console.log('req.headers', req.headers)
+    var version = req.param('ios-version') || req.param('android-version')
+    switch (version) {
       case '1.6':
+        result = Object.assign(
+          suggestUpdate,
+          { message: 'The version you are using is not longer up to date. Please go to the App Store to update.' }
+        )
+        break
       case '1.65':
-        result = {
-          type: 'force',  // can be 'force' or 'suggest'
-          title: 'A new version of the app is available',
-          message: 'The version you are using is no longer compatible with the site. Please go to the App Store now to update',
-          iTunesItemIdentifier: '1002185140'
-        }
+        result = Object.assign(
+          forceUpdate,
+          { message: 'The version you are using is no longer compatible with the site. Please go to the App Store now to update' }
+        )
         break
       case undefined:
         break
+      default:
+        result = Object.assign(
+          forceUpdate,
+          { message: 'The version you are using is no longer compatible with the site. Please go to the App Store now to update' }
+        )
     }
-
-    switch (req.headers['android-version']) {
-      case undefined:
-        break
-    }
-
     res.ok(result)
   }
 }
