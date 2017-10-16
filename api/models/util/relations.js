56e2bfad92fa7bb315411d75b68d9844ff31d7ba
Rich Churcher <rich.churcher@gmail.com>
2017-10-16 13:33:01 +1300

    Allow rewriting property names

--- api/models/util/relations.js
+++ api/models/util/relations.js
@@ -1,35 +1,34 @@
 import { camelCase, mapKeys, reduce } from 'lodash/fp'
 
-const valuesToString = (v, k, acc) => {
-  const stringifyKeys = [
-    'id',
-    'createdAt',
-    'updatedAt'
-  ]
-  acc[k] = stringifyKeys.includes(k) ? v.toString() : v
-  return acc
-}
-
 // Pick some fields from a `belongsTo` relation. Takes a relation and an array
 // of field names. Snake case will be rewritten to camelcase in output property
-// names. Some values are converted via toString (dates, ids).
-export const refineOne = (relation, fields) => {
+// names. An optional third parameter specifies fields to rewrite. For example:
+//
+//   refineOne(
+//     post,
+//     [ 'id', 'created_at', 'name' ],
+//     { 'name': 'title' }
+//   )
+//
+// would yield something like:
+//
+//   { createdAt: '...', id: '1', 'title': 'Aardvarks' }
+export const refineOne = (relation, fields, rewrite) => {
   // A relation might simply not be set, so not a programmer error:
   if (!relation) return null
 
   // Lack of field names is a problem though...
   if (!Array.isArray(fields)) throw new Error ('Expected an array of field names.')
 
-  return reduce(
-    valuesToString,
-    mapKeys(k => camelCase(k), relation.pick(fields)),
-    {}
+  return mapKeys(
+    k => rewrite && rewrite[k] ? rewrite[k] : camelCase(k),
+    relation.pick(fields)
   )
 }
 
 // Pick some fields from a `belongsToMany` relation
-export const refineMany = (relation, fields) => {
+export const refineMany = (relation, fields, rewrite) => {
   if (!relation) return []
   if (!Array.isArray(fields)) throw new Error ('Expected an array of field names.')
-  return relation.map(entity => refineOne(entity, fields))
+  return relation.map(entity => refineOne(entity, fields, rewrite))
 }
