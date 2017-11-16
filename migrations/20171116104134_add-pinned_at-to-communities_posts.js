6f187f60c0b68dce899ce72902d40260cb963cd7
Robbie Carlton <robbie.carlton@gmail.com>
2017-11-16 10:47:44 -0700

    correct down function

--- migrations/20171116104134_add-pinned_at-to-communities_posts.js
+++ migrations/20171116104134_add-pinned_at-to-communities_posts.js
@@ -5,7 +5,7 @@ exports.up = function (knex, Promise) {
 }
 
 exports.down = function (knex, Promise) {
-  return knex.schema.table('communities', table => {
+  return knex.schema.table('communities_posts', table => {
     table.dropColumn('pinned_at')
   })
 }
