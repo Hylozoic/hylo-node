9f3736d57ed57c55d3cbcfa10a4aea1fdf3f5a39
Rich Churcher <rich.churcher@gmail.com>
2017-11-30 21:25:11 +1300

    Sketch out mutations

--- api/graphql/mutations/network.js
+++ api/graphql/mutations/network.js
@@ -2,6 +2,42 @@ import validateNetworkData from '../../models/network/validateNetworkData'
 import underlyingUpdateNetwork from '../../models/network/updateNetwork'
 import convertGraphqlData from './convertGraphqlData'
 
+export function addCommunityToNetwork (userId, { communityId, networkId }) {
+  return NetworkMembership.hasModeratorRole(userId, networkId)
+    .then(ok => {
+      if (!ok) throw new Error("You don't have permission to modify this network.")
+    })
+    .then(() => true)
+}
+
+export function addNetworkModeratorRole (userId, { personId, networkId }) {
+  return NetworkMembership.hasModeratorRole(userId, networkId)
+    .then(ok => {
+      if (!ok) throw new Error("You don't have permission to modify this network.")
+    })
+    .then(() => NetworkMembership.addModerator(personId, networkId))
+    .then(() => Network.find(networkId))
+}
+
+export function removeCommunityFromNetwork (userId, { communityId, networkId }) {
+  return NetworkMembership.hasModeratorRole(userId, networkId)
+    .then(ok => {
+      if (!ok) throw new Error("You don't have permission to modify this network.")
+    })
+    .then(() => true)
+}
+
+export function removeNetworkModeratorRole (userId, { personId, networkId }) {
+  return NetworkMembership.hasModeratorRole(userId, networkId)
+    .then(ok => {
+      if (!ok) throw new Error("You don't have permission to modify this network.")
+    })
+    .then(() => NetworkMembership
+      .where({ user_id: userId, network_id: networkId })
+      .delete())
+    .then(() => Network.find(networkId))
+}
+
 export function updateNetwork (userId, { id, data }) {
   const convertedData = convertGraphqlData(data)
   return NetworkMembership.hasModeratorRole(userId, id)
