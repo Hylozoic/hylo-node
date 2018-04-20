5f6c9a8019bdc2437d464761debbe1e5a3fd0e1b
Robbie Carlton <robbie.carlton@gmail.com>
2018-04-20 14:15:42 -0600

    update notification settings for users who joined in 2018

--- api/models/mixins/HasSettings.js
+++ api/models/mixins/HasSettings.js
@@ -2,10 +2,8 @@ import { get, has, isUndefined, merge, unset } from 'lodash'
 
 export default {
   addSetting: function (value, save = false) {
-    console.log('in addSetting')
     this.set('settings', merge({}, this.get('settings'), value))
     if (save) {
-      console.log('saving')
       return this.save({settings: this.get('settings')}, {patch: true})
     }
     return this
--- migrations/20180419113145_update-new-user-notification-settings.js
+++ migrations/20180419113145_update-new-user-notification-settings.js
@@ -3,37 +3,27 @@ const models = require('../api/models')
 const DataType = require('../api/models/group/DataType').default
 
 exports.up = async function (knex, Promise) {
-  console.log('initialzing models')
   models.init()
-  // const users = await User.where('created_at', '>', '2017-12-31')
 
-  console.log('fetching users')
-  const users = await User.where('id', '=', '11204')
-    .fetchAll()
+  const users = await User.where('created_at', '>', '2017-12-31')
+  .fetchAll()
 
-  console.log('users', users)
-
-  users.each(user => {
-    console.log('adding setting to user', user.id)
-    user.addSetting({
-      dm_notifications: 'both',
-      comment_notifications: 'both'
-    }, true)
-    .then(() => {
-      console.log('going to fetch memberships')
-      return GroupMembership.where({
-        group_data_type: DataType.COMMUNITY,
-        user_id: user.id
-      }).fetchAll().then(ms => console.log('ms', ms))
-    })
-    .then(memberships => {
-      console.log('memberships', memberships)
-      memberships.each(async membership => {
-        console.log('membership.settings', membership.get('settings'))
-        // console.log('membership.addSetting', membership.addSetting)
-      })
-    })
-  })
+  return Promise.map(users.models, user => user.addSetting({
+    dm_notifications: 'both',
+    comment_notifications: 'both'
+  }, true)
+  .then(() => GroupMembership.where({
+    group_data_type: DataType.COMMUNITY,
+    user_id: user.id
+  }).fetchAll())
+  .then(memberships => Promise.map(memberships.models,
+    membership => {
+      return membership.addSetting({
+        sendEmail: true,
+        sendPushNotifications: true
+      }, true)
+    }
+  )))
 }
 
 exports.down = function (knex, Promise) {
