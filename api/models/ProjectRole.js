02f9ae5611df6a97d73251316c39c380b55cee7a
Robbie Carlton <robbie.carlton@gmail.com>
2018-08-03 13:59:31 -0600

    tests

--- api/models/ProjectRole.js
+++ api/models/ProjectRole.js
@@ -1,10 +1,10 @@
 module.exports = bookshelf.Model.extend({
   tableName: 'project_roles',
-    project: function () {
-      return this.belongsTo(Post)
-    }
+  project: function () {
+    return this.belongsTo(Post)
+  }
 }, {
-   find: function (id, options) {
-     return ProjectRole.where({id}).fetch(options)
-   }
+  find: function (id, options) {
+    return ProjectRole.where({id}).fetch(options)
+  }
 })
