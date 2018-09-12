1bab8010d2c9a467048ea49eed0c7ecba13e5a6b
Robbie Carlton <robbie.carlton@gmail.com>
2018-09-12 13:49:50 -0600

    add BlockedUser model

---
+++ api/models/BlockedUser.js
@@ -0,0 +1,42 @@
+/* eslint-disable camelcase */
+module.exports = bookshelf.Model.extend({
+  tableName: 'blocked_users',
+
+  user: function () {
+    return this.belongsTo(User, 'user_id')
+  },
+
+  blockedUser: function () {
+    return this.belongsTo(User, 'blocked_user_id')
+  }
+
+}, {
+
+  create: function (userId, blockedUserId) {
+    if (userId === blockedUserId) {
+      throw new Error('blocked_user_id cannot equal user_id')
+    }
+
+    if (!userId || !blockedUserId) {
+      throw new Error('must provice a user_id and blocked_user_id')
+    }
+
+    return this.find(userId, blockedUserId)
+    .then(existing => {
+      if (existing) return existing
+
+      return new BlockedUser({
+        user_id: userId,
+        blocked_user_id: blockedUserId,
+        created_at: new Date(),
+        updated_at: new Date()
+      })
+      .save()
+    })
+  },
+
+  find: function (user_id, blocked_user_id) {
+    if (!user_id) throw new Error('Parameter user_id must be supplied.')
+    return BlockedUser.where({user_id, blocked_user_id}).fetch()
+  }
+})
