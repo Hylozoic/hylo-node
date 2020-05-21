1d9e1a474301b685060523ea187b0e852e64b7f8
Clare Politano Hutchings <cpolitano8@gmail.com>
2020-05-21 16:44:28 -0700

    check that resource has location

--- api/models/post/validatePostData.js
+++ api/models/post/validatePostData.js
@@ -4,15 +4,22 @@ export default function validatePostData (userId, data) {
   if (!trim(data.name)) {
     throw new Error('title can\'t be blank')
   }
+
   const allowedTypes = [Post.Type.REQUEST, Post.Type.OFFER, Post.Type.DISCUSSION, Post.Type.PROJECT, Post.Type.EVENT, Post.Type.RESOURCE]
   if (data.type && !includes(allowedTypes, data.type)) {
     throw new Error('not a valid type')
   }
+
   if (isEmpty(data.community_ids)) {
     throw new Error('no communities specified')
   }
+
   if (data.topicNames && data.topicNames.length > 3) {
     throw new Error('too many topics in post, maximum 3')
+  }
+  
+  if (data.type === Post.Type.RESOURCE && !data.location_id) {
+    throw new Error('resources must have a location')
   }
 
   return Group.allHaveMember(data.community_ids, userId, Community)
