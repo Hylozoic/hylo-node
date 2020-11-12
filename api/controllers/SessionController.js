3e0fe375016d42bd3d1719d823af2217d4109c7e
Tibet Sprague <tibet.sprague@gmail.com>
2020-11-12 10:54:32 -0800

    Try to track down error when logging in on staging

--- api/controllers/SessionController.js
+++ api/controllers/SessionController.js
@@ -146,6 +146,7 @@ module.exports = {
       return user
     }).catch(function (err) {
       // 422 means 'well-formed but semantically invalid'
+      sails.log.error(err.stack)
       res.status(422).send(err.message)
     })
   },
