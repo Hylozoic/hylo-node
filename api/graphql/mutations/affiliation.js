2682e9494bd17e4e8fd5a49fc84628ce6b7425e0
wushenshen <wushenshen@gmail.com>
2020-12-13 12:12:50 -0800

    add some url validation when adding an affiliation

--- api/graphql/mutations/affiliation.js
+++ api/graphql/mutations/affiliation.js
@@ -1,9 +1,12 @@
+import validator from 'validator'
+
 export async function canDeleteAffiliation (userId, affiliationId) {
   const affiliation = await Affiliation.find(affiliationId)
   return affiliation.get('user_id') === userId
 }
 
 export async function createAffiliation (userId, { role, preposition, orgName, url }) {
+  if (!validator.isURL(url)) throw new Error(`Please enter a valid URL.`)
   if (userId && role && preposition && orgName) {
     return Affiliation.create({
       userId,
