5ee61386110fe9b09adc9b2e9789e69826d6307b
Veselin Nikolov <vesoNikolov@gmail.com>
2020-12-16 15:53:11 +0200

    Uses explicit bluebird promise
     From 0.18+ bluebird is no longer used by knex and for seeds and
    mgirations the promise is no longer passed
    (More info: https://github.com/knex/knex/blob/master/UPGRADING.md#upgrading-to-version-0180)

--- seeds/migrations.js
+++ seeds/migrations.js
@@ -1,17 +1,18 @@
 const fs = require('fs')
 const path = require('path')
+const Promise = require('bluebird')
 
-exports.seed = function (knex, Promise) {
+exports.seed = function (knex) {
   return knex('knex_migrations').del()
     .then(() => {
       const files = fs.readdirSync(path.join(__dirname, '..', 'migrations'))
-      return addMigrations(knex, Promise, files.filter(f => f.slice(-3) === '.js'))
+      return addMigrations(knex, files.filter(f => f.slice(-3) === '.js'))
     })
 }
 
 // Add all migrations in directory to knex_migrations (any .js file in the
 // directory is assumed to be a migration).
-function addMigrations (knex, Promise, migrations) {
+function addMigrations (knex, migrations) {
   return Promise.reduce(
     migrations,
     (_, name) => knex('knex_migrations').insert({
