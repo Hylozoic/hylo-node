58ad02c7fa0841ad76699e9d156d5f84cf1dc4a4
Tibet Sprague <tibet.sprague@gmail.com>
2021-10-21 14:08:47 -0500

    Fix reseting of unread counts when viewing a group and a topic
    Fixes parts of https://github.com/Hylozoic/hylo-evo/issues/959

--- api/graphql/index.js
+++ api/graphql/index.js
@@ -144,10 +144,8 @@ export function makeAuthenticatedQueries (userId, fetchOne, fetchMany) {
       // you can specify id or slug, but not both
       const group = await fetchOne('Group', slug || id, slug ? 'slug' : 'id')
       if (updateLastViewed && group) {
-        const membership = await GroupMembership.forPair(userId, group).fetch()
-        if (membership) {
-          await membership.addSetting({ lastReadAt: new Date() }, true)
-        }
+        // Resets new post count to 0
+        await GroupMembership.updateLastViewedAt(userId, group)
       }
       return group
     },
@@ -350,7 +348,7 @@ export function makeMutations (userId, isAdmin) {
     updateGroupTopic: (root, { id, data }) => updateGroupTopic(id, data),
 
     // TODO: need this and the one above?
-    updateGroupTopicFollow: (root, args) => updateGroupTopic(userId, args),
+    updateGroupTopicFollow: (root, args) => updateGroupTopicFollow(userId, args),
 
     updateMe: (root, { changes }) => updateMe(userId, changes),
 
