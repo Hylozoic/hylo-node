fdf1a30a940a184e51fb5d1e396db2634f521054
Tom Watson <thomasgabriel.watson@gmail.com>
2021-10-28 16:37:32 -0700

    Add second type of duplicate location record check

--- api/graphql/mutations/location.js
+++ api/graphql/mutations/location.js
@@ -1,20 +1,33 @@
 import convertGraphqlData from './convertGraphqlData'
 
 export function findLocation (data) {
-  return Location.query({where: {
-    address_number: data.address_number || null,
-    address_street: data.address_street || null,
-    city: data.city || null,
-    locality: data.locality || null,
-    neighborhood: data.neighborhood || null,
-    postcode: data.postcode || null,
-    country: data.country || null
-  }}).fetch()
+  let query
+
+  if (!data.address_street && !data.address_number && !data.full_text) return null
+  if (data.address_number && data.address_street) {
+    query = {
+      where: {
+        address_number: data.address_number,
+        address_street: data.address_street,
+        city: data.city || null,
+        locality: data.locality || null,
+        neighborhood: data.neighborhood || null,
+        postcode: data.postcode || null,
+        country: data.country || null
+      }
+    }
+  }
+  if (data.full_text) {
+    query = {
+      where: {
+        full_text: data.full_text
+      }
+    }
+  }
+  return Location.query(query).fetch()
 }
 
 export function findOrCreateLocation (data) {
   const convertedData = convertGraphqlData(data)
   return findLocation(convertedData).then(location => location || Location.create(convertedData))
 }
-
-
