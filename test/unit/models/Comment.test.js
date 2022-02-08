6af1d057b4fa982a6ab8100d15c075688067d54c
Loren Johnson <lorenjohnson@gmail.com>
2022-02-07 19:34:55 -0800

    Update test

--- test/unit/models/Comment.test.js
+++ test/unit/models/Comment.test.js
@@ -11,17 +11,17 @@ describe('Comment', () => {
   describe('cleanEmailText', () => {
     it('wraps content in <p> tags and handles weird newlines', () => {
       const text = 'Ok then\r\nAll right\r\rSo it shall be'
-      expect(Comment.cleanEmailText(user, text)).to.equal('<p>Ok then<br>All right</p>\n<p>So it shall be</p>\n')
+      expect(Comment.cleanEmailText(user, text)).to.equal('<p>Ok then<br/>All right</p>\n<p>So it shall be</p>\n')
     })
 
     it("cuts off at the sender's name", () => {
       const text = "Wow!\rThat's great!\rBob A"
-      expect(Comment.cleanEmailText(user, text)).to.equal('<p>Wow!<br>That&#39;s great!</p>\n')
+      expect(Comment.cleanEmailText(user, text)).to.equal('<p>Wow!<br/>That&#39;s great!</p>\n')
     })
 
     it("cuts off at the sender's name preceded by dashes", () => {
       const text = "Wow!\rThat's great!\r--Bob A"
-      expect(Comment.cleanEmailText(user, text)).to.equal('<p>Wow!<br>That&#39;s great!</p>\n')
+      expect(Comment.cleanEmailText(user, text)).to.equal('<p>Wow!<br/>That&#39;s great!</p>\n')
     })
 
     it('removes a common signature pattern with two dashes', () => {
@@ -36,7 +36,7 @@ describe('Comment', () => {
 
     it('removes even a mangled divider', () => {
       const text = 'yoyo\nMeow!-----+Only+text+above+the+dashed+line+will+be+included lol\nok'
-      expect(Comment.cleanEmailText(user2, text)).to.equal('<p>yoyo<br>Meow!</p>\n')
+      expect(Comment.cleanEmailText(user2, text)).to.equal('<p>yoyo<br/>Meow!</p>\n')
     })
   })
 
