a73a45e52c8ea2e0b76b3819f8b3e81fd901732e
Tibet Sprague <tibet.sprague@gmail.com>
2023-01-16 21:18:46 -0800

    Remove consoles and temp renderError function for oAuth

--- api/policies/checkClientCredentials.js
+++ api/policies/checkClientCredentials.js
@@ -7,35 +7,32 @@ module.exports = async (req, res, next) => {
   let clientId
   if (match) {
     const token = match[1]
-    console.log("got token, ", token)
+
     try {
-      // Look for a JWT for client_credentials flow for API clients
+      // Look for a JWT for API clients using the Client Credentials oAuth flow
       const decoded = decodeHyloJWT(token)
-      console.log("decoded token:, ", decoded)
       clientId = decoded.client_id
 
       // TODO: check scopes/claims? or is that already happening in the OIDC resource server code?
-      //       i think it is checking if scope is ok there but probably need to check here too?
+      //       I think it is checking if scope is ok there but probably need to check here too?
       // XXX: for now only allow 'super' API clients to do anything
       const client = await (new OIDCAdapter("Client")).find(clientId)
-      console.log("client:, ", client)
       if (!client || client.role !== 'super') {
         return res.status(403).json({ error: 'Unauthorized' })
       }
       req.api_client = { id: clientId, name: client.name, scope: decoded.scope, super: true }
     } catch (e) {
-      // Not a JWT, look up token in the oidc_payloads table to see if it as an AccessToken for authorization_code flow
+      // Not a JWT, look up token in the oidc_payloads table to see if it as an AccessToken for the Authorization Code oAuth flow
       const accessToken = await (new OIDCAdapter("AccessToken")).find(token)
-      console.log("got access token", accessToken, accessToken?.exp)
       const expiresAt = accessToken && new Date(accessToken.exp * 1000)
-      console.log("expires", expiresAt, Date.now(), expiresAt > Date.now())
+
       if (expiresAt && expiresAt > Date.now()) {
         clientId = accessToken.clientId
         const client = await (new OIDCAdapter("Client")).find(clientId)
         if (!client) {
           return res.status(403).json({ error: 'Unauthorized' })
         }
-        console.log("client, ", client)
+
         req.api_client = { id: clientId, name: client.name, scope: accessToken.scope }
 
         // Log user in using Access Token
--- api/services/OpenIDConnect.js
+++ api/services/OpenIDConnect.js
@@ -102,23 +102,7 @@ const configuration = {
       return client.noPKCE ? false : true
     }
   },
-  proxy: true, // maybe??
-  renderError: async function(ctx, out, error) {
-    ctx.type = 'html';
-    ctx.body = `<!DOCTYPE html>
-      <head>
-        <title>oops! something went wrong</title>
-        <style>/* css and html classes omitted for brevity, see lib/helpers/defaults.js */</style>
-      </head>
-      <body>
-        <div>
-          <h1>oops! something went wrong</h1>
-          <b>Error: ${error}</b>
-          ${Object.entries(out).map(([key, value]) => `<pre><strong>${key}</strong>: ${value}</pre>`).join('')}
-        </div>
-      </body>
-      </html>`;
-  },
+  proxy: true,
   routes: {
     authorization: '/auth',
     backchannel_authentication: '/backchannel',
